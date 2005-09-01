07fb178e1f609b6fbc83cc96d03a5c9aa3e41b18
Patrick West <pwest@opendap.org>
2005-09-01 14:38:48 +0000

    data names for DODSDataHandlerInterface

---
+++ dispatch/OPeNDAPDataNames.h
@@ -0,0 +1,23 @@
+#define DATA_REQUEST "request"
+
+#define AGG_CMD "aggregation_command"
+#define AGG_HANDLER "aggregation_handler"
+
+#define POST_CONSTRAINT "post_constraint"
+
+#define RETURN_CMD "return_command"
+
+#define USER_ADDRESS "user_address"
+#define USER_NAME "user_name"
+
+#define SERVER_PID "pid"
+
+#define CONTAINER_NAME "container_name"
+#define STORE_NAME "store_name"
+#define SYMBOLIC_NAME "symbolic_name"
+#define REAL_NAME "real_name"
+#define REAL_NAME_LIST "real_name_list"
+#define CONTAINER_TYPE "type"
+
+#define DEF_NAME "def_name"
+#define DEFINITIONS "definitions"
