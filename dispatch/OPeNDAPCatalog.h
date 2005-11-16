cfda02cde1d6bf28228325eb99608debf1d5199d
Patrick West <pwest@opendap.org>
2005-11-16 17:56:12 +0000

    abstract base class for objects that know how to manipulate catalogs and return information

---
+++ dispatch/OPeNDAPCatalog.h
@@ -0,0 +1,58 @@
+// OPeNDAPCatalog.h
+
+// This file is part of bes, A C++ back-end server implementation framework
+// for the OPeNDAP Data Access Protocol.
+
+// Copyright (c) 2004,2005 University Corporation for Atmospheric Research
+// Author: Patrick West <pwest@ucar.org>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// You can contact University Corporation for Atmospheric Research at
+// 3080 Center Green Drive, Boulder, CO 80301
+ 
+// (c) COPYRIGHT University Corporation for Atmostpheric Research 2004-2005
+// Please read the full copyright statement in the file COPYRIGHT_UCAR.
+//
+// Authors:
+//      pwest       Patrick West <pwest@ucar.edu>
+
+#ifndef I_OPeNDAPCatalog_h
+#define I_OPeNDAPCatalog_h 1
+
+#include <string>
+
+using std::string ;
+
+class DODSTextInfo ;
+
+/** @brief abstract base class catalog object. Derived classes know how to
+ * show nodes and leaves in a catalog.
+ */
+class OPeNDAPCatalog {
+protected:
+				OPeNDAPCatalog( ) {} ;
+public:
+    virtual			~OPeNDAPCatalog( void ) {} ;
+
+    virtual void		show_nodes( const string &node,
+					    DODSTextInfo *info ) = 0 ;
+    virtual void		show_leaves( const string &node,
+					     DODSTextInfo *info ) = 0 ;
+};
+
+#endif // I_OPeNDAPCatalog_h
+
+// $Log: OPeNDAPCatalog.h,v $
