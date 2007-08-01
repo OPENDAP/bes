7d0c96d51eba95476b6029fa90e882603854e093
Patrick West <pwest@opendap.org>
2007-08-01 06:19:14 +0000

    BESDEBUG upgrade using context say. M    hello_world/SayModule.cc

--- hello_world/SayModule.cc
+++ hello_world/SayModule.cc
@@ -51,19 +51,19 @@ using std::endl ;
 void
 SayModule::initialize( const string &modname )
 {
-    BESDEBUG( "Initializing Say Handler:" << endl )
+    BESDEBUG( "say", "Initializing Say Handler:" << endl )
 
-    BESDEBUG( "    adding " << modname << " request handler" << endl )
+    BESDEBUG( "say", "    adding " << modname << " request handler" << endl )
     BESRequestHandlerList::TheList()->add_handler( modname, new SayRequestHandler( modname ) ) ;
 
-    BESDEBUG( "    adding " << say_RESPONSE << " response handler" << endl )
+    BESDEBUG( "say", "    adding " << say_RESPONSE << " response handler" << endl )
     BESResponseHandlerList::TheList()->add_handler( say_RESPONSE, SayResponseHandler::SayResponseBuilder ) ;
 
-    BESDEBUG( "    adding " << say_RESPONSE << " command" << endl )
+    BESDEBUG( "say", "    adding " << say_RESPONSE << " command" << endl )
     BESCommand *cmd = new SayCommand( say_RESPONSE ) ;
     BESCommand::add_command( say_RESPONSE, cmd ) ;
 
-    BESDEBUG( "    adding Say reporter" << endl )
+    BESDEBUG( "say", "    adding Say reporter" << endl )
     BESReporterList::TheList()->add_reporter( modname, new SayReporter ) ;
 
 
@@ -74,14 +74,14 @@ SayModule::initialize( const string &modname )
 void
 SayModule::terminate( const string &modname )
 {
-    BESDEBUG( "Removing Say Handlers" << endl )
+    BESDEBUG( "say", "Removing Say Handlers" << endl )
     BESRequestHandler *rh = BESRequestHandlerList::TheList()->remove_handler( modname ) ;
     if( rh ) delete rh ;
 
-    BESDEBUG( "    removing " << say_RESPONSE << " response handler" << endl )
+    BESDEBUG( "say", "    removing " << say_RESPONSE << " response handler" << endl )
     BESResponseHandlerList::TheList()->remove_handler( say_RESPONSE ) ;
 
-    BESDEBUG( "    removing " << say_RESPONSE << " command" << endl )
+    BESDEBUG( "say", "    removing " << say_RESPONSE << " command" << endl )
     BESCommand::del_command( say_RESPONSE ) ;
 }
 
