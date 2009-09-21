a0ea095d56157bce30ac18504056d7b9c9f8299f
James Gallagher <jgallagher@opendap.org>
2009-09-21 16:45:16 +0000

    cgi_util -- mime_util and some formatting hackery.
    M    csv-handler/dap_csv_handler.cc
    M    csv-handler/CSVDDS.cc

--- csv-handler/CSVDDS.cc
+++ csv-handler/CSVDDS.cc
@@ -11,19 +11,19 @@
 // modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation; either
 // version 2.1 of the License, or (at your option) any later version.
-// 
+//
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 // You can contact University Corporation for Atmospheric Research at
 // 3080 Center Green Drive, Boulder, CO 80301
- 
+
 // (c) COPYRIGHT University Corporation for Atmospheric Research 2004-2005
 // Please read the full copyright statement in the file COPYRIGHT_UCAR.
 //
@@ -53,7 +53,7 @@
 #include "CSV_Obj.h"
 
 void csv_read_descriptors(DDS &dds, const string &filename) {
-  
+
   vector<string> fieldList;
   string type;
   int recordCount;
@@ -83,16 +83,16 @@ void csv_read_descriptors(DDS &dds, const string &filename) {
       bt = dds.get_factory()->NewStr(string(*it));
       ar->add_var(bt);
       ar->append_dim(recordCount, "record");
-      
+
       index = 0;
-      for(vector<string>::iterator it = ((vector<string>*)data)->begin(); 
+      for(vector<string>::iterator it = ((vector<string>*)data)->begin();
 	  it != ((vector<string>*)data)->end(); it++) {
 	strings[index] = *it;
 	index++;
       }
-      
+
       ar->set_value(strings, recordCount);
-      
+
     } else if(type.compare(string(INT16)) == 0) {
       short* int16 = new short[recordCount];
       bt = dds.get_factory()->NewInt16(*it);
@@ -130,7 +130,7 @@ void csv_read_descriptors(DDS &dds, const string &filename) {
       ar->append_dim(recordCount, "record");
 
       index = 0;
-      for(vector<float>::iterator it = ((vector<float>*)data)->begin(); 
+      for(vector<float>::iterator it = ((vector<float>*)data)->begin();
 	  it != ((vector<float>*)data)->end(); it++) {
 	floats[index] = *it;
 	index++;
@@ -145,7 +145,7 @@ void csv_read_descriptors(DDS &dds, const string &filename) {
       ar->append_dim(recordCount, "record");
 
       index = 0;
-      for(vector<double>::iterator it = ((vector<double>*)data)->begin(); 
+      for(vector<double>::iterator it = ((vector<double>*)data)->begin();
 	  it != ((vector<double>*)data)->end(); it++) {
 	doubles[index] = *it;
 	index++;
--- csv-handler/dap_csv_handler.cc
+++ csv-handler/dap_csv_handler.cc
@@ -11,19 +11,19 @@
 // modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation; either
 // version 2.1 of the License, or (at your option) any later version.
-// 
+//
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 // You can contact University Corporation for Atmospheric Research at
 // 3080 Center Green Drive, Boulder, CO 80301
- 
+
 // (c) COPYRIGHT University Corporation for Atmospheric Research 2004-2005
 // Please read the full copyright statement in the file COPYRIGHT_UCAR.
 //
@@ -40,7 +40,7 @@
 #include"DataDDS.h"
 
 #include"ObjectType.h"
-#include"cgi_util.h"
+#include"mime_util.h"
 #include"ConstraintEvaluator.h"
 
 #include"CSVDDS.h"
