8bc2d2cbe5f190cf0b1c1e9b64b7ce83900383ac
James Gallagher <jgallagher@opendap.org>
2009-10-05 20:17:21 +0000

    Chnages for the 3.7.15 release. These should be merged to the trunk. One change in the RequestHandler.cc will need to be undone (removal of an include.
    M    configure.ac
    M    NEWS
    M    ChangeLog
    M    hdf4_handler.spec
    M    bes-testsuite/Makefile.am
    M    INSTALL
    M    HDF4RequestHandler.cc
    M    README
    M    Makefile.am

--- HDF4RequestHandler.cc
+++ HDF4RequestHandler.cc
@@ -33,7 +33,10 @@
 
 #include "HDF4RequestHandler.h"
 #include <BESResponseNames.h>
+#if 0
+// Removed for the build against libdap 3.9.3 and bes 3.7.2
 #include <BESDapNames.h>
+#endif
 #include <BESDASResponse.h>
 #include <BESDDSResponse.h>
 #include <BESDataDDSResponse.h>
