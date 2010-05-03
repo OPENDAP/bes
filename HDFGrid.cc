85e8615b0d1badfb6322a11298e1ff7a56b487c3
James Gallagher <jgallagher@opendap.org>
2010-05-03 22:11:45 +0000

    Fixed ticket 1542. The code in HDFGrid that transfers the *_dim_0, _dim_1, ... attributes to Map vectors was broken. It was not picking up the correct substring to get teh trailing digit. Fixed. Added tests based on the f97182070958.hdf data file.
    M    bes-testsuite/hdf4_handlerTest.at
    A    bes-testsuite/h4/f97182070958.hdf.ddx.bescmd
    A    bes-testsuite/h4/f97182070958.hdf.das.bescmd.baseline
    A    bes-testsuite/h4/f97182070958.hdf.das.bescmd
    A    bes-testsuite/h4/f97182070958.hdf.dds.bescmd
    A    bes-testsuite/h4/f97182070958.hdf.dds.bescmd.baseline
    A    bes-testsuite/h4/f97182070958.hdf.ddx.bescmd.baseline
    M    HDFGrid.cc
    AM   hdf-testfiles/f97182070958.hdf

--- HDFGrid.cc
+++ HDFGrid.cc
@@ -49,12 +49,15 @@
 #include <hdfclass.h>
 #include <hcstream.h>
 
+#include <Error.h>
+#include <debug.h>
+
 #include "escaping.h"
 #include "HDFGrid.h"
 #include "HDFArray.h"
 #include "hdfutil.h"
 
-#include "Error.h"
+//#include "Error.h"
 
 HDFGrid::HDFGrid(const string &n, const string &d) : Grid(n, d)
 {
@@ -227,9 +230,12 @@ void HDFGrid::transfer_attributes(AttrTable *at)
 		AttrTable *dim = at->get_attr_table(a_p);
 		// Get the integer from the end of the name and use that as the
 		// index to find the matching Map variable.
-		int n = atoi(dim->get_name().substr(i + 5).c_str());
+		DBG(cerr << "dim->name(): " << dim->get_name() << endl);
+		DBG(cerr << "dim->get_name().substr(i + dim_name_base.length()): " << dim->get_name().substr(i + dim_name_base.length()) << endl);
+		int n = atoi(dim->get_name().substr(i + dim_name_base.length()).c_str());
 		// Note that the maps are HDFArray instances, so we use that
 		// for the actual copy operation.
+		DBG(cerr << "Inside HDFGrid::transfer_attreibutes: n = " << n << endl);
 		dynamic_cast<HDFArray&> (*(*(map_begin() + n))).transfer_dimension_attribute(
 			dim);
 	    }
