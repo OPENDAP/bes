91682d6c525e531c2cb5016435341ef04aeb0180
James Gallagher <jgallagher@opendap.org>
2013-08-26 23:56:14 +0000

    indenting...
    M    dapreader/DapModule.cc

--- dapreader/DapModule.cc
+++ dapreader/DapModule.cc
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-using std::endl ;
+using std::endl;
 
 #include "DapModule.h"
 #include "DapRequestHandler.h"
@@ -19,80 +19,65 @@ using std::endl ;
 
 #define DAP_CATALOG "catalog"
 
-void
-DapModule::initialize( const string &modname )
+void DapModule::initialize(const string &modname)
 {
-    BESDEBUG( modname, "Initializing Dap Module "
-		       << modname << endl ) ;
+	BESDEBUG(modname, "Initializing Dap Module " << modname << endl);
 
-    BESDEBUG( modname, "    adding dap request handler" << endl ) ;
-    BESRequestHandlerList::TheList()->add_handler( modname, new DapRequestHandler( modname ) ) ;
+	BESDEBUG(modname, "    adding dap request handler" << endl);
+	BESRequestHandlerList::TheList()->add_handler(modname, new DapRequestHandler(modname));
 
-    BESDEBUG( modname, "    adding " << DAP_CATALOG << " catalog" << endl ) ;
-    if( !BESCatalogList::TheCatalogList()->ref_catalog( DAP_CATALOG ) )
-    {
-	BESCatalogList::TheCatalogList()->
-	    add_catalog( new BESCatalogDirectory( DAP_CATALOG ) ) ;
-    }
-    else
-    {
-	BESDEBUG( modname, "    catalog already exists, skipping" << endl ) ;
-    }
+	BESDEBUG(modname, "    adding " << DAP_CATALOG << " catalog" << endl);
+	if (!BESCatalogList::TheCatalogList()->ref_catalog(DAP_CATALOG)) {
+		BESCatalogList::TheCatalogList()->add_catalog(new BESCatalogDirectory(DAP_CATALOG));
+	}
+	else {
+		BESDEBUG(modname, "    catalog already exists, skipping" << endl);
+	}
 
-    BESDEBUG( modname, "    adding catalog container storage " << DAP_CATALOG
-		    << endl ) ;
-    if( !BESContainerStorageList::TheList()->ref_persistence( DAP_CATALOG ) )
-    {
-	BESContainerStorageCatalog *csc =
-	    new BESContainerStorageCatalog( DAP_CATALOG ) ;
-	BESContainerStorageList::TheList()->add_persistence( csc ) ;
-    }
-    else
-    {
-	BESDEBUG( modname, "    storage already exists, skipping" << endl ) ;
-    }
+	BESDEBUG(modname, "    adding catalog container storage " << DAP_CATALOG << endl);
+	if (!BESContainerStorageList::TheList()->ref_persistence(DAP_CATALOG)) {
+		BESContainerStorageCatalog *csc = new BESContainerStorageCatalog(DAP_CATALOG);
+		BESContainerStorageList::TheList()->add_persistence(csc);
+	}
+	else {
+		BESDEBUG(modname, "    storage already exists, skipping" << endl);
+	}
 
-    BESDEBUG( modname, "    adding Dap debug context" << endl ) ;
-    BESDebug::Register( modname ) ;
+	BESDEBUG(modname, "    adding Dap debug context" << endl);
+	BESDebug::Register(modname);
 
-    BESDEBUG( modname, "Done Initializing Dap Module "
-		       << modname << endl ) ;
+	BESDEBUG(modname, "Done Initializing Dap Module " << modname << endl);
 }
 
-void
-DapModule::terminate( const string &modname )
+void DapModule::terminate(const string &modname)
 {
-    BESDEBUG( modname, "Cleaning Dap module " << modname << endl ) ;
+	BESDEBUG(modname, "Cleaning Dap module " << modname << endl);
 
-    BESRequestHandler *rh = 0 ;
+	BESRequestHandler *rh = 0;
 
-    BESDEBUG( modname, "    removing dap request handler" << endl ) ;
-    rh = BESRequestHandlerList::TheList()->remove_handler( modname ) ;
-    if( rh ) delete rh ; rh = 0 ;
+	BESDEBUG(modname, "    removing dap request handler" << endl);
+	rh = BESRequestHandlerList::TheList()->remove_handler(modname);
+	if (rh) delete rh;
+	rh = 0;
 
-    BESDEBUG( "nc", "    removing catalog container storage"
-                    << DAP_CATALOG << endl ) ;
-    BESContainerStorageList::TheList()->deref_persistence( DAP_CATALOG ) ;
+	BESDEBUG("nc", "    removing catalog container storage" << DAP_CATALOG << endl);
+	BESContainerStorageList::TheList()->deref_persistence(DAP_CATALOG);
 
-    BESDEBUG( "nc", "    removing " << DAP_CATALOG << " catalog" << endl ) ;
-    BESCatalogList::TheCatalogList()->deref_catalog( DAP_CATALOG ) ;
+	BESDEBUG("nc", "    removing " << DAP_CATALOG << " catalog" << endl);
+	BESCatalogList::TheCatalogList()->deref_catalog(DAP_CATALOG);
 
-    BESDEBUG( modname, "Done Cleaning Dap module "
-		       << modname << endl ) ;
+	BESDEBUG(modname, "Done Cleaning Dap module " << modname << endl);
 }
 
-extern "C"
+extern "C" {
+BESAbstractModule *maker()
 {
-    BESAbstractModule *maker()
-    {
-	return new DapModule ;
-    }
+	return new DapModule;
+}
 }
 
-void
-DapModule::dump( ostream &strm ) const
+void DapModule::dump(ostream &strm) const
 {
-    strm << BESIndent::LMarg << "DapModule::dump - ("
-			     << (void *)this << ")" << endl ;
+	strm << BESIndent::LMarg << "DapModule::dump - (" << (void *) this << ")" << endl;
 }
 
