c1781033aff9e19997bd6543ff4edf10c9f8ebdc
Nathan Potter <ndp@opendap.org>
2014-02-28 17:34:07 +0000

    bes: Incremental ci for team sync

---
+++ dap/unit-tests/StoredDap4ResultTest.cc
@@ -0,0 +1,335 @@
+// -*- mode: c++; c-basic-offset:4 -*-
+
+// This file is part of libdap, A C++ implementation of the OPeNDAP Data
+// Access Protocol.
+
+// Copyright (c) 2002,2003 OPeNDAP, Inc.
+// Author: James Gallagher <jgallagher@opendap.org>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+
+#include "config.h"
+
+#include <dirent.h>
+
+#include <cppunit/TextTestRunner.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <DMR.h>
+#include <D4ParserSax2.h>
+#include <ConstraintEvaluator.h>
+#include <DAS.h>
+#include <DDS.h>
+
+#include <GetOpt.h>
+#include <GNURegex.h>
+#include <util.h>
+#include <debug.h>
+
+#include <test/D4TestTypeFactory.h>
+
+#include "TheBESKeys.h"
+#include "BESStoredDapResultCache.h"
+#include "BESDapResponseBuilder.h"
+#include "BESDebug.h"
+
+
+#include "testFile.h"
+#include "test_config.h"
+
+#define BES_DATA_ROOT "BES.Data.RootDirectory"
+#define BES_CATALOG_ROOT "BES.Catalog.catalog.RootDirectory"
+
+
+
+
+using namespace CppUnit;
+using namespace std;
+using namespace libdap;
+
+int test_variable_sleep_interval = 0;
+
+static bool debug = false;
+static bool parser_debug = false;
+
+#undef DBG
+#define DBG(x) do { if (debug) (x); } while(false);
+
+using namespace libdap;
+
+class StoredDap4ResultTest: public TestFixture {
+private:
+	D4TestTypeFactory *d4_ttf;
+	D4BaseTypeFactory *d4_btf;
+	DMR *test_01_dmr;
+	D4ParserSax2 *d4parser;
+
+	BESDapResponseBuilder responseBuilder;
+
+    string d_data_root_dir;
+    string d_stored_result_subdir;
+	string d_response_cache;
+    BESStoredDapResultCache *cache;
+
+public:
+    StoredDap4ResultTest() : d4_ttf(0), d4_btf(0),
+                         test_01_dmr(0),
+    					 d4parser(0),
+    					 d_data_root_dir(string(TEST_SRC_DIR)),
+    					 d_stored_result_subdir("/response_cache"),
+    					 d_response_cache(string(TEST_SRC_DIR)+"/response_cache"),
+    					 cache(0) {
+    }
+
+    ~StoredDap4ResultTest() {
+    }
+
+    void clean_cache(const string &directory, const string &prefix) {
+        DIR *dip = opendir(directory.c_str());
+        if (!dip)
+            throw InternalErr(__FILE__, __LINE__, "Unable to open cache directory " + directory);
+
+        struct dirent *dit;
+        // go through the cache directory and collect all of the files that
+        // start with the matching prefix
+        while ((dit = readdir(dip)) != NULL) {
+            string dirEntry = dit->d_name;
+            if (dirEntry.compare(0, prefix.length(), prefix) == 0) {
+            	unlink(string(directory + "/" + dit->d_name).c_str());
+            }
+        }
+
+        closedir(dip);
+    }
+
+    void setUp() {
+		DBG(cerr << "setUp() - BEGIN" << endl);
+		DBG(BESDebug::SetUp("cerr,all"));
+
+
+        d4parser = new D4ParserSax2();
+    	DBG(cerr << "Built D4ParserSax2() " << endl);
+
+    	d4_ttf = new D4TestTypeFactory();
+    	DBG(cerr << "Built D4TestTypeFactory() " << endl);
+
+    	d4_btf = new D4BaseTypeFactory();
+    	DBG(cerr << "Built D4BaseTypeFactory() " << endl);
+
+
+    	test_01_dmr = new DMR(d4_ttf);
+    	DBG(cerr << "Built DMR(D4TestTypeFactory *) " << endl);
+
+    	string dmr_filename = (string)TEST_SRC_DIR + "/input-files/test_01.dmr";
+    	DBG(cerr << "Parsing DMR file " << dmr_filename << endl);
+    	d4parser->intern(readTestBaseline(dmr_filename), test_01_dmr, parser_debug);
+    	DBG(cerr << "Parsed DMR from file " << dmr_filename << endl);
+
+
+    	// for these tests, set the filename to the dataset_name. ...keeps the cache names short
+        test_01_dmr->set_filename(test_01_dmr->name());
+
+    	// cid == http://dods.coas.oregonstate.edu:8080/dods/dts/test.01.blob
+    	DBG(cerr << "DDS Name: " << test_01_dmr->name() << endl);
+
+		DBG(cerr << "setUp() - END" << endl);
+    }
+
+    void tearDown() {
+		DBG(cerr << "tearDown() - BEGIN" << endl);
+
+		// clean_cache(d_response_cache, "result_");
+
+        delete d4parser;
+        delete test_01_dmr;
+        delete d4_ttf;
+
+        DBG(cerr << "tearDown() - END" << endl);
+    }
+
+
+
+
+
+    bool re_match(Regex &r, const string &s) {
+        DBG(cerr << "s.length(): " << s.length() << endl);
+        int pos = r.match(s.c_str(), s.length());
+        DBG(cerr << "r.match(s): " << pos << endl);
+        return pos > 0 && static_cast<unsigned> (pos) == s.length();
+    }
+
+    bool re_match_binary(Regex &r, const string &s) {
+        DBG(cerr << "s.length(): " << s.length() << endl);
+        int pos = r.match(s.c_str(), s.length());
+        DBG(cerr << "r.match(s): " << pos << endl);
+        return pos > 0;
+    }
+
+
+
+
+
+
+    // Because setup() and teardown() clean out the cache directory, there should
+    // never be a cached item; calling read_cached_dataset() should return a
+    // valid DDS with data and store a copy in the cache.
+	void cache_a_dap4_response()
+	{
+		DBG(cerr << "**** cache_a_dap4_response() - BEGIN" << endl);
+		string baseline_local_id = d_stored_result_subdir + "/result_16472407572333548466.dap";
+
+		cache = BESStoredDapResultCache::get_instance(d_data_root_dir, d_stored_result_subdir, "result_", 1000);
+
+		DBG(cerr << "cache_a_dap4_response() - Got the instance of BESStoredResultCache object: " );
+		if(cache){
+			DBG(cerr << *cache << endl);
+		}
+		else {
+			DBG(cerr << "NULL" << endl);
+		}
+
+		string stored_result_local_id;
+		try {
+			// TODO Could stat the cache file to make sure it's not already there.
+			DBG(cerr << "cache_a_response() - caching a dataset... " << endl);
+			stored_result_local_id = cache->store_dap4_result(*test_01_dmr, "", &responseBuilder);
+
+			DBG(cerr << "Cached response id: " << stored_result_local_id << endl);
+			CPPUNIT_ASSERT(stored_result_local_id == baseline_local_id);
+			// TODO Stat the cache file to check it's size
+			cache->delete_instance();
+		}
+		catch (Error &e) {
+			CPPUNIT_FAIL(e.get_error_message());
+		}
+		DBG(cerr << "**** cache_a_dap4_response() - END" << endl);
+
+    }
+
+
+	// The first call reads values into the DDS, stores a copy in the cache and
+	// returns the DDS. The second call reads the value from the cache.
+	void cache_and_read_a_dap4_response()
+	{
+		DBG(cerr << "**** cache_and_read_a_dap4_response() - BEGIN" << endl);
+		string baseline_local_id = d_stored_result_subdir + "/result_16472407572333548466.dap";
+
+		cache = BESStoredDapResultCache::get_instance(d_data_root_dir, d_stored_result_subdir, "result_", 1000);
+		DBG(cerr << "cache_and_read_a_dap4_response() - Got BESStoredDapResultCache instance." << endl);
+		string stored_result_local_id;
+		try {
+			// TODO Could stat the cache file to make sure it's not already there.
+			DBG(cerr << "cache_and_read_a_dap4_response() - caching a dataset... " << endl);
+			stored_result_local_id = cache->store_dap4_result(*test_01_dmr, "", &responseBuilder);
+
+			DBG(cerr << "cache_and_read_a_dap4_response() - Cached response id: " << stored_result_local_id << endl);
+			CPPUNIT_ASSERT(stored_result_local_id == baseline_local_id);
+
+
+			// DDS *get_cached_dap2_data_ddx(const string &cache_file_name, BaseTypeFactory *factory, const string &dataset)
+			// Force read from the cache file
+
+			string cacheFileName = d_data_root_dir+stored_result_local_id;
+			DBG(cerr << "cache_and_read_a_dap4_response() - Reading stored DAP4 dataset. cache filename: "<< cacheFileName << endl);
+
+			DMR *cached_data = cache->get_cached_dap4_data(cacheFileName, d4_btf, "test.05");
+
+			DBG(cerr << "cache_and_read_a_dap4_response() - Stored DAP4 dataset has been read." << endl);
+			DBG(cerr << "cache_and_read_a_dap4_response() - cached_data->request_size(true): " << cached_data->request_size(true) << endl);
+
+			CPPUNIT_ASSERT(cached_data);
+			CPPUNIT_ASSERT(stored_result_local_id == baseline_local_id);
+			// There are nine variables in test.05.ddx
+			CPPUNIT_ASSERT(cached_data->request_size(true) == 9);
+
+#if 0
+			ostringstream oss;
+			DDS::Vars_iter i = cache_dap->var_begin();
+			while (i != cache_dap->var_end()) {
+				DBG(cerr << "Variable " << (*i)->name() << endl);
+				// this will incrementally add thr string rep of values to 'oss'
+				(*i)->print_val(oss, "", false /*print declaration */);
+				DBG(cerr << "Value " << oss.str() << endl);
+				++i;
+			}
+
+			// In this regex the value of <number> in the DAP2 Str variable (Silly test string: <number>)
+			// is a any single digit. The *Test classes implement a counter and return strings where
+			// <number> is 1, 2, ..., and running several of the tests here in a row will get a range of
+			// values for <number>.
+			Regex regex("2551234567894026531840320006400099.99999.999\"Silly test string: [0-9]\"\"http://dcz.gso.uri.edu/avhrr-archive/archive.html\"");
+			CPPUNIT_ASSERT(re_match(regex, oss.str()));
+
+#endif
+
+			delete cached_data; cached_data = 0;
+	    	// cache->delete_instance();
+			cache->delete_instance();
+
+		}
+		catch (Error &e) {
+			CPPUNIT_FAIL(e.get_error_message());
+		}
+		DBG(cerr << "**** cache_and_read_a_dap4_response() - END" << endl);
+
+    }
+
+
+
+    CPPUNIT_TEST_SUITE( StoredDap4ResultTest );
+
+    CPPUNIT_TEST(cache_a_dap4_response);
+    CPPUNIT_TEST(cache_and_read_a_dap4_response);
+
+    CPPUNIT_TEST_SUITE_END();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(StoredDap4ResultTest);
+
+int main(int argc, char*argv[]) {
+    CppUnit::TextTestRunner runner;
+    runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
+
+    GetOpt getopt(argc, argv, "d");
+    char option_char;
+    while ((option_char = getopt()) != EOF)
+        switch (option_char) {
+        case 'd':
+            debug = 1;  // debug is a static global
+            break;
+        default:
+            break;
+        }
+
+    bool wasSuccessful = true;
+    string test = "";
+    int i = getopt.optind;
+    if (i == argc) {
+        // run them all
+        wasSuccessful = runner.run("");
+    }
+    else {
+        while (i < argc) {
+            test = string("StoredDap4ResultTest::") + argv[i++];
+
+            wasSuccessful = wasSuccessful && runner.run(test);
+        }
+    }
+
+    return wasSuccessful ? 0 : 1;
+}
