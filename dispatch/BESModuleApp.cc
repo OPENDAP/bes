a6eddbe1e5bcf724d99440c13fb4254709afbfd7
James Gallagher <jgallagher@opendap.org>
2014-08-21 17:58:15 +0000

    Spelling in an error message

--- dispatch/BESModuleApp.cc
+++ dispatch/BESModuleApp.cc
@@ -10,12 +10,12 @@
 // modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation; either
 // version 2.1 of the License, or (at your option) any later version.
-// 
+//
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -139,7 +139,7 @@ int BESModuleApp::loadModules()
                     return 1;
                 }
                 if (so == "") {
-                    cerr << "couldn't find the module for " << (*i) << endl;
+                    cerr << "Couldn't find the module for " << (*i) << endl;
                     return 1;
                 }
                 bes_module new_mod;
