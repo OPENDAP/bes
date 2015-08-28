b3ac890116d873fe9af877727c9c8b56b31dc80c
James Gallagher <jgallagher@opendap.org>
2015-08-28 13:03:17 -0600

    Fixed the install_bes_source_deps.sh script
    ...so that it will build libdap 3.15

--- install_bes_source_deps.sh
+++ install_bes_source_deps.sh
@@ -6,12 +6,14 @@ set -e
 if test ! -d "$HOME/deps/bin" -a false
 then
   wget http://ftp.gnu.org/gnu/bison/bison-3.0.4.tar.gz
-  wget http://www.opendap.org/pub/tmp/libdap-3.15.0.tar.gz
-  wget http://www.opendap.org/pub/tmp/hdf5-1.8.15-patch1.tar.gz
   tar -xzf bison-3.0.4.tar.gz
   (cd bison-3.0.4 && ./configure --prefix=$HOME/deps/ && make -j7 && make install)
+
+  wget http://www.opendap.org/pub/tmp/libdap-3.15.0.tar.gz
   tar -xzf libdap-3.15.0.tar.gz
-  (cd libdap-3.14.0 && ./configure --prefix=$HOME/deps/ && make -j7 && make install)
+  (cd libdap-3.15.0 && ./configure --prefix=$HOME/deps/ && make -j7 && make install)
+
+  wget http://www.opendap.org/pub/tmp/hdf5-1.8.15-patch1.tar.gz
   tar -xzf hdf5-1.8.15-patch1.tar.gz
   (cd hdf5-1.8.15-patch1 && ./configure --prefix=$HOME/deps/ CFLAGS="-fPIC -O2 -w" && make -j7 && make install)
 else
