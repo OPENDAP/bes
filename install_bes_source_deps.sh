31b89c69b65e080c99d6c499b0dc46596d3ea932
James Gallagher <jgallagher@opendap.org>
2015-09-02 15:15:45 -0600

    Update travis.yml so that bes:master will build the correct version of lobdap

--- install_bes_source_deps.sh
+++ install_bes_source_deps.sh
@@ -3,15 +3,15 @@
 set -e
 
 # check to see if dependencies folder is empty
-if test ! -d "$HOME/deps/bin"
+if test ! -d "$HOME/deps/bin" -a false
 then
   wget http://ftp.gnu.org/gnu/bison/bison-3.0.4.tar.gz
-  wget http://www.opendap.org/pub/tmp/libdap-3.14.0.tar.gz
+  wget http://www.opendap.org/pub/tmp/libdap-3.14.2.tar.gz
   wget http://www.opendap.org/pub/tmp/hdf5-1.8.15-patch1.tar.gz
   tar -xzf bison-3.0.4.tar.gz
   (cd bison-3.0.4 && ./configure --prefix=$HOME/deps/ && make -j7 && make install)
-  tar -xzf libdap-3.14.0.tar.gz
-  (cd libdap-3.14.0 && ./configure --prefix=$HOME/deps/ && make -j7 && make install)
+  tar -xzf libdap-3.14.2.tar.gz
+  (cd libdap-3.14.2 && ./configure --prefix=$HOME/deps/ && make -j7 && make install)
   tar -xzf hdf5-1.8.15-patch1.tar.gz
   (cd hdf5-1.8.15-patch1 && ./configure --prefix=$HOME/deps/ CFLAGS="-fPIC -O2 -w" && make -j7 && make install)
 else
