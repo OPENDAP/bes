b83da8358d0d086e609de72b0c34ef58d19cf2b9
James Gallagher <jgallagher@opendap.org>
2015-09-04 11:24:55 -0600

    Travis now used hyrax-dependencies
    ...instead of a one-off process. No guarantee this will work
    because it's huge, but it's also worth a try.

--- install_bes_source_deps.sh
+++ install_bes_source_deps.sh
@@ -2,31 +2,42 @@
 
 set -e
 
-# we could test for versions...
+# Add in a better test that looks at the version numbers of the stuff
+# in hyrax-dependencies/downloads or src. Just get the basic build working
+# for now.
 
-if test ! -x "$HOME/deps/bin/bison"
+if test ! -d "$HOME/deps/lib"
 then
-  wget http://ftp.gnu.org/gnu/bison/bison-3.0.4.tar.gz
-  tar -xzf bison-3.0.4.tar.gz
-  (cd bison-3.0.4 && ./configure --prefix=$HOME/deps/ && make -j7 && make install)
+  wget http://www.opendap.org/pub/tmp/travis/hyrax-dependencies-1.11.2.tar
+  tar -xzf hyrax-dependencies-1.11.2.tar
+  (cd hyrax-dependencies && make -j7 prefix=$HOME/deps)
 else
-    echo "Using cached bison."
+    echo "Using cached hyrax-dependencies."
 fi
+
+# if test ! -x "$HOME/deps/bin/bison"
+# then
+#   wget http://ftp.gnu.org/gnu/bison/bison-3.0.4.tar.gz
+#   tar -xzf bison-3.0.4.tar.gz
+#   (cd bison-3.0.4 && ./configure --prefix=$HOME/deps/ && make -j7 && make install)
+# else
+#     echo "Using cached bison."
+# fi
 
 if test ! -x "$HOME/deps/bin/dap-config"
 then
-  wget http://www.opendap.org/pub/tmp/libdap-3.15.0.tar.gz
+  wget http://www.opendap.org/pub/tmp/travis/libdap-3.15.0.tar.gz
   tar -xzf libdap-3.15.0.tar.gz
   (cd libdap-3.15.0 && ./configure --prefix=$HOME/deps/ && make -j7 && make install)
 else
     echo "Using cached libdap."
 fi
 
-if test ! -x "$HOME/deps/bin/h5ls"
-then
-  wget http://www.opendap.org/pub/tmp/hdf5-1.8.15-patch1.tar.gz
-  tar -xzf hdf5-1.8.15-patch1.tar.gz
-  (cd hdf5-1.8.15-patch1 && ./configure --prefix=$HOME/deps/ CFLAGS="-fPIC -O2 -w" && make -j7 && make install)
-else
-  echo "Using cached libhdf5."
-fi
+# if test ! -x "$HOME/deps/bin/h5ls"
+# then
+#   wget http://www.opendap.org/pub/tmp/hdf5-1.8.15-patch1.tar.gz
+#   tar -xzf hdf5-1.8.15-patch1.tar.gz
+#   (cd hdf5-1.8.15-patch1 && ./configure --prefix=$HOME/deps/ CFLAGS="-fPIC -O2 -w" && make -j7 && make install)
+# else
+#   echo "Using cached libhdf5."
+# fi
