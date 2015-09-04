2bc140a593cf725c4e884e1c5dfbcd0cdd0957b2
James Gallagher <jgallagher@opendap.org>
2015-09-04 15:01:45 -0600

    More work on the Travis build

--- install_bes_source_deps.sh
+++ install_bes_source_deps.sh
@@ -5,43 +5,31 @@ set -e
 # Add in a better test that looks at the version numbers of the stuff
 # in hyrax-dependencies/downloads or src. Just get the basic build working
 # for now.
-#
-# FIXME reset the deps and remove the mkdir src and upload a new hyrax tar
-# jhrg 9/4/15
 
-if test ! -d "$HOME/deps/lib" || true
+# hyrax-dependencies appends '/deps' to 'prefix'
+export prefix=$HOME
+export PATH=$HOME/deps/bin:$PATH
+
+# Force the build FIXME
+rm -rf $HOME/deps
+
+if test ! -d "$HOME/deps"
 then
-  rm -rf $HOME/deps/*
   wget http://www.opendap.org/pub/tmp/travis/hyrax-dependencies-1.11.2.tar
   tar -xf hyrax-dependencies-1.11.2.tar
-  (cd hyrax-dependencies && mkdir src && make for-travis -j7 prefix=$HOME)
+  (cd hyrax-dependencies && make for-travis -j7)
 else
     echo "Using cached hyrax-dependencies."
 fi
 
-# if test ! -x "$HOME/deps/bin/bison"
-# then
-#   wget http://ftp.gnu.org/gnu/bison/bison-3.0.4.tar.gz
-#   tar -xzf bison-3.0.4.tar.gz
-#   (cd bison-3.0.4 && ./configure --prefix=$HOME/deps/ && make -j7 && make install)
-# else
-#     echo "Using cached bison."
-# fi
+# unlike hyrax-dependencies, the libdap tar needs --prefix to be the
+# complete dir name. The hyrax-deps... project is a bit of a hack...
 
 if test ! -x "$HOME/deps/bin/dap-config"
 then
   wget http://www.opendap.org/pub/tmp/travis/libdap-3.15.0.tar.gz
   tar -xzf libdap-3.15.0.tar.gz
-  (cd libdap-3.15.0 && ./configure --prefix=$HOME/deps/ && make -j7 && make install)
+  (cd libdap-3.15.0 && ./configure --prefix=$prefix/deps/ && make -j7 && make install)
 else
     echo "Using cached libdap."
 fi
-
-# if test ! -x "$HOME/deps/bin/h5ls"
-# then
-#   wget http://www.opendap.org/pub/tmp/hdf5-1.8.15-patch1.tar.gz
-#   tar -xzf hdf5-1.8.15-patch1.tar.gz
-#   (cd hdf5-1.8.15-patch1 && ./configure --prefix=$HOME/deps/ CFLAGS="-fPIC -O2 -w" && make -j7 && make install)
-# else
-#   echo "Using cached libhdf5."
-# fi
