5278d1298e33c4413ebebf1c16a59c9b639a1da4
James Gallagher <jgallagher@opendap.org>
2015-09-18 17:34:25 -0600

    moved version_update_modules.sh to the modules dir

---
+++ modules/version_update_modules.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#
+# Update the handler version numbers in the configure.ac, Makefile.am,
+# NEWS and update the ChangeLog with the git log info for each of the
+# bes modules given on the command line.
+#
+# This script automates rote updates when little has changed in a
+# handler. It will update the NEWS file, but not README or INSTALL.
+# When there are substantive changes, don't use it!
+#
+# This script tests for a 'sentinel' file that, if present, stops the
+# script from making any changes. This is because running the script
+# several times in the same directories for a single release is
+# probably an error. The idea behind the sentinel file is that a
+# person has to intentionally remove it to get the script to run and
+# edit the configure.ac, etc., files.
+
+for module in "$*"
+do
+
+    (cd $module
+
+     # Test the sentinel file. If this is here, do nothing.
+     if test -f version_updated
+     then
+	 echo "Found a 'version_updated' file, exiting."
+	 exit 1
+     fi
+
+     # If the sentinel file was not found, update the module's version information,
+     # by first creating teh sentinel file...
+     cat <<EOF >version_updated
+Updated on `date`
+EOF
+     
+     # Get the version number and module from the configure.ac.
+
+     name=`grep AC_INIT configure.ac | sed 's@AC_INIT(\(.*\),.*\([0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\),.*@\1@'`
+     version=`grep AC_INIT configure.ac | sed 's@AC_INIT(\(.*\),.*\([0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\),.*@\2@'`
+
+     # gnarly awk code from stack overflow
+     new_version=`echo $version | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}'`
+     echo "New Version: $new_version"
+
+     # Update configure.ac
+     new_ac_init_line="AC_INIT($name, $new_version, opendap-tech@opendap.org)"
+     sed "s/AC_INIT.*/$new_ac_init_line/g" < configure.ac > configure.ac.tmp
+
+     mv configure.ac configure.ac.bak
+     mv configure.ac.tmp configure.ac
+
+     # Update Makefile.am
+
+     new_m_ver_line="M_VER=$new_version"
+     sed "s/M_VER.*/$new_m_ver_line/g" < Makefile.am > Makefile.am.tmp
+
+     mv Makefile.am Makefile.am.bak
+     mv Makefile.am.tmp Makefile.am
+
+     # Update ChangeLog
+     start_date=`awk '/....-..-../ {print $1 ; exit}' ChangeLog`
+     if test -z "$start_date"; then start_date="1970-01-01"; fi
+
+     gitlog-to-changelog --since=$start_date > ChangeLog.tmp.top
+     cat ChangeLog.tmp.top ChangeLog > ChangeLog.tmp
+     rm ChangeLog.tmp.top
+
+     mv ChangeLog ChangeLog.bak
+     mv ChangeLog.tmp ChangeLog
+
+     # Update NEWS
+
+     cat <<EOF >NEWS.tmp.top
+News for version $new_version
+
+Minor updates, see the ChangeLog
+
+EOF
+
+     cat NEWS.tmp.top NEWS > NEWS.tmp
+     rm NEWS.tmp.top
+
+     mv NEWS NEWS.bak
+     mv NEWS.tmp NEWS
+
+     # This ends the subshell that processes a given module
+    )
+
+done
+
