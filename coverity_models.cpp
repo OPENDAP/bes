08c73e578b630dfcf4257ec9e63ad2d8aa7d795f
James Gallagher <jgallagher@opendap.org>
2015-09-17 21:44:15 -0600

    Added a bit more info about the coverity models

--- coverity_models.cpp
+++ coverity_models.cpp
@@ -1,3 +1,13 @@
+
+// A simple model for teh Coverity static checker
+// Update this and load it into Coverity to prevent false
+// positives due to code that's unusual - such as special
+// macros or functions that free memory (so delete or free
+// is not called when Coverity expects it to be).
+// 
+// See https://scan.coverity.com/models#overriding for more
+// info about models
+
 void ff_destroy_std_args(void* x) {
     __coverity_free__(x);
 }
