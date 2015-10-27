542aec2b96c3d3c9830d72025633fcc4910163ba
James Gallagher <jgallagher@opendap.org>
2015-10-26 21:14:29 -0600

    distcheck works

--- coverity_models.cpp
+++ coverity_models.cpp
@@ -1,5 +1,5 @@
 
-// A simple model for teh Coverity static checker
+// A simple model for the Coverity static checker
 // Update this and load it into Coverity to prevent false
 // positives due to code that's unusual - such as special
 // macros or functions that free memory (so delete or free
@@ -11,3 +11,10 @@
 void ff_destroy_std_args(void* x) {
     __coverity_free__(x);
 }
+
+#if 0
+Structure *d4s;
+D4Attribute *x = new D4Attribute("HDF5_HARDLINK",attr_str_c);
+d4s->attributes()->add_attribute_nocopy(x);
+//Should be modeled as __coverity_delete__(x)
+#endif
