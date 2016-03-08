0f58dc58482e6d9b2e8abcfd022585ad10f01a2c
kyang2014 <myang6@hdfgroup.org>
2016-03-08 14:07:14 -0600

    HFVHANDLER-300, adding the download.sh to obtain the NASA files.

--- data.nasa/download.sh
+++ data.nasa/download.sh
@@ -15,6 +15,7 @@ if [ -z "$GET" ]; then
 fi
 $GET ftp://ftp.hdfgroup.uiuc.edu/pub/outgoing/opendap/data/HDF4/NASA1/GESDISC/AIRS.2009.11.30.240.L2.CO2_Std.v5.4.11.0.CO2.T09346091404.hdf
 $GET ftp://ftp.hdfgroup.uiuc.edu/pub/outgoing/opendap/data/HDF4/NASA1/GESDISC/AIRS.2002.08.01.L3.RetStd_H031.v4.0.21.0.G06104133732.hdf
+$GET ftp://ftp.hdfgroup.uiuc.edu/pub/outgoing/opendap/data/HDF4/NASA1/GESDISC/AIRS.2013.01.01.L3.RetStd001.v6.0.9.0.G13092214104.hdf
 $GET ftp://ftp.hdfgroup.uiuc.edu/pub/outgoing/opendap/data/HDF4/NASA1/LAADS/MOD021KM.A2010277.1710.005.2010278082807.hdf
 $GET ftp://ftp.hdfgroup.uiuc.edu/pub/outgoing/opendap/data/HDF4/NASA1/LaRC/MISR_AEROSOL_P017_O036105_F10_0020_GOM_b64-72.hdf
 $GET ftp://ftp.hdfgroup.uiuc.edu/pub/outgoing/opendap/data/HDF4/NASA1/GESDISC/3B43.070901.6A.hdf
