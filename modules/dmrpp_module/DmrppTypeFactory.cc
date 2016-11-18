8bdbc8e6f3e78aa93945060229b88f41c84d1d71
James Gallagher <jgallagher@opendap.org>
2016-11-17 17:35:01 -0700

    Added a class factory to the DMR++ Handler

---
+++ modules/dmrpp_module/DmrppTypeFactory.cc
@@ -0,0 +1,257 @@
+// -*- mode: c++; c-basic-offset:4 -*-
+
+// This file is part of the BES
+
+// Copyright (c) 2016 OPeNDAP, Inc.
+// Author: James Gallagher <jgallagher@opendap.org>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+
+#include "config.h"
+
+#include <string>
+
+#include <BESDapError.h>
+#include <BESDebug.h>
+
+#include "DmrppByte.h"
+
+#include "DmrppInt8.h"
+
+#include "DmrppInt16.h"
+#include "DmrppUInt16.h"
+#include "DmrppInt32.h"
+#include "DmrppUInt32.h"
+
+#include "DmrppInt64.h"
+#include "DmrppUInt64.h"
+
+#include "DmrppFloat32.h"
+#include "DmrppFloat64.h"
+
+#include "DmrppStr.h"
+#include "DmrppUrl.h"
+
+#include "DmrppD4Enum.h"
+
+#include "DmrppD4Opaque.h"
+
+#include "DmrppArray.h"
+#include "DmrppStructure.h"
+
+#include "DmrppD4Sequence.h"
+#include "DmrppD4Group.h"
+
+#include "DmrppTypeFactory.h"
+
+using namespace libdap;
+using namespace std;
+
+BaseType *DmrppTypeFactory::NewVariable(Type t, const string &name) const
+{
+	switch (t) {
+	case dods_byte_c:
+		return NewByte(name);
+	case dods_char_c:
+		return NewChar(name);
+
+	case dods_uint8_c:
+		return NewUInt8(name);
+	case dods_int8_c:
+		return NewInt8(name);
+
+	case dods_int16_c:
+		return NewInt16(name);
+	case dods_uint16_c:
+		return NewUInt16(name);
+	case dods_int32_c:
+		return NewInt32(name);
+	case dods_uint32_c:
+		return NewUInt32(name);
+
+	case dods_int64_c:
+		return NewInt64(name);
+	case dods_uint64_c:
+		return NewUInt64(name);
+
+	case dods_float32_c:
+		return NewFloat32(name);
+	case dods_float64_c:
+		return NewFloat64(name);
+
+	case dods_str_c:
+		return NewStr(name);
+	case dods_url_c:
+		return NewURL(name);
+
+	case dods_enum_c:
+		return NewEnum(name);
+
+	case dods_opaque_c:
+		return NewOpaque(name);
+
+	case dods_array_c:
+		return NewArray(name);
+
+	case dods_structure_c:
+		return NewStructure(name);
+
+	case dods_sequence_c:
+		return NewD4Sequence(name);
+
+	case dods_group_c:
+		return NewGroup(name);
+
+	default:
+		throw BESDapError("Unimplemented type in DAP4.", true /*fatal*/, internal_error, __FILE__, __LINE__);
+	}
+}
+
+Byte *
+DmrppTypeFactory::NewByte(const string &n) const
+{
+	return new DmrppByte(n);
+}
+
+Byte *
+DmrppTypeFactory::NewChar(const string &n) const
+{
+	Byte *b = new DmrppByte(n);
+	b->set_type(dods_char_c);
+	return b;
+}
+
+Byte *
+DmrppTypeFactory::NewUInt8(const string &n) const
+{
+	Byte *b = new DmrppByte(n);
+	b->set_type(dods_uint8_c);
+	return b;
+}
+
+Int8 *
+DmrppTypeFactory::NewInt8(const string &n) const
+{
+	return new DmrppInt8(n);
+}
+
+Int16 *
+DmrppTypeFactory::NewInt16(const string &n) const
+{
+	return new DmrppInt16(n);
+}
+
+UInt16 *
+DmrppTypeFactory::NewUInt16(const string &n) const
+{
+	return new DmrppUInt16(n);
+}
+
+Int32 *
+DmrppTypeFactory::NewInt32(const string &n) const
+{
+	BESDEBUG("dmrpp", "Inside DAP4BaseTypeFactory::NewInt32" << endl);
+	return new DmrppInt32(n);
+}
+
+UInt32 *
+DmrppTypeFactory::NewUInt32(const string &n) const
+{
+	return new DmrppUInt32(n);
+}
+
+Int64 *
+DmrppTypeFactory::NewInt64(const string &n) const
+{
+    BESDEBUG("dmrpp", "Inside DAP4BaseTypeFactory::NewInt64" << endl);
+	return new DmrppInt64(n);
+}
+
+UInt64 *
+DmrppTypeFactory::NewUInt64(const string &n) const
+{
+	return new DmrppUInt64(n);
+}
+
+Float32 *
+DmrppTypeFactory::NewFloat32(const string &n) const
+{
+	return new DmrppFloat32(n);
+}
+
+Float64 *
+DmrppTypeFactory::NewFloat64(const string &n) const
+{
+	return new DmrppFloat64(n);
+}
+
+Str *
+DmrppTypeFactory::NewStr(const string &n) const
+{
+	return new DmrppStr(n);
+}
+
+Url *
+DmrppTypeFactory::NewUrl(const string &n) const
+{
+	return new DmrppUrl(n);
+}
+
+/** Note that this method is called NewURL - URL in caps.
+ */
+Url *
+DmrppTypeFactory::NewURL(const string &n) const
+{
+	return NewUrl(n);
+}
+
+D4Opaque *
+DmrppTypeFactory::NewOpaque(const string &n) const
+{
+	return new DmrppD4Opaque(n);
+}
+
+D4Enum *
+DmrppTypeFactory::NewEnum(const string &name, Type type) const
+{
+	return new DmrppD4Enum(name, type);
+}
+
+Array *
+DmrppTypeFactory::NewArray(const string &n, BaseType *v) const
+{
+	return new DmrppArray(n, v);
+}
+
+Structure *
+DmrppTypeFactory::NewStructure(const string &n) const
+{
+	return new DmrppStructure(n);
+}
+
+D4Sequence *
+DmrppTypeFactory::NewD4Sequence(const string &n) const
+{
+	return new DmrppD4Sequence(n);
+}
+
+D4Group *
+DmrppTypeFactory::NewGroup(const string &n) const
+{
+	return new DmrppD4Group(n);
+}
+
---
+++ modules/dmrpp_module/DmrppTypeFactory.h
@@ -0,0 +1,81 @@
+
+// -*- mode: c++; c-basic-offset:4 -*-
+
+// This file is part of the BES
+
+// Copyright (c) 2016 OPeNDAP, Inc.
+// Author: James Gallagher <jgallagher@opendap.org>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+
+#ifndef _dmrpp_type_factory_h
+#define _dmrpp_type_factory_h
+
+#include <string>
+
+#include <D4BaseTypeFactory.h>
+
+/**
+ * A factory for the DAP4 DmrppByte, ...,  types.
+ *
+ * @author James Gallagher
+ */
+class DmrppTypeFactory : public libdap::D4BaseTypeFactory {
+public:
+    DmrppTypeFactory() {}
+    virtual ~DmrppTypeFactory() {}
+
+    virtual BaseTypeFactory *ptr_duplicate() const { return new DmrppTypeFactory; }
+
+    virtual libdap::BaseType *NewVariable(libdap::Type t, const std::string &name) const;
+
+    virtual libdap::Byte *NewByte(const std::string &n = "") const;
+
+    // New for DAP4
+    virtual libdap::Int8 *NewInt8(const std::string &n = "") const;
+    virtual libdap::Byte *NewUInt8(const std::string &n = "") const;
+    virtual libdap::Byte *NewChar(const std::string &n = "") const;
+
+    virtual libdap::Int16 *NewInt16(const std::string &n = "") const;
+    virtual libdap::UInt16 *NewUInt16(const std::string &n = "") const;
+    virtual libdap::Int32 *NewInt32(const std::string &n = "") const;
+    virtual libdap::UInt32 *NewUInt32(const std::string &n = "") const;
+
+    // New for DAP4
+    virtual libdap::Int64 *NewInt64(const std::string &n = "") const;
+    virtual libdap::UInt64 *NewUInt64(const std::string &n = "") const;
+
+    virtual libdap::Float32 *NewFloat32(const std::string &n = "") const;
+    virtual libdap::Float64 *NewFloat64(const std::string &n = "") const;
+
+    virtual libdap::D4Enum *NewEnum(const std::string &n = "", libdap::Type type = libdap::dods_null_c) const;
+
+    virtual libdap::Str *NewStr(const std::string &n = "") const;
+    virtual libdap::Url *NewUrl(const std::string &n = "") const;
+    virtual libdap::Url *NewURL(const std::string &n = "") const;
+
+    virtual libdap::D4Opaque *NewOpaque(const std::string &n = "") const;
+
+    virtual libdap::Array *NewArray(const std::string &n = "", libdap::BaseType *v = 0) const;
+
+    virtual libdap::Structure *NewStructure(const std::string &n = "") const;
+    virtual libdap::D4Sequence *NewD4Sequence(const std::string &n = "") const;
+
+    virtual libdap::D4Group *NewGroup(const std::string &n = "") const;
+};
+
+#endif // _dmrpp_type_factory_h
