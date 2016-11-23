b35158f85bc451a6e3875ae86b4640fb9b29adda
ndp-opendap <ndp@opendap.org>
2016-11-23 15:36:27 -0800

    Added new read logic to stuff...

--- modules/dmrpp_module/DmrppD4Enum.cc
+++ modules/dmrpp_module/DmrppD4Enum.cc
@@ -95,12 +95,13 @@ void DmrppD4Enum::dump(ostream & strm) const
 {
     strm << DapIndent::LMarg << "DmrppD4Enum::dump - (" << (void *) this << ")" << endl;
     DapIndent::Indent();
-    strm << DapIndent::LMarg << "offset: " << get_offset() << endl;
-    strm << DapIndent::LMarg << "size:   " << get_size() << endl;
-    strm << DapIndent::LMarg << "md5:    " << get_md5() << endl;
-    strm << DapIndent::LMarg << "uuid:   " << get_uuid() << endl;
+    strm << DapIndent::LMarg << "offset:   " << get_offset() << endl;
+    strm << DapIndent::LMarg << "size:     " << get_size() << endl;
+    strm << DapIndent::LMarg << "md5:      " << get_md5() << endl;
+    strm << DapIndent::LMarg << "uuid:     " << get_uuid() << endl;
+    strm << DapIndent::LMarg << "data_url: " << get_data_url() << endl;
     D4Enum::dump(strm);
-    strm << DapIndent::LMarg << "value: " << d_buf << endl;
+    strm << DapIndent::LMarg << "value:    " << d_buf << endl;
     DapIndent::UnIndent();
 }
 
