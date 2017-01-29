98d40864ebd6df9c72a7aa0eab3d95a001425376
James Gallagher <jgallagher@opendap.org>
2017-01-28 19:42:53 -0700

    Moved teh asciival and www-interface modules out of dap-server.
    This organization was the last vestage of the old 'server3' code.

---
+++ modules/asciival/get_ascii.h
@@ -0,0 +1,43 @@
+
+// -*- mode: c++; c-basic-offset:4 -*-
+
+// Copyright (c) 2006 OPeNDAP, Inc.
+// Author: James Gallagher <jgallagher@opendap.org>
+//
+// This is free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 2.1 of the License, or (at your
+// option) any later version.
+//
+// This is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+// more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+
+#ifndef E_get_ascii_h
+#define E_get_ascii_h 1
+
+#include <DDS.h>
+
+namespace libdap {
+    class BaseType ;
+}
+
+namespace dap_asciival {
+
+    void get_data_values_as_ascii(libdap::DDS *dds, FILE *dest);
+    void get_data_values_as_ascii(libdap::DDS *dds, std::ostream &strm);
+
+    libdap::DDS *datadds_to_ascii_datadds(libdap::DDS *dds);
+
+    libdap::BaseType *basetype_to_asciitype(libdap::BaseType *bt);
+}
+
+#endif // E_get_ascii_h
+
