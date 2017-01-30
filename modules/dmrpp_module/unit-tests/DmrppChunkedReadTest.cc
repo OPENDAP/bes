2c56ecaf162da11253d197d050fdd8bea84aa243
ndp-opendap <ndp@opendap.org>
2017-01-30 15:26:26 -0800

    Prunning test data

--- modules/dmrpp_module/unit-tests/DmrppChunkedReadTest.cc
+++ modules/dmrpp_module/unit-tests/DmrppChunkedReadTest.cc
@@ -559,7 +559,7 @@ public:
 
     CPPUNIT_TEST_SUITE( DmrppChunkedReadTest );
 
-#if 1
+#if 0
     CPPUNIT_TEST(test_read_oneD_chunked_array);
     CPPUNIT_TEST(test_read_twoD_chunked_array);
     CPPUNIT_TEST(test_read_twoD_chunked_asymmetric_array);
@@ -591,6 +591,8 @@ public:
     CPPUNIT_TEST(test_a2_local_twoD_chunked_array);
     CPPUNIT_TEST(test_a3_local_twoD_chunked_array);
 #endif
+    CPPUNIT_TEST(test_a2_local_twoD_chunked_array);
+    CPPUNIT_TEST(test_a3_local_twoD_chunked_array);
 
 
 
