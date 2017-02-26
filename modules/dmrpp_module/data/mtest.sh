a91d79cf11b00ddbf75995ca047b9745e51ef8e0
ndp-opendap <ndp@opendap.org>
2017-02-26 13:03:28 -0800

    sync

--- modules/dmrpp_module/data/mtest.sh
+++ modules/dmrpp_module/data/mtest.sh
@@ -24,19 +24,48 @@ function get_resource_size(){
 ##################################################
 #
 # Here are some simple tests to see...
-#
-
-
-# time -p ./multiball -u "https://s3.amazonaws.com/opendap.test/data/nc/MB2006001_2006001_chla.nc" -s 140904652 -o MB2006001_2006001_chla.nc -c 20
-
-
-
 
 #url="https://s3.amazonaws.com/opendap.test/MVI_1803.MOV"; resource_size=1647477620;
 url="https://s3.amazonaws.com/opendap.test/data/nc/MB2006001_2006001_chla.nc"; resource_size=140904652;
 #url="https://s3.amazonaws.com/opendap.test/data/nc/MB2006001_2006001_chla.nc"; resource_size=1403;
 name="mvi_1803"
 
+#############################
+#MULTIBALL
+for shards in 20 10 5 2 1
+do
+    file_base=$name"_"$shards;
+    rm -f $file_base*
+    for rep in {1..10}
+    do
+        echo -n "url: $url size: $resource_size shards: $shards  rep: $rep  seconds: \c"
+        (time -p ./multiball -u $url -s $resource_size -o $file_base -c $shards) 2>> $file_base.log 
+        seconds=`tail -3 $file_base.log | grep real | awk '{print $2;}' -`
+        echo $seconds;
+        echo "$file_base: shards: $shards rep: $rep seconds: $seconds" >> $file_base.log;
+    done
+    time_vals=`grep real $file_base.log | awk '{printf("%s + ",$2);}' -`"0.0";
+    #echo "time_vals: $time_vals";
+    avg=`echo "scale=3; v=$time_vals; v=v/10.0; v" | bc`;
+    echo "$file_base:  shards: $shards average_time: $avg" | tee -a $file_base.log
+    #exit;
+done
+
+#
+# CuRL Command Line
+#
+file_base=$name"_curl_cmdln";
+for rep in {1..10}
+do
+    echo -n "url: $url CuRL_command_line rep: $rep  seconds: \c"
+    (time -p curl -s "$url" -o $file_base) 2>> $file_base.log
+    seconds=`tail -3 $file_base.log | grep real | awk '{print $2;}' -`
+    echo $seconds;
+    echo "$file_base: rep: $rep seconds: $seconds" >> $file_base.log;
+done
+time_vals=`grep real $file_base.log | awk '{printf("%s + ",$2);}' -`"0.0";
+avg=`echo "scale=3; v=$time_vals; v=v/10.0; v" | bc`;
+echo "$file_base:  CuRL_command_line average_time: $avg" | tee -a $file_base.log
 
 
 
@@ -46,6 +75,7 @@ name="mvi_1803"
 for shards in 20 10 5 2 1
 do
     file_base=$name"_curl_mproc";
+    reps=10;
     for rep in {1..10}
     do
        time -p (
@@ -89,57 +119,14 @@ do
    done
     time_vals=`grep real $file_base.log | awk '{printf("%s + ",$2);}' -`"0.0";
     avg=`echo "scale=3; v=$time_vals; v=v/10.0; v" | bc`;
-    echo "CuRL_command_line_multi_proc shards: $shards avg_time: $avg resource_size: $resource_size url: $url" | tee -a $file_base.log
+    echo "CuRL_command_line_multi_proc shards: $shards reps: $reps avg_time: $avg resource_size: $resource_size url: $url" | tee -a $file_base.log
 done
 
 
 
 exit;
 
-
-
-
-
-
-
-
-#############################
-#MULTIBALL
-for shards in 20 10 5 2 1
-do
-    file_base=$name"_"$shards;
-    rm -f $file_base*
-    for rep in {1..10}
-    do
-        echo -n "url: $url size: $resource_size shards: $shards  rep: $rep  seconds: \c"
-        (time -p ./multiball -u $url -s $resource_size -o $file_base -c $shards) 2>> $file_base.log 
-        seconds=`tail -3 $file_base.log | grep real | awk '{print $2;}' -`
-        echo $seconds;
-        echo "$file_base: shards: $shards rep: $rep seconds: $seconds" >> $file_base.log;
-    done
-    time_vals=`grep real $file_base.log | awk '{printf("%s + ",$2);}' -`"0.0";
-    #echo "time_vals: $time_vals";
-    avg=`echo "scale=3; v=$time_vals; v=v/10.0; v" | bc`;
-    echo "$file_base:  shards: $shards average_time: $avg" | tee -a $file_base.log
-    #exit;
-done
-#
-# CuRL Command Line
-#
-file_base=$name"_curl_cmdln";
-for rep in {1..10}
-do
-    echo -n "url: $url CuRL_command_line rep: $rep  seconds: \c"
-    (time -p curl -s "$url" -o $file_base) 2>> $file_base.log
-    seconds=`tail -3 $file_base.log | grep real | awk '{print $2;}' -`
-    echo $seconds;
-    echo "$file_base: rep: $rep seconds: $seconds" >> $file_base.log;
-done
-time_vals=`grep real $file_base.log | awk '{printf("%s + ",$2);}' -`"0.0";
-avg=`echo "scale=3; v=$time_vals; v=v/10.0; v" | bc`;
-echo "$file_base:  CuRL_command_line average_time: $avg" | tee -a $file_base.log
-
-
+# time -p ./multiball -u "https://s3.amazonaws.com/opendap.test/data/nc/MB2006001_2006001_chla.nc" -s 140904652 -o MB2006001_2006001_chla.nc -c 20
 
 
 # time -p ./multiball -u "https://s3.amazonaws.com/cloudydap/airs/AIRS.2015.01.02.L3.RetStd_IR001.v6.0.11.0.G15005190621.nc.h5"  -s 318843092 -o airs -c 20
