03847de8fc615b01009c65bb5ffbd7be69ad8419
ndp-opendap <ndp@opendap.org>
2017-02-26 18:38:56 -0800

    sync

--- modules/dmrpp_module/data/mtest.sh
+++ modules/dmrpp_module/data/mtest.sh
@@ -143,7 +143,7 @@ function multi_process_curl_cmdln() {
 
 url="https://s3.amazonaws.com/opendap.test/MVI_1803.MOV"; resource_size=1647477620;
 #url="https://s3.amazonaws.com/opendap.test/data/nc/MB2006001_2006001_chla.nc"; resource_size=140904652;
-url="https://s3.amazonaws.com/opendap.test/data/nc/MB2006001_2006001_chla.nc"; resource_size=1403;
+#url="https://s3.amazonaws.com/opendap.test/data/nc/MB2006001_2006001_chla.nc"; resource_size=1403;
 name="scratch/"`basename $url`
 
 rm -f $name*
