5d61d82071d5b22318c3cbb5200dd2d22e5160cc
ndp-opendap <ndp@opendap.org>
2017-02-26 18:02:46 -0800

    sync

--- modules/dmrpp_module/data/mtest.sh
+++ modules/dmrpp_module/data/mtest.sh
@@ -131,8 +131,8 @@ function multi_process_curl_cmdln() {
 #
 # Here are some simple tests to see...
 
-#url="https://s3.amazonaws.com/opendap.test/MVI_1803.MOV"; resource_size=1647477620;
-url="https://s3.amazonaws.com/opendap.test/data/nc/MB2006001_2006001_chla.nc"; resource_size=140904652;
+url="https://s3.amazonaws.com/opendap.test/MVI_1803.MOV"; resource_size=1647477620;
+#url="https://s3.amazonaws.com/opendap.test/data/nc/MB2006001_2006001_chla.nc"; resource_size=140904652;
 #url="https://s3.amazonaws.com/opendap.test/data/nc/MB2006001_2006001_chla.nc"; resource_size=1403;
 name="scratch/"`basename $url`
 
