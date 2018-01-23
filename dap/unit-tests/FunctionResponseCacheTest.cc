9e9f95f5ccff41915eed4cfcb736ed574e029182
James Gallagher <jgallagher@opendap.org>
2018-01-22 22:40:27 -0700

    More fixes; distcheck doesn't all work.
    [skip ci]

--- dap/unit-tests/FunctionResponseCacheTest.cc
+++ dap/unit-tests/FunctionResponseCacheTest.cc
@@ -140,7 +140,7 @@ private:
 
 public:
     FunctionResponseCacheTest() :
-        dp(&ttf), test_dds(0), d_cache(string(TEST_SRC_DIR) + c_cache_name), cache(0)
+        dp(&ttf), test_dds(0), d_cache(string(TEST_BUILD_DIR) + c_cache_name), cache(0)
     {
         libdap::ServerFunctionsList::TheList()->add_function(new TestFunction());
     }
