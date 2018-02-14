0eb1cda5cc6b5a20bf58c35489ac95e581b2b992
James Gallagher <jgallagher@opendap.org>
2018-02-13 17:32:37 -0700

    Attempt to fix the file locking cache tests with a parallel build.

--- dispatch/tests/FileLockingCacheTest.cc
+++ dispatch/tests/FileLockingCacheTest.cc
@@ -3,8 +3,8 @@
 // This file is part of bes, A C++ back-end server implementation framework
 // for the OPeNDAP Data Access Protocol.
 
-// Copyright (c) 2004-2009 University Corporation for Atmospheric Research
-// Author: Patrick West <pwest@ucar.edu> and Jose Garcia <jgarcia@ucar.edu>
+// Copyright (c) 2017 University Corporation for Atmospheric Research
+// Author: Nathan Potter <ndp@opendap.org>
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -22,13 +22,6 @@
 //
 // You can contact University Corporation for Atmospheric Research at
 // 3080 Center Green Drive, Boulder, CO 80301
-
-// (c) COPYRIGHT University Corporation for Atmospheric Research 2004-2005
-// Please read the full copyright statement in the file COPYRIGHT_UCAR.
-//
-// Authors:
-//      pwest       Patrick West <pwest@ucar.edu>
-//      jgarcia     Jose Garcia <jgarcia@ucar.edu>
 
 #include <unistd.h>  // for sleep
 #include <sys/types.h>
