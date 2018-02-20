c43b8fe87942227b0c74d454250db655b9a6c347
James Gallagher <jgallagher@opendap.org>
2018-02-20 13:11:03 -0700

    Added initial files

---
+++ dap/GlobalMetadataStore.h
@@ -0,0 +1,139 @@
+// -*- mode: c++; c-basic-offset:4 -*-
+
+// This file is part of Hyrax, A C++ implementation of the OPeNDAP Data
+// Access Protocol.
+
+// Copyright (c) 2018 OPeNDAP, Inc.
+// Author: James Gallagher <jgallagher@opendap.org>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+
+#ifndef _global_metadata_cache_h
+#define _global_metadata_cache_h
+
+#include <string>
+
+#include "BESFileLockingCache.h"
+
+namespace libdap {
+class DDS;
+class DMR;
+}
+
+namespace bes {
+
+/**
+ * @brief Cache the results from server functions.
+ *
+ * @author jhrg
+ */
+
+class GlobalMetadataStore: public BESFileLockingCache {
+private:
+    static bool d_enabled;
+    static GlobalMetadataStore *d_instance;
+
+    // Called by atexit()
+    static void delete_instance() {
+        delete d_instance;
+        d_instance = 0;
+    }
+
+    std::string get_hash(const std::string &name);
+
+    bool store_dds_response(libdap::DDS *dds, const std::string &key);
+    bool store_das_response(libdap::DDS *dds, const std::string &key);
+
+    // Suppress the automatic generation of these ctors
+    GlobalMetadataStore();
+    GlobalMetadataStore(const GlobalMetadataStore &src);
+
+    friend GlobalMetadataStroreTest;
+
+#if 0
+    bool is_valid(const std::string &cache_file_name, const std::string &dataset);
+
+    std::string get_resource_id(libdap::DDS *dds, const std::string &constraint);
+    std::string get_hash_basename(const std::string &resource_id);
+
+    libdap::DDS *read_cached_data(istream &cached_data);
+
+    libdap::DDS *write_dataset_to_cache(libdap::DDS *dds, const string &resourceId, const string &constraint,
+        const string &cache_file_name);
+
+    libdap::DDS *load_from_cache(const string &resource_id, string &cache_file_name);
+
+    friend class FunctionResponseCacheTest;
+    friend class StoredResultTest;
+#endif
+
+protected:
+    /**
+     * Protected constructor that calls BESFileLockingCache's constructor;
+     * lookup cache directory, item prefix and max cache size in BESKeys
+     *
+     * @note Use the get_instance() methods to get a pointer to the singleton for
+     * this class. Do not use this method except in derived classes. This method
+     * either builds a valid object or throws an exception.
+     *
+     * @param cache_dir key to find cache dir
+     * @param prefix key to find the cache prefix
+     * @param size key to find the cache size (in MBytes)
+     * @throws BESSyntaxUserError if the keys are not set in the BESKeys, if the key
+     * are either the empty string or zero, respectively, or if the cache directory does not exist.
+     */
+    GlobalMetadataStore(const string &cache_dir, const string &prefix, unsigned long long size) :
+        BESFileLockingCache(cache_dir, prefix, size)
+    {
+    }
+
+public:
+    static const string PATH_KEY;
+    static const string PREFIX_KEY;
+    static const string SIZE_KEY;
+
+    static GlobalMetadataStore *get_instance(const string &cache_dir, const string &prefix, unsigned long long size);
+    static GlobalMetadataStore *get_instance();
+
+    static string get_cache_dir_from_config();
+    static string get_cache_prefix_from_config();
+    static unsigned long get_cache_size_from_config();
+
+    virtual ~GlobalMetadataStore()
+    {
+    }
+
+    virtual void add_object(const std::string &name, libdap::DDS *dds);
+
+#if 0
+    virtual std::string get_dds_response(const std::string &key) { }
+    virtual std::string get_das_response(const std::string &key) { }
+
+    virtual void add_object(const std::string &name, libdap::DMR *dmr) { }
+
+    virtual std::string get_dmr_response(const std::string &key) { }
+
+    // These 'get' methods return null or the empty string if the thing is not in the store.
+    virtual libdap::DDS *get_dds_object(const std::string &key) { }
+    virtual libdap::DMR *get_dmr_object(const std::string &key) { }
+#endif
+
+};
+
+} // namespace bes
+
+#endif // _global_metadata_cache_h
---
+++ dap/unit-tests/GlobalMetadataStoreTest.cc
@@ -0,0 +1,378 @@
+// -*- mode: c++; c-basic-offset:4 -*-
+
+// This file is part of libdap, A C++ implementation of the OPeNDAP Data
+// Access Protocol.
+
+// Copyright (c) 2002,2003 OPeNDAP, Inc.
+// Author: James Gallagher <jgallagher@opendap.org>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+
+#include "config.h"
+
+#include <cppunit/TextTestRunner.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <Array.h>
+#include <Byte.h>
+#include <ServerFunctionsList.h>
+#include <ConstraintEvaluator.h>
+#include <DAS.h>
+#include <DDS.h>
+#include <DDXParserSAX2.h>
+
+#include <GetOpt.h>
+#include <GNURegex.h>
+#include <util.h>
+#include <debug.h>
+
+#include "BESError.h"
+#include "TheBESKeys.h"
+#include "BESDebug.h"
+
+#include "GlobalMetadataStore.h"
+
+#include "TestFunction.h"
+#include "test_utils.h"
+#include "test_config.h"
+
+int test_variable_sleep_interval = 0;
+
+static bool debug = false;
+static bool bes_debug = false;
+static bool clean = true;
+static const string c_cache_name = "/response_cache";
+
+#undef DBG
+#define DBG(x) do { if (debug) (x); } while(false);
+
+using namespace CppUnit;
+using namespace std;
+using namespace libdap;
+using namespace bes;
+
+const Type requested_type = dods_byte_c;
+const int num_dim = 2;
+const int dim_sz = 3;
+
+// Move this into the class when we goto C++-11
+const string d_cache_prefix = "mds"; // used when cleaning the cache, etc.
+
+class GlobalMetadataStoreTest: public TestFixture {
+private:
+    DDXParser dp;
+    TestTypeFactory ttf;
+    DDS *test_dds;
+
+    ConstraintEvaluator eval;
+
+    string d_cache;
+    BESDapFunctionResponseCache *cache;
+
+public:
+    GlobalMetadataStoreTest() :
+        dp(&ttf), test_dds(0), d_cache(string(TEST_BUILD_DIR) + c_cache_name), cache(0)
+    {
+        libdap::ServerFunctionsList::TheList()->add_function(new TestFunction());
+    }
+
+    ~GlobalMetadataStoreTest()
+    {
+    }
+
+    void setUp()
+    {
+        DBG(cerr << "setUp() - BEGIN" << endl);
+        if (bes_debug) BESDebug::SetUp("cerr,response_cache");
+
+        string cid; // This is an unused value-result parameter. jhrg 5/10/16
+        test_dds = new DDS(&ttf);
+        dp.intern((string) TEST_SRC_DIR + "/input-files/test.05.ddx", test_dds, cid);
+
+        // for these tests, set the filename to the dataset_name. ...keeps the cache names short
+        test_dds->filename(test_dds->get_dataset_name());
+
+        DBG(cerr << "DDS Name: " << test_dds->get_dataset_name() << endl);
+
+        if (clean) clean_cache_dir(d_cache);
+
+        TheBESKeys::ConfigFile = (string) TEST_SRC_DIR + "/input-files/test.keys"; // empty file. jhrg 10/20/15
+
+        DBG(cerr << "setUp() - END" << endl);
+    }
+
+    void tearDown()
+    {
+        DBG(cerr << "tearDown() - BEGIN" << endl);
+
+        delete test_dds;
+
+        if (clean) clean_cache_dir(d_cache);
+
+        DBG(cerr << "tearDown() - END" << endl);
+    }
+
+    bool re_match(Regex &r, const string &s)
+    {
+        DBG(cerr << "s.length(): " << s.length() << endl);
+        int pos = r.match(s.c_str(), s.length());
+        DBG(cerr << "r.match(s): " << pos << endl);
+        return pos > 0 && static_cast<unsigned>(pos) == s.length();
+    }
+
+    bool re_match_binary(Regex &r, const string &s)
+    {
+        DBG(cerr << "s.length(): " << s.length() << endl);
+        int pos = r.match(s.c_str(), s.length());
+        DBG(cerr << "r.match(s): " << pos << endl);
+        return pos > 0;
+    }
+
+    // The directory 'never' does not exist; the cache won't be initialized,
+    // so is_available() should be false
+    void ctor_test_1()
+    {
+        DBG(cerr << "ctor_test_1() - BEGIN" << endl);
+
+        string cacheDir = string(TEST_SRC_DIR) + "/never";
+        string prefix = "rc";
+        long size = 1000;
+
+        cache = BESDapFunctionResponseCache::get_instance(cacheDir, prefix, size);
+        DBG(cerr << "ctor_test_1() - retrieved BESDapFunctionResponseCache instance: " << cache << endl);
+
+        CPPUNIT_ASSERT(!cache);
+
+        DBG(cerr << "ctor_test_1() - END" << endl);
+    }
+
+    // The directory 'd_response_cache' should exist so is_available() should be
+    // true.
+    void ctor_test_2()
+    {
+        DBG(cerr << "ctor_test_2() - BEGIN" << endl);
+
+        string cacheDir = d_cache;
+        string prefix = "rc";
+        long size = 1000;
+        cache = BESDapFunctionResponseCache::get_instance(cacheDir, prefix, size);
+        DBG(cerr << "ctor_test_1() - retrieved BESDapFunctionResponseCache instance: " << cache << endl);
+
+        CPPUNIT_ASSERT(cache);
+
+        DBG(cerr << "ctor_test_2() - END" << endl);
+    }
+
+    // Because setup() and teardown() clean out the cache directory, there should
+    // never be a cached item; calling read_cached_dataset() should return a
+    // valid DDS with data and store a copy in the cache.
+    void cache_a_response()
+    {
+        DBG(cerr << "cache_a_response() - BEGIN" << endl);
+        cache = BESDapFunctionResponseCache::get_instance(d_cache, d_cache_prefix, 1000);
+
+        DBG(cerr << "cache_a_response() - Retrieved BESDapFunctionResponseCache object: " << cache << endl);
+
+        string token;
+        try {
+            CPPUNIT_ASSERT(test_dds);
+
+            DDS *result = cache->get_or_cache_dataset(test_dds, "test(\"foo\")");
+
+            CPPUNIT_ASSERT(result);
+
+            CPPUNIT_ASSERT(result->var("foo"));
+            CPPUNIT_ASSERT(result->var("foo")->type() == dods_array_c);
+        }
+        catch (BESError &e) {
+            CPPUNIT_FAIL(e.get_message());
+        }
+
+        DBG(cerr << "cache_a_response() - END" << endl);
+    }
+
+    // The first call reads values into the DDS, stores a copy in the cache and
+    // returns the DDS. The second call reads the value from the cache.
+    //
+    // Use load_from_cache() (Private interface) to read the data.
+    void cache_and_read_a_response()
+    {
+        DBG(cerr << "cache_and_read_a_response() - BEGIN" << endl);
+
+        cache = BESDapFunctionResponseCache::get_instance(d_cache, d_cache_prefix, 1000);
+        try {
+            const string constraint = "test(\"bar\")";
+
+            // This code is here to load the DataDDX response into the cache if it is not
+            // there already. If it is there, it reads it from the cache.
+            DDS *result = cache->get_or_cache_dataset(test_dds, constraint);
+
+            CPPUNIT_ASSERT(result);
+            int var_count = result->var_end() - result->var_begin();
+            CPPUNIT_ASSERT(var_count == 1);
+
+            //DDS *result2 = cache->get_or_cache_dataset(test_dds, "test(\"bar\")");
+            string resource_id = cache->get_resource_id(test_dds, constraint);
+            string cache_file_name = cache->get_hash_basename(resource_id);
+
+            DBG(
+                cerr << "cache_and_read_a_response() - resource_id: " << resource_id << ", cache_file_name: "
+                    << cache_file_name << endl);
+
+            DDS *result2 = cache->load_from_cache(resource_id, cache_file_name);
+            // Better not be null!
+            CPPUNIT_ASSERT(result2);
+            result2->filename("function_result_SimpleTypes");
+
+            // There are nine variables in test.05.ddx
+            var_count = result2->var_end() - result2->var_begin();
+            DBG(cerr << "cache_and_read_a_response() - var_count: " << var_count << endl);
+            CPPUNIT_ASSERT(var_count == 1);
+
+            ostringstream oss;
+            DDS::Vars_iter i = result2->var_begin();
+            while (i != result2->var_end()) {
+                DBG(cerr << "Variable " << (*i)->name() << endl);
+                // this will incrementally add the string rep of values to 'oss'
+                (*i)->print_val(oss, "", false /*print declaration */);
+                DBG(cerr << "Value " << oss.str() << endl);
+                ++i;
+            }
+
+            CPPUNIT_ASSERT(oss.str().compare("{{0, 1, 2},{3, 4, 5},{6, 7, 8}}") == 0);
+        }
+        catch (Error &e) {
+            CPPUNIT_FAIL(e.get_error_message());
+        }
+
+        DBG(cerr << "cache_and_read_a_response() - END" << endl);
+    }
+
+    // The first call reads values into the DDS, stores a copy in the cache and
+    // returns the DDS. The second call reads the value from the cache.
+    //
+    // Use the public interface to read the data (cache_dataset()), but w/o a
+    // constraint
+    void cache_and_read_a_response2()
+    {
+        DBG(cerr << "cache_and_read_a_response() - BEGIN" << endl);
+
+        cache = BESDapFunctionResponseCache::get_instance(d_cache, d_cache_prefix, 1000);
+        try {
+            // This code is here to load the DataDDX response into the cache if it is not
+            // there already. If it is there, it reads it from the cache.
+            DDS *result = cache->get_or_cache_dataset(test_dds, "test(\"bar\")");
+
+            CPPUNIT_ASSERT(result);
+            int var_count = result->var_end() - result->var_begin();
+            CPPUNIT_ASSERT(var_count == 1);
+
+            DDS *result2 = cache->get_or_cache_dataset(test_dds, "test(\"bar\")");
+            // Better not be null!
+            CPPUNIT_ASSERT(result2);
+            result2->filename("function_result_SimpleTypes");
+
+            // There are nine variables in test.05.ddx
+            var_count = result2->var_end() - result2->var_begin();
+            DBG(cerr << "cache_and_read_a_response() - var_count: " << var_count << endl);
+            CPPUNIT_ASSERT(var_count == 1);
+
+            ostringstream oss;
+            DDS::Vars_iter i = result2->var_begin();
+            while (i != result2->var_end()) {
+                DBG(cerr << "Variable " << (*i)->name() << endl);
+                // this will incrementally add the string rep of values to 'oss'
+                (*i)->print_val(oss, "", false /*print declaration */);
+                DBG(cerr << "Value " << oss.str() << endl);
+                ++i;
+            }
+
+            CPPUNIT_ASSERT(oss.str().compare("{{0, 1, 2},{3, 4, 5},{6, 7, 8}}") == 0);
+        }
+        catch (Error &e) {
+            CPPUNIT_FAIL(e.get_error_message());
+        }
+
+        DBG(cerr << "cache_and_read_a_response() - END" << endl);
+    }
+
+CPPUNIT_TEST_SUITE( GlobalMetadataStoreTest );
+
+    CPPUNIT_TEST(ctor_test_1);
+    CPPUNIT_TEST(ctor_test_2);
+    CPPUNIT_TEST(cache_a_response);
+    CPPUNIT_TEST(cache_and_read_a_response);
+    CPPUNIT_TEST(cache_and_read_a_response2);
+
+    CPPUNIT_TEST_SUITE_END();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(GlobalMetadataStoreTest);
+
+int main(int argc, char*argv[])
+{
+
+    GetOpt getopt(argc, argv, "dbkh");
+    int option_char;
+    while ((option_char = getopt()) != -1)
+        switch (option_char) {
+        case 'd':
+            debug = 1;  // debug is a static global
+            break;
+        case 'b':
+            bes_debug = true;  // bes_debug is a static global
+            cerr << "##### BES DEBUG is ON" << endl;
+            break;
+        case 'k':   // -k turns off cleaning the metadata store dir
+            clean = false;
+            break;
+        case 'h': {     // help - show test names
+            cerr << "Usage: GlobalMetadataStoreTest has the following tests:" << endl;
+            const std::vector<Test*> &tests = GlobalMetadataStoreTest::suite()->getTests();
+            unsigned int prefix_len = GlobalMetadataStoreTest::suite()->getName().append("::").length();
+            for (std::vector<Test*>::const_iterator i = tests.begin(), e = tests.end(); i != e; ++i) {
+                cerr << (*i)->getName().replace(0, prefix_len, "") << endl;
+            }
+            break;
+        }
+        default:
+            break;
+        }
+
+    CppUnit::TextTestRunner runner;
+    runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
+
+    bool wasSuccessful = true;
+    string test = "";
+    int i = getopt.optind;
+    if (i == argc) {
+        // run them all
+        wasSuccessful = runner.run("");
+    }
+    else {
+        while (i < argc) {
+            if (debug) cerr << "Running " << argv[i] << endl;
+            test = GlobalMetadataStoreTest::suite()->getName().append("::").append(argv[i]);
+            wasSuccessful = wasSuccessful && runner.run(test);
+
+            ++i;
+        }
+    }
+
+    return wasSuccessful ? 0 : 1;
+}
