33bebc6ba4c62a1884cf2238856609809ba40fc6
James Gallagher <jgallagher@opendap.org>
2018-03-01 17:18:48 -0700

    Fixed an issue in the GatewayCache dtor - it was calling delete_instance()
    That should only happen once, when the process exits.

--- modules/gateway_module/GatewayCache.h
+++ modules/gateway_module/GatewayCache.h
@@ -1,3 +1,27 @@
+// -*- mode: c++; c-basic-offset:4 -*-
+
+// This file is part of gateway_module, A C++ module that can be loaded in to
+// the OPeNDAP Back-End Server (BES) and is able to handle remote requests.
+
+// Copyright (c) 2015 OPeNDAP, Inc.
+// Author: Nathan Potter <ndp@opendap.org>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+
 /*
  * GatewayCache.h
  *
@@ -8,12 +32,30 @@
 #ifndef MODULES_GATEWAY_MODULE_GATEWAYCACHE_H_
 #define MODULES_GATEWAY_MODULE_GATEWAYCACHE_H_
 
+#include "config.h"
+
 #include "BESFileLockingCache.h"
 
 namespace gateway
 {
 
-
+/**
+ * @brief A cache for data files accessed using the Gateway.
+ *
+ * This cache is a simple cache for data files implemented using
+ * advisory file locking on a POSIX file system (it is a specialization
+ * of BESFileLockingCache).
+ *
+ * This cache uses the following keys in the bes.conf file:
+ * - _Gateway.Cache.dir_: The directory where retrieved data files should be stored
+ * - _Gateway.Cache.prefix_: The item-name prefix for this cache
+ * - _Gateway.Cache.size_: The size of the cache. Limit the total amount of
+ *   data cached to this many MB. If zero, the cache size is unlimited.
+ *
+ * All of the keys must be defined for this cache (the BES uses several caches
+ * and some of them are optional - this cache is not optional).
+ *
+ */
 class GatewayCache: public BESFileLockingCache
 {
 private:
@@ -28,9 +70,7 @@ private:
     static string getCachePrefixFromConfig();
     static unsigned long getCacheSizeFromConfig();
 
-
 protected:
-
     GatewayCache(const string &cache_dir, const string &prefix, unsigned long long size);
 
 public:
@@ -41,8 +81,7 @@ public:
     static GatewayCache *get_instance(const string &cache_dir, const string &prefix, unsigned long long size);
     static GatewayCache *get_instance();
 
-
-	virtual ~GatewayCache();
+	virtual ~GatewayCache() { }
 };
 
 
