7c9b2040d35e11517c67ace558a68d3960a47313
Slav Korolev <slav.korolev@nasa.gov>
2018-04-28 09:33:42 -0400

    HYRAX-698. Mimic of h5dstoreinfo.c from chunks repository.

--- modules/dmrpp_module/unit-tests/DmrppChunkedReadTest.cc
+++ modules/dmrpp_module/unit-tests/DmrppChunkedReadTest.cc
@@ -24,6 +24,7 @@
 // You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
 
 #include <memory>
+#include <stdlib.h>
 
 #include <cppunit/TextTestRunner.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
@@ -68,10 +69,12 @@
 #include "HDF5RequestHandler.h"
 #include "h5get.h"
 #include "HDF5CF.h"
+#include "H5Dpublic.h"
 
 
 
 using namespace libdap;
+using namespace HDF5CF;
 
 static bool debug = false;
 
@@ -106,39 +109,247 @@ public:
 
     void test_chunked_hdf5()
     {
-        string filename = "/Users/vskorole/opendap/hyrax/bes/modules/dmrpp_module/data/dmrpp/chunked_fourD.h5";
-        BESDEBUG("dmrpp", "filename =  '" << filename<< "'" << endl);
+        BESDEBUG("dmrpp", "Use: ./h5dstoreinfo h5_file_name h5_dset_path." << endl);
+
+        string h5_file_name = "/Users/vskorole/opendap/hyrax/bes/modules/dmrpp_module/data/dmrpp/chunked_fourD.h5";
+        //TODO: How to get h5_dset_path?
+        string h5_dset_path = "d_16_chunks";
+        hid_t file = -1; /* handles */
+        hid_t dataset;
+
+        /* Will be used to store the chunking info. */
+        H5D_chunk_storage_info_t* chunk_st_ptr;
+        herr_t status;
+
+        unsigned int num_chunk_dims = 0;
+
+
         /*
          * Open the file and the dataset.
          */
-        DMR *dmr = new DMR();
-
-        // For the time being, separate CF file ID from the default file ID(mainly for debugging)
-        hid_t cf_fileid = -1;
-
-        H5Eset_auto2(H5E_DEFAULT,NULL,NULL);
-        D4BaseTypeFactory MyD4TypeFactory;
-        dmr->set_factory(&MyD4TypeFactory);
-
-        cf_fileid = H5Fopen(filename.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
-        if (cf_fileid < 0){
-            string invalid_file_msg="Could not open this HDF5 file ";
-            invalid_file_msg +=filename;
-            invalid_file_msg +=". It is very possible that this file is not an HDF5 file ";
-            invalid_file_msg +=" but with the .h5/.HDF5 suffix. Please check with the data";
-            invalid_file_msg +=" distributor.";
-            throw BESInternalError(invalid_file_msg,__FILE__,__LINE__);
+        file = H5_DLL::H5Fopen(h5_file_name.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+        if (file < 0) {
+            BESDEBUG("dmrpp", "HDF5 file %s cannot be opened successfully,check the file name and try again." << h5_file_name << endl);
         }
-        /*
-         * Get dataset rank and dimension.
-         */
-        bool include_attr = true;
-        H5GCFProduct product_type = check_product(cf_fileid);
-        GMPattern  gproduct_pattern = OTHERGMS;
-        HDF5CF::GMFile * f = NULL;
-        f = new HDF5CF::GMFile(filename.c_str(),cf_fileid,product_type,gproduct_pattern);
-        f->Retrieve_H5_Info(filename.c_str(),cf_fileid,include_attr);
 
+        dataset = H5_DLL::H5Dopen2(file, h5_dset_path.c_str(), H5P_DEFAULT);
+        if (dataset < 0) {
+            H5Fclose(file);
+            BESDEBUG("dmrpp", "HDF5 dataset %s cannot be opened successfully,check the dataset path and try again." << h5_dset_path);
+        }
+        uint8_t layout_type = 0;
+        uint8_t storage_status = 0;
+        int num_chunk = 0;
+        int i = 0;
+        int j = 0;
+
+        status = H5Dget_dataset_storage_info(dataset, &layout_type, (hsize_t*) &num_chunk, &storage_status);
+        if (status < 0) {
+            H5Dclose(dataset);
+            H5Fclose(file);
+            BESDEBUG("dmrpp", "Cannot get HDF5 dataset storage info. successfully." << endl);
+        }
+
+        if (storage_status == 0) {
+
+            hid_t dtype_id = H5Dget_type(dataset);
+            if (dtype_id < 0) {
+                H5Dclose(dataset);
+                H5Fclose(file);
+                BESDEBUG("dmrpp", "Cannot obtain the correct HDF5 datatype." << endl);
+            }
+            if (H5Tget_class(dtype_id) == H5T_INTEGER || H5Tget_class(dtype_id) == H5T_FLOAT) {
+                hid_t dcpl_id = H5Dget_create_plist(dataset);
+                if (dcpl_id < 0) {
+                    H5Dclose(dataset);
+                    H5Fclose(file);
+                    BESDEBUG("dmrpp", "Cannot obtain the HDF5 dataset creation property list." << endl);
+                }
+
+                H5D_fill_value_t fvalue_status;
+                if (H5Pfill_value_defined(dcpl_id, &fvalue_status) < 0) {
+                    H5Pclose(dcpl_id);
+                    H5Dclose(dataset);
+                    H5Fclose(file);
+                    BESDEBUG("dmrpp", "Cannot obtain the fill value status." << endl);
+                }
+
+                if (fvalue_status == H5D_FILL_VALUE_UNDEFINED) {
+                    if (layout_type == 1)
+                        BESDEBUG("dmrpp", " The storage size is 0 and the storge type is contiguous." << endl);
+                    else if (layout_type == 2)
+                        BESDEBUG("dmrpp", " The storage size is 0 and the storage type is chunking." << endl);
+                    else if (layout_type == 3) BESDEBUG("dmrpp", " The storage size is 0 and the storage type is compact." << endl);
+                    BESDEBUG("dmrpp", " The Fillvalue is undefined ." << endl);
+
+                }
+                else {
+
+                    if (layout_type == 1)
+                        BESDEBUG("dmrpp", " The storage size is 0 and the storage type is contiguous." << endl);
+                    else if (layout_type == 2)
+                        BESDEBUG("dmrpp", " The storage size is 0 and the storage type is chunking." << endl);
+                    else if (layout_type == 3) BESDEBUG("dmrpp", " The storage size is 0 and the storage type is compact." << endl);
+
+                    char *fvalue = NULL;
+                    size_t fv_size = H5Tget_size(dtype_id);
+                    if (fv_size == 1)
+                        fvalue = (char *) malloc(1);
+                    else if (fv_size == 2)
+                        fvalue = (char *) malloc(2);
+                    else if (fv_size == 4)
+                        fvalue = (char *) malloc(4);
+                    else if (fv_size == 8)
+                        fvalue = (char *) malloc(8);
+
+                    if (fv_size <= 8) {
+                        if (H5Pget_fill_value(dcpl_id, dtype_id, (void*) fvalue) < 0) {
+                            H5Pclose(dcpl_id);
+                            H5Dclose(dataset);
+                            H5Fclose(file);
+                            BESDEBUG("dmrpp", "Cannot obtain the fill value status." << endl);
+                        }
+
+                        if (H5Tget_class(dtype_id) == H5T_INTEGER) {
+
+                            H5T_sign_t fv_sign = H5Tget_sign(dtype_id);
+                            if (fv_size == 1) {
+                                if (fv_sign == H5T_SGN_NONE) {
+
+                                    BESDEBUG("dmrpp", "This dataset's datatype is unsigned char " << endl);
+                                    BESDEBUG("dmrpp", "and the fillvalue is " << *fvalue << endl);
+                                }
+                                else {
+                                    BESDEBUG("dmrpp", "This dataset's datatype is char ");
+                                    BESDEBUG("dmrpp", "and the fillvalue is " << *fvalue << endl);
+                                }
+                            }
+                            else if (fv_size == 2) {
+                                if (fv_sign == H5T_SGN_NONE) {
+                                    BESDEBUG("dmrpp", "This dataset's datatype is unsigned short ");
+                                    BESDEBUG("dmrpp", "and the fillvalue is " << *fvalue << endl);
+                                }
+                                else {
+                                    BESDEBUG("dmrpp", "This dataset's datatype is short ");
+                                    BESDEBUG("dmrpp", "and the fillvalue is " << *fvalue << endl);
+                                }
+                            }
+                            else if (fv_size == 4) {
+                                if (fv_sign == H5T_SGN_NONE) {
+                                    BESDEBUG("dmrpp", "This dataset's datatype is unsigned int ");
+                                    BESDEBUG("dmrpp", "and the fillvalue is " << *fvalue << endl);
+                                }
+                                else {
+                                    BESDEBUG("dmrpp", "This dataset's datatype is int ");
+                                    BESDEBUG("dmrpp", "and the fillvalue is " << *fvalue << endl);
+                                }
+                            }
+                            else if (fv_size == 8) {
+                                if (fv_sign == H5T_SGN_NONE) {
+                                    BESDEBUG("dmrpp", "This dataset's datatype is unsigned long long ");
+                                    BESDEBUG("dmrpp", "and the fillvalue is " << *fvalue << endl);
+                                }
+                                else {
+                                    BESDEBUG("dmrpp", "This dataset's datatype is long long ");
+                                    BESDEBUG("dmrpp", "and the fillvalue is " << *fvalue << endl);
+                                }
+                            }
+                        }
+                        if (H5Tget_class(dtype_id) == H5T_FLOAT) {
+                            if (fv_size == 4) {
+                                BESDEBUG("dmrpp", "This dataset's datatype is float ");
+                                BESDEBUG("dmrpp", "and the fillvalue is " << *fvalue << endl);
+                            }
+                            else if (fv_size == 8) {
+                                BESDEBUG("dmrpp", "This dataset's datatype is double ");
+                                BESDEBUG("dmrpp", "and the fillvalue is " << *fvalue << endl);
+                            }
+                        }
+
+                        if (fvalue != NULL) free(fvalue);
+
+                    }
+                    else
+                        BESDEBUG("dmrpp",
+                            "The size of the datatype is greater than 8 bytes, Use HDF5 API H5Pget_fill_value() to retrieve the fill value of this dataset." << endl);
+
+                }
+                H5Pclose(dcpl_id);
+
+            }
+            else {
+                if (layout_type == 1)
+                    BESDEBUG("dmrpp", " The storage size is 0 and the storage type is contiguous." << endl);
+                else if (layout_type == 2)
+                    BESDEBUG("dmrpp", " The storage size is 0 and the storage type is chunking." << endl);
+                else if (layout_type == 3) BESDEBUG("dmrpp", " The storage size is 0 and the storage type is compact." << endl);
+
+                BESDEBUG("dmrpp",
+                    "The datatype is neither float nor integer,use HDF5 API H5Pget_fill_value() to retrieve the fill value of this dataset." << endl);
+
+            }
+
+        }
+
+        else {
+            /* layout_type:  1 contiguous 2 chunk 3 compact */
+            if (layout_type == 1) {/* Contiguous storage */
+                haddr_t cont_addr = 0;
+                hsize_t cont_size = 0;
+                BESDEBUG("dmrpp", "Storage: contiguous" << endl);
+                if (H5Dget_dataset_contiguous_storage_info(dataset, &cont_addr, &cont_size) < 0) {
+                    H5Dclose(dataset);
+                    H5Fclose(file);
+                    BESDEBUG("dmrpp", "Cannot obtain the contiguous storage info." << endl);
+                }
+                BESDEBUG("dmrpp", "    Addr: " << cont_addr << endl);
+                BESDEBUG("dmrpp", "    Size: " << cont_size << endl);
+
+            }
+            else if (layout_type == 2) {/*chunking storage */
+                BESDEBUG("dmrpp", "storage: chunked." << endl);
+                BESDEBUG("dmrpp", "   Number of chunks is " << num_chunk << endl);
+
+                /* Allocate the memory for the struct to obtain the chunk storage information */
+                chunk_st_ptr = (H5D_chunk_storage_info_t*) calloc(num_chunk, sizeof(H5D_chunk_storage_info_t));
+                if (!chunk_st_ptr) {
+                    H5Dclose(dataset);
+                    H5Fclose(file);
+                    BESDEBUG("dmrpp", "Cannot allocate the memory to store the chunked storage info." << endl);
+                }
+
+                if (H5Dget_dataset_chunk_storage_info(dataset, chunk_st_ptr, &num_chunk_dims) < 0) {
+                    H5Dclose(dataset);
+                    H5Fclose(file);
+                    BESDEBUG("dmrpp", "Cannot get HDF5 chunk storage info. successfully." << endl);
+                }
+
+                BESDEBUG("dmrpp", "   Number of dimensions in a chunk is " << num_chunk_dims - 1 << endl);
+                for (int i = 0; i < num_chunk; i++) {
+                    BESDEBUG("dmrpp", "    Chunk index:  " << i << endl);
+                    BESDEBUG("dmrpp", "      Number of bytes: " << chunk_st_ptr[i].nbytes << endl);
+                    BESDEBUG("dmrpp", "      Logical offset: offset");
+                    for (int j = 0; j < num_chunk_dims - 1; j++)
+                        BESDEBUG("dmrpp", "[" << chunk_st_ptr[i].chunk_offset[j] << "]" << endl);
+                    BESDEBUG("dmrpp", "" << endl);
+                    BESDEBUG("dmrpp", "      Physical offset: " << chunk_st_ptr[i].chunk_addr << endl);
+                }
+                free(chunk_st_ptr);
+            }
+            else if (layout_type == 3) {/* Compact storage */
+                BESDEBUG("dmrpp", "Storage: compact" << endl);
+                size_t comp_size = 0;
+                if (H5Dget_dataset_compact_storage_info(dataset, &comp_size) < 0) {
+                    H5Dclose(dataset);
+                    H5Fclose(file);
+                    BESDEBUG("dmrpp", "Cannot obtain the compact storage info." << endl);
+                }
+                BESDEBUG("dmrpp", "   Size: " << comp_size << endl);
+
+            }
+        }
+        H5Dclose(dataset);
+        H5Fclose(file);
     }
 
     /**
