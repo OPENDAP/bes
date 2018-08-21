c947d763027fd26b91f186ebc1e41d99dbfb06e9
Nathan Potter <ndp@opendap.org>
2018-08-21 16:12:06 -0700

    hdf4 and hdf5 updates

--- dispatch/ShowNodeResponseHandler.cc
+++ dispatch/ShowNodeResponseHandler.cc
@@ -137,6 +137,11 @@ void ShowNodeResponseHandler::execute(BESDataHandlerInterface &dhi)
             }
         }
     }
+
+    if (catalog != datCatalogList->default_catalog()){
+        string name = BESUtil::assemblePath(catalog->get_catalog_name(),node->get_name(),true);
+        node->set_name(name);
+    }
     //---------------------------------------------------------------
 
     BESInfo *info = BESInfoList::TheList()->build_info();
