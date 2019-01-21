ff1c701d7390c53fded2160030e44039cad959e2
Nathan Potter <ndp@opendap.org>
2019-01-21 11:08:01 -0800

    Typo fixy

--- modules/fileout_covjson/FoDapCovJsonTransform.cc
+++ modules/fileout_covjson/FoDapCovJsonTransform.cc
@@ -1302,7 +1302,7 @@ void FoDapCovJsonTransform::transform(ostream *strm, libdap::DDS *dds, string in
     }
     else {
         // If this file can't be converted, then its failing spacial/temporal requirements
-        throw BESInternalError("File cannot be converted to COVJSON format due to missing or incompatible spacial dimensions", __FILE__, __LINE__);
+        throw BESInternalError("File cannot be converted to COVJSON format due to missing or incompatible spatial dimensions", __FILE__, __LINE__);
     }
 }
 
