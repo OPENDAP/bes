6b6ce74b5588b7e18197679c0d19c4d425ba985b
Muqun Yang <myang6@hdfgroup.org>
2019-09-13 09:22:50 -0500

    HFVHANDLER-257, update the nccopy shell script. Will handle this in another ticket.

--- bes-testsuite/filenetCDF.test/nc_copy_test/nccopy-test.sh
+++ bes-testsuite/filenetCDF.test/nc_copy_test/nccopy-test.sh
@@ -1,7 +1,6 @@
-nccopy -k 'classic' http://alpaca:8080/opendap/data/hdf5/hdf5-files/fake-files-for-handler/t_2d_2dll.nc4.h5
-nccopy -k 'netCDF-4 classic model' http://alpaca:8080/opendap/data/hdf5/hdf5-files/fake-files-for-handler/t_2d_2dll.nc4.h5 tt.nc4
-
-nccopy -k 'classic' http://alpaca:8080/opendap/data/hdf5/hdf5-files/fake-files-for-handler/t_2d_2dll.nc4.h5
-nccopy -k 'netCDF-4 classic model' http://alpaca:8080/opendap/data/hdf5/hdf5-files/fake-files-for-handler/t_2d_2dll.nc4.h5 tt.nc4
+nccopy -k 'classic' http://alpaca:8080/opendap/data/hdf5/hdf5-files/fake-files-for-handler/t_2d_2dll.nc4.h5 t_2d_2dll.nc
+nccopy -k 'netCDF-4 classic model' http://alpaca:8080/opendap/data/hdf5/hdf5-files/fake-files-for-handler/t_2d_2dll.nc4.h5 t_2d_2dll.nc4
+nccopy -k 'classic' http://alpaca:8080/opendap/data/hdf5/hdf5-files/fake-files-for-handler/grid_1_2d.h5 grid_1_2d.nc
+nccopy -k 'netCDF-4 classic model' http://alpaca:8080/opendap/data/hdf5/hdf5-files/fake-files-for-handler/grid_1_2d.h5 grid_1_2d.nc4
 nccopy -k 'classic' http://alpaca:8080/opendap/data/hdf5/hdf5-files/fake-files-for-handler/dim_scale.h5  dim_scale.nc
 nccopy -k 'netCDF-4 classic model' http://alpaca:8080/opendap/data/hdf5/hdf5-files/fake-files-for-handler/dim_scale.h5 dim_scale.nc4
