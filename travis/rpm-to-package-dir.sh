b18171da381fcd7369bf98ea67ecd901d03ca8f6
James Gallagher <jgallagher@opendap.org>
2019-10-29 16:52:27 -0600

    Extracted the code to trigger the hyrax-docker build to a script.

--- travis/rpm-to-package-dir.sh
+++ travis/rpm-to-package-dir.sh
@@ -12,14 +12,15 @@ DIST=${1}
 # 'prefix' and 'TRAVIS_BUILD_DIR' are in the environment
 
 # Copied the RPM files with version numbers.
-cp ${prefix}/rpmbuild/RPMS/x86_64/* ${TRAVIS_BUILD_DIR}/package/;
+cp ${prefix}/rpmbuild/RPMS/x86_64/* ${TRAVIS_BUILD_DIR}/package/
 
 # Now make a second set of copies with 'snapshot' in place of the version
-ver=`basename ${prefix}/rpmbuild/RPMS/x86_64/bes-[-0-9.]*.rpm | sed -e "s|bes-||g" -e "s|.static.${DIST}.x86_64.rpm||g"`;
-for file in ${prefix}/rpmbuild/RPMS/x86_64/*; do
-    echo "Updating BES ${DIST} snapshot with ${file}";
-    snap=`basename ${file} | sed "s|${ver}|snapshot|g"`;
-    cp ${file} ${TRAVIS_BUILD_DIR}/package/${snap};
+ver=`basename ${prefix}/rpmbuild/RPMS/x86_64/bes-[-0-9.]*.rpm | sed -e "s|bes-||g" -e "s|.static.${DIST}.x86_64.rpm||g"`
+for file in ${prefix}/rpmbuild/RPMS/x86_64/*
+do
+    echo "Updating BES ${DIST} snapshot with ${file}"
+    snap=`basename ${file} | sed "s|${ver}|snapshot|g"`
+    cp ${file} ${TRAVIS_BUILD_DIR}/package/${snap}
 done
 
 exit 0
