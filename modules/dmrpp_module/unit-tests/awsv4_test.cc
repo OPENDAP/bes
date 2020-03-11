5448eb26c166f4ec9aff4db2346d680510f15c85
James Gallagher <jgallagher@opendap.org>
2020-03-11 10:35:10 -0600

    Fixed awsv4_test unit tests - added example secret key back
    And added .gitallowed file at the top level of the bes to
    suppress false positive from git secrets. This key is not
    secret - it's used by AWS in the example code documenting
    how to form V4 signatures.

--- modules/dmrpp_module/unit-tests/awsv4_test.cc
+++ modules/dmrpp_module/unit-tests/awsv4_test.cc
@@ -122,7 +122,8 @@ namespace dmrpp {
             request_time = mktime(&t_info);
             if(debug) cerr << "request_time: " << request_time << endl;
             aws_key_id = "AKIDEXAMPLE";
-            aws_secret_key = "EXAMPLEKEY_IS_AN_EXAMPLEKEYEXAMPLEKEY_IS";
+            aws_secret_key = "wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY";
+            //                wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
 
             region = "us-east-1";
             serviceName = "service";
