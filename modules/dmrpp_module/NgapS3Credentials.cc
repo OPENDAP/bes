2800c925e6eadb5e90c9e1702fb66b03d19a6c6e
James Gallagher <jgallagher@opendap.org>
2020-03-11 17:54:51 -0600

    Fixes for distcheck - still work to do.

--- modules/dmrpp_module/NgapS3Credentials.cc
+++ modules/dmrpp_module/NgapS3Credentials.cc
@@ -1,6 +1,25 @@
+// -*- mode: c++; c-basic-offset:4 -*-
+
+// This file is part of the Hyrax data server.
+
+// Copyright (c) 2020 OPeNDAP, Inc.
+// Author: Nathan Potter <ndp@opendap.org>
 //
-// Created by ndp on 2/12/20.
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
 //
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
 
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
@@ -12,6 +31,7 @@
 #include "curl_utils.h"
 
 #include "NgapS3Credentials.h"
+
 using std::string;
 using std::endl;
 
@@ -19,7 +39,6 @@ using std::endl;
 #define AWS_SECRET_ACCESS_KEY_KEY "secretAccessKey"
 #define AWS_SESSION_TOKEN_KEY "sessionToken"
 #define AWS_EXPIRATION_KEY "expiration"
-
 
 #define MODULE "ngap"
 
