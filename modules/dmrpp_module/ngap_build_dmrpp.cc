6c52ba54fe6d2c563e8dd95e53620cb561da99dc
slloyd <slloyd@localhost>
2020-03-12 15:41:37 -0600

    3/12/20 - ngap-hyrax-92 - (In Progress) -o flag addition I

--- modules/dmrpp_module/ngap_build_dmrpp.cc
+++ modules/dmrpp_module/ngap_build_dmrpp.cc
@@ -875,6 +875,155 @@ int generate_dmrpp(const string &input_data_file, istream *dmr_istrm, const stri
 
 
 /**
+ * @brief Generates a dmrpp file using an input stream
+ *
+ * @param input_data_file The name of the data file to use
+ * @param dmr_istrm The input stream containing the dmr file
+ * @param url_name The path to the file
+ * @return
+ */
+int generate_dmrpp(const string &input_data_file, istream *dmr_istrm, const string &url_name, string output_filename){
+//FIXME implement output file
+    if (input_data_file.empty()) {
+        cerr << "HDF5 file name must be given (-f <input>)." << endl;
+        return 1;
+    }
+
+    int status=0;
+    hid_t file = 0;
+    try {
+        // Turn off automatic hdf5 error printing.
+        // See: https://support.hdfgroup.org/HDF5/doc1.8/RM/RM_H5E.html#Error-SetAuto2
+        //if (!verbose) H5Eset_auto2(H5E_DEFAULT, NULL, NULL);
+
+        // For a given HDF5, get info for all the HDF5 datasets in a DMR or for a
+        // given HDF5 dataset
+        if (dmr_istrm) {
+            // Get dmr:
+            unique_ptr<DMRpp> dmrpp(new DMRpp);
+            DmrppTypeFactory dtf;
+            dmrpp->set_factory(&dtf);
+
+            D4ParserSax2 parser;
+            parser.intern(*dmr_istrm, dmrpp.get(), false);
+
+            // Open the hdf5 file
+            file = H5Fopen(input_data_file.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+            if (file < 0) {
+                cerr << "Error: HDF5 file '" + input_data_file + "' cannot be opened." << endl;
+                return 1;
+            }
+
+            // iterate over all the variables in the DMR
+            get_chunks_for_all_variables(file, dmrpp->root());
+
+            XMLWriter writer;
+            dmrpp->print_dmrpp(writer, url_name);
+
+            if (!output_filename.empty()){
+                std::ofstream ofs (output_filename, std::ofstream::out);
+
+                ofs << writer.get_doc();
+
+                ofs.close();
+            }
+            else{
+                cout << writer.get_doc();
+            }
+        }
+        else {
+            bool found;
+            string bes_data_root;
+            try {
+                TheBESKeys::TheKeys()->get_value(ROOT_DIRECTORY, bes_data_root, found);
+                if (!found) {
+                    cerr << "Error: Could not find the BES root directory key." << endl;
+                    return 1;
+                }
+            }
+            catch (BESError &e) {
+                cerr << "BESError: " << e.get_message() << endl;
+                return 1;
+            }
+
+            // Use the values from the bes.conf file... jhrg 5/21/18
+            bes::DmrppMetadataStore *mds = bes::DmrppMetadataStore::get_instance();
+            if (!mds) {
+                cerr << "The Metadata Store (MDS) must be configured for this command to work." << endl;
+                return 1;
+            }
+
+            // Use the full path to open the file, but use the 'name' (which is the
+            // path relative to the BES Data Root) with the MDS.
+            // Changed this to utilze assmeblePath() because simply concatenating the strings
+            // is fragile. - ndp 6/6/18
+            string h5_file_path = BESUtil::assemblePath(bes_data_root,input_data_file);
+
+            //bes::DmrppMetadataStore::MDSReadLock lock = mds->is_dmr_available(input_data_file /*h5_file_path*/);
+            bes::DmrppMetadataStore::MDSReadLock lock = mds->is_dmr_available(h5_file_path, input_data_file, "h5");
+            if (lock()) {
+                // parse the DMR into a DMRpp (that uses the DmrppTypes)
+                unique_ptr<DMRpp> dmrpp(dynamic_cast<DMRpp*>(mds->get_dmr_object(input_data_file /*h5_file_path*/)));
+                if (!dmrpp.get()) {
+                    cerr << "Expected a DMR++ object from the DmrppMetadataStore." << endl;
+                    return 1;
+                }
+
+                // Open the hdf5 file
+                file = H5Fopen(h5_file_path.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+                if (file < 0) {
+                    cerr << "Error: HDF5 file '" + h5_file_path + "' cannot be opened." << endl;
+                    return 1;
+                }
+
+                get_chunks_for_all_variables(file, dmrpp->root());
+
+                dmrpp->set_href(url_name);
+
+                mds->add_dmrpp_response(dmrpp.get(), input_data_file /*h5_file_path*/);
+
+                XMLWriter writer;
+                dmrpp->set_print_chunks(true);
+                dmrpp->print_dap4(writer);
+
+                if (!output_filename.empty()){
+                    std::ofstream ofs (output_filename, std::ofstream::out);
+
+                    ofs << writer.get_doc();
+
+                    ofs.close();
+                }
+                else{
+                    cout << writer.get_doc();
+                }
+            }
+            else {
+                cerr << "Error: Could not get a lock on the DMR for '" + h5_file_path + "'." << endl;
+                return 1;
+            }
+        }
+    }
+    catch (BESError &e) {
+        cerr << "BESError: " << e.get_message() << endl;
+        status = 1;
+    }
+    catch (std::exception &e) {
+        cerr << "std::exception: " << e.what() << endl;
+        status = 1;
+    }
+    catch (...) {
+        cerr << "Unknown error." << endl;
+        status = 1;
+    }
+
+    H5Fclose(file);
+
+    return status;
+
+}
+
+
+/**
  * @brief Generates a dmrpp file
  *
  * @param input_data_file The name of the data file to use
@@ -901,6 +1050,39 @@ int generate_dmrpp(const string &input_data_file, const string &dmr_filename, co
     if(dmr_istrm)
         delete dmr_istrm;
 
+    return status;
+}
+
+/**
+ * @brief Generates a dmrpp file
+ *
+ * @param input_data_file The name of the data file to use
+ * @param dmr_filename The string of the dmr file
+ * @param url_name The path to the file
+ * @param output_filename The filename of the output file
+ * @return
+ */
+int generate_dmrpp(const string &input_data_file, const string &dmr_filename, const string &url_name, const string output_filename) {
+
+    ifstream *dmr_istrm=0;
+    int status;
+
+    if (!dmr_filename.empty()) {
+        dmr_istrm = new ifstream (dmr_filename.c_str());
+    }
+
+    try {
+        status = generate_dmrpp(input_data_file, dmr_istrm, url_name, output_filename);
+    }
+    catch(...){
+        if(dmr_istrm)
+            delete dmr_istrm;
+        throw;
+    }
+    if(dmr_istrm)
+        delete dmr_istrm;
+
+    return status;
 }
 
 
@@ -920,6 +1102,7 @@ int main(int argc, char*argv[]) {
     bool just_dmr = false;
     int status = 0;
     string input_data_file = "";
+    string output_data_file = "";
     string run_alternate = "";
     /* t = data_root
      * c = config file
@@ -933,7 +1116,7 @@ int main(int argc, char*argv[]) {
      * m = just_dmr
     */
 
-    GetOpt getopt(argc, argv, "t:c:f:u:bhvVmX");
+    GetOpt getopt(argc, argv, "t:c:f:u:o:bhvVmX");
     int option_char;
     while ((option_char = getopt()) != -1) {
         switch (option_char) {
@@ -949,6 +1132,9 @@ int main(int argc, char*argv[]) {
                 break;
             case 'f':
                 input_data_file = getopt.optarg;
+                break;
+            case 'o':
+                output_data_file = getopt.optarg;
                 break;
             case 'u':
                 url_name = getopt.optarg;
@@ -998,7 +1184,12 @@ int main(int argc, char*argv[]) {
 
         build_dmr_with_StandAloneApp(bes_conf_filename, bes_cmd_filename, dmr_filename);
 
-        status = generate_dmrpp(input_data_file, dmr_filename, url_name);
+        if (output_data_file.empty()){
+            status = generate_dmrpp(input_data_file, dmr_filename, url_name);
+        }
+        else{
+            status = generate_dmrpp(input_data_file, dmr_filename, url_name, output_data_file);
+        }
     }
     else {
         /////////////////////////////////////////////////////////////////////////////////////////////////////
