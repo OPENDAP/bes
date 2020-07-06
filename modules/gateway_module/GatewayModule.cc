b05d676b8591ac2c235b9d8f8397fe2fdd2b5ee1
ndp-opendap <ndp@opendap.org>
2020-07-06 13:29:53 -0700

    gateway almost converted to use nbes/http problems remain in the ncml_handler gateway aggregations.

--- modules/gateway_module/GatewayModule.cc
+++ modules/gateway_module/GatewayModule.cc
@@ -28,21 +28,15 @@
 
 #include <iostream>
 #include <vector>
-#include <string>
 
 #include <BESRequestHandlerList.h>
 #include <BESDebug.h>
 #include <BESResponseHandlerList.h>
-#include <BESResponseNames.h>
 #include <BESContainerStorageList.h>
-#include <TheBESKeys.h>
-#include <BESSyntaxUserError.h>
 
 #include "GatewayModule.h"
 #include "GatewayRequestHandler.h"
-#include "GatewayResponseNames.h"
 #include "GatewayContainerStorage.h"
-#include "GatewayUtils.h"
 #include "GatewayPathInfoResponseHandler.h"
 #include "GatewayPathInfoCommand.h"
 
@@ -59,8 +53,9 @@ void GatewayModule::initialize(const string &modname)
     BESDEBUG(modname, "    adding " << modname << " container storage" << endl);
     BESContainerStorageList::TheList()->add_persistence(new GatewayContainerStorage(modname));
 
-    BESDEBUG(modname, "    initialize the gateway utilities and params" << endl);
-    GatewayUtils::Initialize();
+    // BESDEBUG(modname, "    initialize the gateway utilities and params" << endl);
+    // THis called used to set up HTTP cache proxies, etc now that's done in http - ndp 7/6/20
+    // GatewayUtils::Initialize();
 
     BESDEBUG(modname, "    adding Gateway debug context" << endl);
     BESDebug::Register(modname);
--- modules/gateway_module/GatewayNames.h
+++ modules/gateway_module/GatewayNames.h
@@ -27,19 +27,9 @@
 #ifndef E_GatewayResponseNames_H
 #define E_GatewayResponseNames_H 1
 
-#define Gateway_NAME "gateway"
+#define GATEWAY_MODULE "gateway"
+#define GATEWAY_MODULE_VERSION "0.2.0"
 
-// These are the names of the be keys used to configure the handler.
-
-#define Gateway_WHITELIST "Gateway.Whitelist"
-#define Gateway_MIMELIST "Gateway.MimeTypes"
-#define Gateway_PROXYPROTOCOL "Gateway.ProxyProtocol"
-#define Gateway_PROXYHOST "Gateway.ProxyHost"
-#define Gateway_PROXYPORT "Gateway.ProxyPort"
-#define Gateway_PROXYAUTHTYPE "Gateway.ProxyAuthType"
-#define Gateway_PROXYUSER "Gateway.ProxyUser"
-#define Gateway_PROXYPASSWORD "Gateway.ProxyPassword"
-#define Gateway_PROXYUSERPW "Gateway.ProxyUserPW"
-#define Gateway_USE_INTERNAL_CACHE "Gateway.UseInternalCache"
+#define MODULE GATEWAY_MODULE
 
 #endif // E_GatewayResponseNames_H
