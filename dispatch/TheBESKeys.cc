1a95546eedfb3642e18527ed4f1203f0fe2b8e04
ndp-opendap <ndp@opendap.org>
2020-09-01 14:37:55 -0700

    Repaired keysT and sorted out regex for EEDTEST

--- dispatch/TheBESKeys.cc
+++ dispatch/TheBESKeys.cc
@@ -143,6 +143,8 @@ void TheBESKeys::initialize_keys()
 {
     kvp::load_keys(d_keys_file_name, d_ingested_key_files, *d_the_keys);
     *d_the_original_keys = *d_the_keys;
+    BESDEBUG(MODULE, prolog << "         d_the_keys.size(): " << d_the_keys->size() << endl);
+    BESDEBUG(MODULE, prolog << "d_the_original_keys.size(): " << d_the_original_keys->size() << endl);
 }
 
 void TheBESKeys::clean()
