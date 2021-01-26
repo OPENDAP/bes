7788eba61751b9255c57d399a4320ba8badd343f
James Gallagher <jgallagher@opendap.org>
2021-01-25 17:14:43 -0700

    Removed the old sidecar builder which used an old libdap::Error ctor.

--- functions/stare/StareFunctions.h
+++ functions/stare/StareFunctions.h
@@ -53,8 +53,8 @@ std::string get_sidecar_file_pathname(const std::string &pathName, const string 
 void get_sidecar_int32_values(hid_t file, const std::string &variable, std::vector<libdap::dods_int32> &values);
 void get_sidecar_uint64_values(hid_t file, const std::string &variable, std::vector<libdap::dods_uint64> &values);
 
-bool target_in_dataset(const std::vector<libdap::dods_uint64> &targetIndices,
-        const std::vector<libdap::dods_uint64> &dataStareIndices);
+bool target_in_dataset(const std::vector<libdap::dods_uint64> &target_indices,
+        const std::vector<libdap::dods_uint64> &data_stare_indices);
 unsigned int count(const std::vector<libdap::dods_uint64> &target_indices,
         const std:: vector<libdap::dods_uint64> &dataset_indices, bool all_target_matches = false);
 
