03a91bd074508d3c6872476fae7b2ec69e86157d
James Gallagher <jgallagher@opendap.org>
2021-12-06 22:32:08 -0700

    Moved new Regex code into dispatch

--- dispatch/BESRegex.cc
+++ dispatch/BESRegex.cc
@@ -1,11 +1,11 @@
-// BESRegex.cc
 
-// This file is part of bes, A C++ back-end server implementation framework
-// for the OPeNDAP Data Access Protocol.
+// -*- mode: c++; c-basic-offset:4 -*-
 
-// Copyright (c) 2004-2009 University Corporation for Atmospheric Research
-// Author: Patrick West <pwest@ucar.edu> and Jose Garcia <jgarcia@ucar.edu>
-// and James Gallagher <jgallagher@gso.uri.edu>
+// This file is part of libdap, A C++ implementation of the OPeNDAP Data
+// Access Protocol.
+
+// Copyright (c) 2005 OPeNDAP, Inc.
+// Author: James Gallagher <jgallagher@opendap.org>
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -21,106 +21,52 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 //
-// You can contact University Corporation for Atmospheric Research at
-// 3080 Center Green Drive, Boulder, CO 80301
- 
-// (c) COPYRIGHT University Corporation for Atmospheric Research 2004-2005
-// Please read the full copyright statement in the file COPYRIGHT_UCAR.
-//
-// Authors:
-//      pwest       Patrick West <pwest@ucar.edu>
-//      jgarcia     Jose Garcia <jgarcia@ucar.edu>
-//      jimg        James Gallagher <jgallagher@gso.uri.edu>
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
 
-#include <config.h>
-
-#ifndef WIN32
-#include <alloca.h>
-#endif
- 
-#include <sys/types.h>
-#include <regex.h>
-
-#include <cstdlib>
-#include <new>
-#include <string>
-#include <stdexcept>
+#include "config.h"
 
 #include "BESRegex.h"
-#include "BESInternalError.h"
-#include "BESScrub.h"
+#include "Error.h"
 
 using namespace std;
 
-void
-BESRegex::init(const char *t)
-{
-    d_pattern = t;
-    d_preg = static_cast<void*>(new regex_t);
-    int result = regcomp(static_cast<regex_t*>(d_preg), t, REG_EXTENDED);
+namespace libdap {
 
-    if (result != 0) {
-        size_t msg_len = regerror(result, static_cast<regex_t*>(d_preg), static_cast<char*>(NULL),
-            static_cast<size_t>(0));
-        char *msg = new char[msg_len + 1];
-        regerror(result, static_cast<regex_t*>(d_preg), msg, msg_len);
-        string err = string("BESRegex error: ") + string(msg);
-        BESInternalError e(err, __FILE__, __LINE__);
-        delete[] msg;
-        throw e;
-    }
-}
-
-BESRegex::~BESRegex()
-{
-    regfree(static_cast<regex_t*>(d_preg));
-    delete static_cast<regex_t*>(d_preg); d_preg = 0;
-
-}
-
-/** Initialize a POSIX regular expression (using the 'extended' features).
-
-    @param t The regular expression pattern. */
-BESRegex::BESRegex(const char* t)
-{
-    init(t);
-}
-
-/** Compatability ctor.
-    @see BESRegex::BESRegex(const char* t) */
-BESRegex::BESRegex(const char* t, int)
-{
-    init(t);
-}
-
-/** @brief Does the regular expression match the string?
-
-    Warning : this function can be used to match strings of zero length
-   	if the regex pattern accepts empty strings. Therefore this function
-   	returns -1 if the pattern does not match.
+/** Does the regular expression match the string? 
 
     @param s The string
     @param len The length of string to consider
     @param pos Start looking at this position in the string
     @return The number of characters that match, -1 if there's no match. */
 int 
-BESRegex::match(const char* s, int len, int pos)
+Regex::match(const char *s, int len, int pos) const
 {
-    // TODO re-implement using auto_ptr or unique_ptr. jhrg 7/27/18
-    regmatch_t *pmatch = new regmatch_t[len+1];
-    string ss = s;
+    if (pos > len)
+        throw Error("Position exceed length in Regex::match()");
 
-    int result = regexec(static_cast<regex_t*>(d_preg), 
-                         ss.substr(pos, len-pos).c_str(), len, pmatch, 0);
-	int matchnum;
-    if (result == REG_NOMATCH)
-        matchnum = -1; //returns -1 due to function being able to match strings of 0 length
-	else
-		matchnum = pmatch[0].rm_eo - pmatch[0].rm_so;
-		
-	delete[] pmatch; pmatch = 0;
+    smatch match;
+    auto target = string(s+pos, len-pos);
+    bool found = regex_search(target, match, d_exp);
+    if (found)
+        return (int)match.length();
+    else
+        return -1;
+}
 
-    return matchnum;
+/**
+ * @brief Search for a match to the regex
+ * @param s The target for the search
+ * @return The length of the matching substring, or -1 if no match was found
+ */
+int
+Regex::match(const string &s) const
+{
+    smatch match;
+    bool found = regex_search(s, match, d_exp);
+    if (found)
+        return (int)match.length();
+    else
+        return -1;
 }
 
 /** Does the regular expression match the string? 
@@ -131,43 +77,41 @@ BESRegex::match(const char* s, int len, int pos)
     value-result parameter.
     @param pos Start looking at this position in the string
     @return The start position of the first match. This is different from 
-    POSIX regular expressions, which return the start position of the
+    POSIX regular expressions, whcih return the start position of the 
     longest match. */
 int 
-BESRegex::search(const char* s, int len, int& matchlen, int pos)
+Regex::search(const char *s, int len, int& matchlen, int pos) const
 {
-	// sanitize allocation
-    if (!BESScrub::size_ok(sizeof(regmatch_t), len+1))
-    	return -1;
-    	
-    // alloc space for len matches, which is theoretical max.
-    // Problem: If somehow 'len' is very large - say the size of a 32-bit int,
-    // then len+1 is a an integer overflow and this might be exploited by
-    // an attacker. It's not likely there will be more than a handful of
-    // matches, so I am going to limit this value to 32766. jhrg 3/4/09
-    if (len > 32766)
-    	return -1;
-
-    regmatch_t *pmatch = new regmatch_t[len+1];
-    string ss = s;
-     
-    int result = regexec(static_cast<regex_t*>(d_preg),
-                         ss.substr(pos, len-pos).c_str(), len, pmatch, 0);
-    if (result == REG_NOMATCH) {
-        delete[] pmatch; pmatch = 0;
+    smatch match;
+    // This is needed because in C++14, the first arg to regex_search() cannot be a
+    // temporary string. It seems the C++11 compilers on some linux dists are using
+    // regex headers that enforce c++14 rules. jhrg 12/2/21
+    auto target = string(s+pos, len-pos);
+    bool found = regex_search(target, match, d_exp);
+    matchlen = (int)match.length();
+    if (found)
+        return (int)match.position();
+    else
         return -1;
-    }
-
-    // Match found, find the first one (pmatch lists the longest first)
-    int m = 0;
-    for (int i = 1; i < len; ++i)
-        if (pmatch[i].rm_so != -1 && pmatch[i].rm_so < pmatch[m].rm_so)
-            m = i;
-            
-    matchlen = pmatch[m].rm_eo - pmatch[m].rm_so;
-    int matchpos = pmatch[m].rm_so;
-    
-    delete[] pmatch; pmatch = 0;
-    return matchpos;
 }
+
+/**
+ * @brief Search for a match to the regex
+ * @param s The target for the search
+ * @param matchlen The number of characters that matched
+ * @return The starting position of the first set of matching characters
+ */
+int
+Regex::search(const string &s, int& matchlen) const
+{
+    smatch match;
+    bool found = regex_search(s, match, d_exp);
+    matchlen = (int)match.length();
+    if (found)
+        return (int)match.position();
+    else
+        return -1;
+}
+
+} // namespace libdap
 
--- dispatch/BESRegex.h
+++ dispatch/BESRegex.h
@@ -1,11 +1,11 @@
-// BESRegex.h
 
-// This file is part of bes, A C++ back-end server implementation framework
-// for the OPeNDAP Data Access Protocol.
+// -*- mode: c++; c-basic-offset:4 -*-
 
-// Copyright (c) 2004-2009 University Corporation for Atmospheric Research
-// Author: Patrick West <pwest@ucar.edu> and Jose Garcia <jgarcia@ucar.edu>
-// and James Gallagher <jgallagher@gso.uri.edu>
+// This file is part of libdap, A C++ implementation of the OPeNDAP Data
+// Access Protocol.
+
+// Copyright (c) 2005 OPeNDAP, Inc.
+// Author: James Gallagher <jgallagher@opendap.org>
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -21,46 +21,64 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 //
-// You can contact University Corporation for Atmospheric Research at
-// 3080 Center Green Drive, Boulder, CO 80301
- 
-// (c) COPYRIGHT University Corporation for Atmospheric Research 2004-2005
-// Please read the full copyright statement in the file COPYRIGHT_UCAR.
-//
-// Authors:
-//      pwest       Patrick West <pwest@ucar.edu>
-//      jgarcia     Jose Garcia <jgarcia@ucar.edu>
-//      jimg        James Gallagher <jgallagher@gso.uri.edu>
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
 
-#ifndef _BESRegex_h
-#define _BESRegex_h 1
+#ifndef _Regex_h
+#define _Regex_h 1
 
-/** a C++ interface to POSIX regular expression functions.
+#include <string>
+#include <regex>
 
-    @author James Gallagher <jgallagher@opendap.org> */
-class BESRegex
+namespace libdap
+{
+
+/**
+ * @brief Regular expression matching
+ *
+ * This class provides an interface that mimics the libgnu C++ library
+ * that was used with the first version of libdap (c. 1993). It can been
+ * re-implemented several times, this last time using the C++-11 regex
+ * class. We found this was faster than the unix regex_t (man(3)) that
+ * was being used.
+ *
+ * @note Make sure to compile the regular expressions only when really
+ * needed (e.g., make Regex instances const, etc., when possible) since
+ * it is an expensive operation
+ *
+ * @author James Gallagher <jgallagher@opendap.org>
+ */
+class Regex
 {
 private:
-    // d_preg was a regex_t* but I needed to include both regex.h and config.h
-    // to make the gnulib code work. Because this header is installed (and is
-    // used by other libraries) it cannot include config.h, so I moved the 
-    // regex.h and config.h (among other) includes to the implementation. It
-    // would be cleaner to use a special class, but for one field that seems
-    // like overkill.
-    void *d_preg;
+    std::regex d_exp;
     std::string d_pattern;
-    void init(const char *t);
+
+    void init(const char *s) { d_exp = std::regex(s); d_pattern = s; }
+    void init(const std::string &s) { d_exp = std::regex(s); d_pattern = s; }
     
 public:
-    BESRegex(const char *t);
-    BESRegex(const char *t, int dummy);
-    ~BESRegex();
+    /// @brief initialize a Regex with a C string
+    explicit Regex(const char *s) { init(s); }
+    /// @deprecated
+    Regex(const char *s, int) { init(s); }
+    /// @brief nitialize a Regex with a C++ string
+    explicit Regex(const std::string &s) { init(s); }
 
-    /// Does the pattern match.
-    int match(const char* s, int len, int pos = 0);
-    /// How much of the string does the pattern matche.
-    int search(const char* s, int len, int& matchlen, int pos = 0);
-    std::string pattern(){ return d_pattern; }
+    ~Regex() = default;
+
+    std::string pattern() const { return d_pattern; }
+
+    /// @brief Does the pattern match.
+    int match(const char *s, int len, int pos = 0) const;
+    /// @brief Does the pattern match.
+    int match(const std::string &s) const;
+
+    /// @brief How much of the string does the pattern match.
+    int search(const char *s, int len, int &matchlen, int pos = 0) const ;
+    /// @brief How much of the string does the pattern match.
+    int search(const std::string &s, int &matchlen) const;
 };
+
+} // namespace libdap
 
 #endif
