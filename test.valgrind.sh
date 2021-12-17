6883ca24442f56539b3040745c02a1a3fea34c3f
Kent Yang <myang6@hdfgroup.org>
2021-12-17 16:45:33 -0600

    Correct minor errors in the script

--- test.valgrind.sh
+++ test.valgrind.sh
@@ -3,7 +3,7 @@ echo ” CF DMR valgrind Test“
 cd bes-testsuite
 /bin/cp -f hdf5_handlerTest.valgrind.at hdf5_handlerTest.at
 cd ..
-make check
+make check -j 8
 /bin/cp -f /tmp/h5.cf.dmr.valgrind.log tt.log
 /bin/bash ./test_valgrind_unit.sh
 status=$?
@@ -20,7 +20,7 @@ echo “NASA DAP2 valgrind test”
 cd bes-testsuite
 /bin/cp -f hdf5_handlerTest.valgrind.nasa.at hdf5_handlerTest.at
 cd ..
-make check
+make check -j 8
 /bin/cp -f /tmp/h5.valgrind.nasa.log tt.log
 /bin/bash ./test_valgrind_unit.sh
 status=$?
@@ -37,7 +37,7 @@ echo “NASA CF DMR valgrind test”
 cd bes-testsuite
 cp hdf5_handlerTest.valgrind.nasa.cfdmr.at hdf5_handlerTest.at
 cd ..
-make check
+make check -j 8
 /bin/cp -f /tmp/h5.valgrind.nasa.log tt.log
 /bin/bash ./test_valgrind_unit.sh
 status=$?
@@ -51,9 +51,9 @@ fi
 
 echo “NASA Default DAP4 valgrind test”
 cd bes-testsuite
-/bin/cp -f hdf5_handlerTest.valgrind.nasa.deault.dap4.at hdf5_handlerTest.at
+/bin/cp -f hdf5_handlerTest.valgrind.nasa.default.dap4.at hdf5_handlerTest.at
 cd ..
-make check
+make check -j 8
 /bin/cp -f /tmp/h5.valgrind.nasa.default.dap4.log tt.log
 /bin/bash ./test_valgrind_unit.sh
 status=$?
@@ -65,11 +65,11 @@ else
 fi
 /bin/rm -rf /tmp/h5.valgrind.nasa.default.dap4.log 
 
-echo” CF DAP2 to DMR valgrind Test“
+echo ” CF DAP2 to DMR valgrind Test“
 cd bes-testsuite
 /bin/cp -f hdf5_handlerTest.dap2dmr.valgrind.at hdf5_handlerTest.at
 cd ..
-make check
+make check -j 8
 /bin/cp -f /tmp/h5.valgrind.log tt.log
 /bin/bash ./test_valgrind_unit.sh
 status=$?
@@ -82,11 +82,11 @@ fi
 
 /bin/rm -rf /tmp/h5.valgrind.log
 
-echo” NASA CF DMR ONLY valgrind Test“
+echo ” NASA CF DMR ONLY valgrind Test“
 cd bes-testsuite
 /bin/cp -f hdf5_handlerTest.dap2dmr.valgrind.at hdf5_handlerTest.at
 cd ..
-make check
+make check -j 8
 /bin/cp -f /tmp/h5.valgrind.log tt.log
 /bin/bash ./test_valgrind_unit.sh
 status=$?
