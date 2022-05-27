694cf33173532b05da67a93b45b08d7a110526f8
ndp-opendap <ndp@opendap.org>
2022-05-27 15:34:08 -0400

    Refining AWSCheck

--- modules/dmrpp_module/tests_build_dmrpp/chkForAWS.sh
+++ modules/dmrpp_module/tests_build_dmrpp/chkForAWS.sh
@@ -2,22 +2,28 @@
 #
 # Checks to see if the AWS CLI is present and configured with credentials.
 #
+aws configure list > /dev/null
+ret=$?
+if test $ret != 0 ; then
+    echo "The AWS CLI is not available. AWS tests will not be run."
+    exit $ret;
+fi
 aws configure list | grep "access_key" | grep -v "<not set>" > /dev/null
 ret=$?
 if test $ret != 0 ; then
-    echo "ERROR - The AWS_ACCESS_KEY_ID is not set."
+    echo "The AWS_ACCESS_KEY_ID is not set. AWS tests will not be run."
     exit $ret;
 fi
 aws configure list | grep "secret_key" | grep -v "<not set>" > /dev/null
 ret=$?
 if test $ret != 0 ; then
-    echo "ERROR - The AWS_SECRET_ACCESS_KEY is not set."
+    echo "The AWS_SECRET_ACCESS_KEY is not set. AWS tests will not be run."
     exit $ret;
 fi
 aws configure list | grep "region" | grep -v "<not set>" > /dev/null
 ret=$?
 if test $ret != 0 ; then
-    echo "WARNING - The AWS_DEFAULT_REGION is not set."
+    echo "The AWS_DEFAULT_REGION is not set."
     exit 0;
 fi
 
