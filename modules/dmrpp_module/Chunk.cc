b81f49f9ec599cc410e7defacecc251c966cdabd
myang6 <myang6@hdfgroup.org>
2022-08-05 14:20:27 -0400

    HYRAX-771, fix bugs in the new code but sst still cannot be decompressed

--- modules/dmrpp_module/Chunk.cc
+++ modules/dmrpp_module/Chunk.cc
@@ -649,6 +649,9 @@ void Chunk::filter_chunk(const string &filters, unsigned long long chunk_size, u
     unsigned long long out_buf_size = 0;
     unsigned long long in_buf_size = 0;
     char**destp = nullptr;
+    char* dest = nullptr;
+    char* tmp_buf = nullptr;
+    char* tmp_dest = nullptr;
     for (auto i = filter_array.rbegin(), e = filter_array.rend(); i != e; ++i) {
 
         string filter = *i;
@@ -657,62 +660,47 @@ void Chunk::filter_chunk(const string &filters, unsigned long long chunk_size, u
 
             
             if (num_deflate > 1) {
- 
-                char*tmp_buf = nullptr;
-                char *dest = new char[chunk_size];
+
+                dest = new char[chunk_size];
                 try {
+                    destp = &dest;
                     if (deflate_index == 0) {
-cerr<<"Coming here i 0"<<endl;
-                        destp = &dest;
                         out_buf_size = inflate(destp, chunk_size, get_rbuf(), get_rbuf_size());
+cerr<<"index 0: input buf size is "<<get_rbuf_size() <<endl;
+                        tmp_dest = *destp;
                     }
                     else {
-cerr<<"Coming here i 1"<<endl;
-                        tmp_buf =*destp;
-                        char *tmp_buf2 = new char[in_buf_size];
-                        memcpy((void*) tmp_buf2,(void*)tmp_buf,in_buf_size);
-cerr<<"in_buf_size is "<<in_buf_size <<endl;
-                        destp = &dest;
-cerr<<"Coming here i 1 before inflate"<<endl;
-                        out_buf_size = inflate(destp, chunk_size, tmp_buf2, in_buf_size);
-cerr<<"Coming here i 1 after inflate()"<<endl;
-                        delete[] tmp_buf2;
+cerr<<"index 1: input buf size is "<<in_buf_size <<endl;
+                        tmp_buf = tmp_dest;
+                        out_buf_size = inflate(destp, chunk_size, tmp_buf, in_buf_size);
+cerr<<"out buf size is "<<out_buf_size <<endl;
+                        tmp_dest = *destp;
                         delete[] tmp_buf;
                     }
                     deflate_index ++;
                     in_buf_size = out_buf_size;
-cerr<<"in_buf_size outside is "<<in_buf_size <<endl;
 #if DMRPP_USE_SUPER_CHUNKS
-                //set_read_buffer(dest, chunk_size, chunk_size, true);
-
-                //set_read_buffer(new_dest, out_buf_size, chunk_size, true);
-                //set_read_buffer(new_dest, chunk_size, chunk_size, true);
-                //set_read_buffer(new_dest, out_buf_size, out_buf_size, true);
-
                     // This is the last deflate filter, output the buffer.
                     if (deflate_index == num_deflate) {
-cerr<<"Coming to the final stop" <<endl;
                         char* newdest = *destp;
                         set_read_buffer(newdest, chunk_size, chunk_size, true);
-cerr<<"After the final stop" <<endl;
                     }
-                    else 
-                        delete[] dest;
+ 
 #else
                 set_rbuf(dest, chunk_size);
 #endif
 
                 }
                 catch (...) {
-                    //delete[] dest;
-                    //delete[] tmp_buf;
+                    delete[] dest;
+                    delete[] tmp_buf;
                     throw;
                 }
  
 
             }
             else {
-                char *dest = new char[chunk_size];
+                dest = new char[chunk_size];
                 destp = &dest;
                 try {
                     if (inflate(destp, chunk_size, get_rbuf(), get_rbuf_size()) <0) {
