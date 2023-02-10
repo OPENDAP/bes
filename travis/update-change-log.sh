867fa1865585e4a5e185f373e8c15b5308f4d7de
ndp-opendap <ndp@opendap.org>
2023-02-10 09:05:36 -0800

    I think this fixes the duplicate entries issue.

--- travis/update-change-log.sh
+++ travis/update-change-log.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-
-export BES_VERSION=$(cat ./bes_VERSION)
-echo "BES_VERSION: ${BES_VERSION}"
-BES_VERSION="${BES_VERSION}-${BES_BUILD_NUMBER}"
-echo "BES_VERSION: ${BES_VERSION}"
-
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Figure out the new ChangeLog content (if any)
+#
 function get_change_log_update_text() {
+    echo "#--------------------------------------------------" >&2
+    echo "# BEGIN: get_change_log_update_text()" >&2
 
     local bes_version_numbers=${1:-"NO_VERSION_FOUND"}
     echo "# bes_version_numbers: ${bes_version_numbers}" >&2
@@ -22,9 +22,6 @@ function get_change_log_update_text() {
 
     # Make the log lines 72 chars max.
     update_text=$(echo "${update_text}" | fold -s -w 72 | awk '{if(!match($0,"^[0-9]|^[\\t]")){printf("\t%s\n",$0);}else{print $0;}}' - )
-    echo "# update_text: " >&2
-    echo "${update_text}" | awk '{print "##    "$0;}' >&2
-
     echo "#--------------------------------------------------" >&2
     echo "# Checking for duplicates" >&2
     local dups=$( echo "${update_text}" | grep -n "${most_recent}";)
@@ -34,31 +31,68 @@ function get_change_log_update_text() {
         # Find most recent dup.
         local last_entry=$( echo "${dups}" | head -n 1 | awk '{split($1,a,":");print a[1]-1;}' ;)
         echo "# last_entry: ${last_entry}" >&2
-        # Trim the update so that it ends before the most recent duplicate.
-        update_text=$(echo "${update_text}" | head -n ${last_entry})
+        if test ${last_entry} -ne 0;
+        then
+            # Trim the update so that it ends before the most recent duplicate.
+            update_text=$(echo "${update_text}" | head -n ${last_entry})
+        else
+            update_text=
+        fi
     else
         echo "# No duplicates found." >&2
     fi
     echo "#--------------------------------------------------" >&2
+    echo "# update_text: " >&2
+    echo "${update_text}" | awk '{print "##    "$0;}' >&2
 
-    echo "## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --"
-    echo "##"
-    echo "##  bes-${bes_version_numbers}"
-    #echo "##  hyrax-${hyrax_version_numbers}"
-    echo "##"
-    echo ""
-    echo "${update_text}"
-    echo ""
-
+    if test -n "${update_text}"
+    then
+        echo "## -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --"
+        echo "##"
+        echo "##  bes-${bes_version_numbers}"
+        #echo "##  hyrax-${hyrax_version_numbers}"
+        echo "##"
+        echo ""
+        echo "${update_text}"
+        echo ""
+    fi
 }
 
-change_log_update=$(get_change_log_update_text "${BES_VERSION}";)
-tmp_file=$(mktemp)
-echo "${change_log_update}" > "${tmp_file}"
-cat ChangeLog >> "${tmp_file}"
-mv "${tmp_file}" ChangeLog
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#  main()
+#
+#  * Gets the ChangeLog update from "get_change_log_update_text BES_VERSION"
+#  * If there is a non empty update then the ChangeLog file is updated.
+#  * Commits the changes to git on the current branch with a [skip ci] message.
+#  * Pushes the result to GitHub.
+#
+function main() {
+    echo "##################################################################################################" >&2
+    echo "#" >&2
+    echo "# BEGIN: $0" >&2
+    echo "#" >&2
+    #
+    export TRAVIS_BRANCH=${TRAVIS_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
+    echo "#    TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
+    #
+    export BES_BUILD_NUMBER=${BES_BUILD_NUMBER:-"<TEST>"}
+    echo "# BES_BUILD_NUMBER: ${BES_BUILD_NUMBER}" >&2
+    #
+    export BES_VERSION=$(cat ./bes_VERSION)
+    BES_VERSION="${BES_VERSION}-${BES_BUILD_NUMBER}"
+    echo "#      BES_VERSION: ${BES_VERSION}"
+    echo "#" >&2
 
 
-
-
-
+    change_log_update=$(get_change_log_update_text "${BES_VERSION}";)
+    if test -n "${change_log_update}"
+    then
+        tmp_file=$(mktemp)
+        echo "${change_log_update}" > "${tmp_file}"
+        cat ChangeLog >> "${tmp_file}"
+        mv "${tmp_file}" ChangeLog
+        git checkout "${TRAVIS_BRANCH}"
+        #git commit -m "TheRobotTravis updated ChangeLog [skip ci]" ChangeLog
+        #git push
+    fi
+}
