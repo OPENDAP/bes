755f4adb31c3916de155141e145eb2ff7559cc58
Muqun Yang <myang6@hdfgroup.org>
2023-02-21 12:24:10 -0600

    HYRAX-955, add a big array shuffle+deflate test.

--- modules/hdf5_handler/data.nasa/download_dap4.sh
+++ modules/hdf5_handler/data.nasa/download_dap4.sh
@@ -96,6 +96,9 @@ $GET https://gamma.hdfgroup.org/ftp/pub/outgoing/opendap/data/HDF5/NASA1/others/
 $GET https://gamma.hdfgroup.org/ftp/pub/outgoing/opendap/data/HDF5/NASA1/others/d_dset_big_1d_cont.h5.gz
 $GET https://gamma.hdfgroup.org/ftp/pub/outgoing/opendap/data/HDF5/NASA1/others/d_dset_big_1d_cont.h5.dmr
 $GET https://gamma.hdfgroup.org/ftp/pub/outgoing/opendap/data/HDF5/NASA1/others/d_dset_big_1d_cont.h5.dmrpp
+$GET https://gamma.hdfgroup.org/ftp/pub/outgoing/opendap/data/HDF5/NASA1/others/big_1d_shuf.h5
+$GET https://gamma.hdfgroup.org/ftp/pub/outgoing/opendap/data/HDF5/NASA1/others/big_1d_shuf.h5.dmr
+$GET https://gamma.hdfgroup.org/ftp/pub/outgoing/opendap/data/HDF5/NASA1/others/big_1d_shuf.h5.dmrpp
 if [ ! -e d_dset_big_1d_cont.h5 ]; then
     gunzip d_dset_big_1d_cont.h5.gz
 fi
