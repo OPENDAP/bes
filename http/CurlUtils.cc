8f6326f483765b3e91104944e02415d1b6c3fe29
James Gallagher <jgallagher@opendap.org>
2023-05-10 17:33:51 -0600

    In CurlUtiles: Only try to rewind the descriptor when it is valid

--- http/CurlUtils.cc
+++ http/CurlUtils.cc
@@ -1033,10 +1033,12 @@ static void super_easy_perform(CURL *c_handle, int fd) {
         }
     } while (!success);
 
-    // rewind the file
-    auto status = lseek(fd, 0, SEEK_SET);
-    if (-1 == status)
-        throw BESInternalError("Could not seek within the response file.", __FILE__, __LINE__);
+    // rewind the file, if the descriptor is valid
+    if (fd >= 0) {
+        auto status = lseek(fd, 0, SEEK_SET);
+        if (-1 == status)
+            throw BESInternalError("Could not seek within the response file.", __FILE__, __LINE__);
+    }
 
     // Unset the buffer as it goes out of scope
     unset_error_buffer(c_handle);
