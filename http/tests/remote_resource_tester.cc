e623afb00f60e58a180502fb1c25edb3c1686f1d
James Gallagher <jgallagher@opendap.org>
2023-05-11 15:03:15 -0600

    RemoteResource retry fix (#771)
    * Fixed the RemoteResources retry code
    
    The problem was that ftruncate was used to remove an error
    message from the file used to hold the response as it was
    read from the network. However, the file descriptor was not
    rewound to the start of the file, so the valid response data
    was written starting past the initial position. A reader would see the
    initial part of the temporary file as filled with nulls. This
    error was introduced with RR was rewritten.
    
    Also refactored the CmrApiTest.cc code since this work overlapped
    the CMR legacy API and I was working on those tests.
    
    Added tests for the RR retry fix

--- http/tests/remote_resource_tester.cc
+++ http/tests/remote_resource_tester.cc
@@ -8,14 +8,24 @@
 #include <string>
 
 #include <sys/stat.h>
+#include <unistd.h>
 
 #include "TheBESKeys.h"
 #include "RemoteResource.h"
+#include "HttpNames.h"
 #include "BESError.h"
 #include "BESDebug.h"
 
 using namespace std;
 using namespace http;
+
+static void usage()
+{
+    cerr << R"(Usage: remote_resource_tester [-v] [-r expected_size] [-b bes.conf] <url> <debug config>
+       -v: verbose
+       -r: retry
+       -b: bes.conf)" << endl;
+}
 
 /**
  * Takes two arguments: bes.conf and a URL which must be usable by RemoteResource
@@ -24,31 +34,93 @@ using namespace http;
  * @param argv
  * @return 0 if things work, 10 if some sort of BESError is caught, else EXIT_FAILURE
  */
-int main(int, char *argv[])
+int main(int argc, char *argv[])
 {
-    cerr << "Hello, world!" << endl;
+    bool verbose = false;
+    bool retry = false;
+    long expected_size = 0;
     string bes_conf = "bes.conf";
 
-    // Hack - depends on the bes.conf not overriding the default value for the temp dir. jhrg 4/21/23
-    mkdir("/tmp/bes_rr_tmp", 0777);
+    int option_char;
+    while ((option_char = getopt(argc, argv, "r:b:vdh")) != EOF)
+        switch (option_char) {
+            case 'v':
+                verbose = true;
+                break;
+            case 'r':
+                retry = true;
+                expected_size = atoi(optarg);
+                break;
+            case 'b':
+                bes_conf = optarg;
+                break;
+            case 'h':
+                usage();
+                break;
+            default:
+                break;
+        }
+
+    argc -= optind;
+    argv += optind;
+
+    if (argc != 2) {
+        usage();
+        return EXIT_FAILURE;
+    }
 
     try {
-        string debug_dest = argv[3];
+        string debug_dest = argv[1];
         debug_dest.append(",http");
         BESDebug::SetUp(debug_dest);
-        TheBESKeys::ConfigFile = argv[1];
-        auto url = std::make_shared<http::url>(argv[2]);
+
+        if (!bes_conf.empty())
+            TheBESKeys::ConfigFile = bes_conf;
+
+        string tmp_dir_path = TheBESKeys::TheKeys()->read_string_key(REMOTE_RESOURCE_TMP_DIR_KEY, "/tmp/bes_rr_tmp");
+        mkdir(tmp_dir_path.c_str(), 0777);
+
+        auto url = std::make_shared<http::url>(argv[0]);
         auto rr = std::make_unique<RemoteResource>(url);
 
         rr->retrieve_resource();
 
         string name = rr->get_filename();
 
-        ifstream rr_cache_file(name);
-        while (rr_cache_file) {
-            string line;
-            getline(rr_cache_file, line);
-            cout << line << endl;
+        if (retry) {
+            struct stat statbuf{};
+            auto status = stat(name.c_str(), &statbuf);
+            if (status != 0) {
+                cerr << "stat failed: " << strerror(errno) << endl;
+                return EXIT_FAILURE;
+            }
+
+            if (verbose)
+                cerr << "size: " << statbuf.st_size << ", expected: " <<  expected_size << endl;
+
+            if (statbuf.st_size == 0) {
+                cerr << "statbuf.st_size == 0" << endl;
+                return EXIT_FAILURE + 12;
+            }
+            else if (statbuf.st_size != expected_size) {
+                cerr << "statbuf.st_size != expected_size" << endl;
+                return EXIT_FAILURE + 13;
+            }
+
+            return EXIT_SUCCESS;
+        }
+        else {
+            if (verbose)
+                cerr << "Data read from: " << name << endl;
+
+            ifstream rr_cache_file(name);
+            while (rr_cache_file) {
+                string line;
+                getline(rr_cache_file, line);
+                cout << line << endl;
+            }
+
+            return EXIT_SUCCESS;
         }
     }
     catch (const BESError &e) {
@@ -63,6 +135,4 @@ int main(int, char *argv[])
         cerr << "Unknown exception" << endl;
         return EXIT_FAILURE;
     }
-
-    return EXIT_SUCCESS;
 }
