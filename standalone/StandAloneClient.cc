eef5d6f1f9e7bd886b11792b6ac522cf6ad49ff0
James Gallagher <jgallagher@opendap.org>
2023-09-10 11:51:01 -0600

    Added multi_input_tests.m4 to EXTRA_DIST in modules/dmrpp_module/tests/Makefile.am

--- standalone/StandAloneClient.cc
+++ standalone/StandAloneClient.cc
@@ -355,7 +355,7 @@ void StandAloneClient::executeCommands(ifstream & istrm, int repeat)
 		}
 
         this->executeCommand(cmd, 1);
-        
+
         if (repeat > 1 && i < repeat - 1) {
             *_strm << "\nNext-Response:\n" << flush;
         }
