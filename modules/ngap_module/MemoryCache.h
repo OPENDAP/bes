2a6eda502b1a9f09a76230583d3e0edf73c7f220
James Gallagher <jgallagher@opendap.org>
2023-10-16 16:35:28 -0600

    MemoryCache<V> is now a header-only implementation.

--- modules/ngap_module/MemoryCache.h
+++ modules/ngap_module/MemoryCache.h
@@ -7,26 +7,88 @@
 
 #include <string>
 #include <unordered_map>
+#include <deque>
+
+#ifndef BESInternalError_h_
+#include "BESInternalError.h"
+#endif
 
 namespace ngap {
 
 template <typename VALUE>
 class MemoryCache {
-    unsigned int d_threshold;       //< Number of items to cache (not bytes, but items)
-    unsigned int d_space;           //< When purging, remove this many items
-    std::unordered_map<std::string, VALUE> d_cache_map;
+    unsigned int d_max_items;       //< Max number of items to cache (not bytes, but items)
+    unsigned int d_purge_items;     //< When purging, remove this many items
+    
+    std::deque<std::string> d_fifo_keys; //< FIFO queue of keys; used by purge()
+    std::unordered_map<std::string, VALUE> d_cache;
 
-    void purge();
+    void purge() {
+        // if number of elements > threshold, purge
+        for (int entries = 0; entries < d_purge_items; ++entries) {
+            std::string key = d_fifo_keys.front();
+
+            d_cache.erase(d_cache.find(key));
+            d_fifo_keys.pop_front();
+        }
+    }
+
+    bool invariant(bool expensive = false) const {
+        if (d_cache.size() > d_max_items)
+            return false;
+        if (d_fifo_keys.size() > d_max_items)
+            return false;
+        if (d_cache.size() != d_fifo_keys.size())
+            return false;
+
+        if (expensive) {
+            // check that the keys in the cache are in the queue
+            for (auto &key : d_fifo_keys) {
+                if (d_cache.find(key) == d_cache.end())
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    friend class MemoryCacheTest;
 
 public:
     MemoryCache() = default;    // this makes testing easier
     MemoryCache(const MemoryCache *src) = delete;
-    MemoryCache(unsigned int t, unsigned int s): d_threshold(t), d_space(s) {}
+    MemoryCache(unsigned int t, unsigned int s): d_max_items(t), d_purge_items(s) {}
     ~MemoryCache() = default;
     MemoryCache &operator=(const MemoryCache *src) = delete;
 
-    virtual bool get(const std::string &key, VALUE &value);    // returns a _copy_ of the caching information
-    virtual void put(const std::string &key, const VALUE &value);    // caches a _copy_ of the value
+    // Return True if the item is in the cache, false otherwise.
+    // If the time is cache, value holds a reference to the item
+    virtual bool get(const std::string &key, VALUE &value) {
+        if (d_cache.find(key) != d_cache.end()) {
+            value = d_cache[key];
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    // Caches a _copy_ of the value. If the key is already in the cache, the value is updated.
+    virtual void put(const std::string &key, const VALUE &value) {
+        // add key, value to the queue of entries if key is not in the cache
+        // add or overwrite/update the value associated with key
+        if (d_cache.find(key) == d_cache.end()) {
+            d_cache.insert(std::pair<std::string, VALUE>(key, value));
+            d_fifo_keys.push_back(key);
+            if (d_cache.size() > d_max_items)
+                purge();
+        }
+        else
+            d_cache[key] = value;
+    }
+
+    virtual unsigned int size() const { return d_cache.size(); }
+    virtual void clear() { d_cache.clear(); }
 };
 
 } // ngap
--- modules/ngap_module/unit-tests/MemoryCacheTest.cc
+++ modules/ngap_module/unit-tests/MemoryCacheTest.cc
@@ -24,7 +24,9 @@
 #include "config.h"
 
 #include <memory>
+#include <string>
 
+#include "BESDebug.h"
 #include "MemoryCache.h"
 
 #include "test_config.h"
@@ -49,10 +51,45 @@ public:
 
     // tearDown; Called after each test; not used.
 
+    void test_put_one_item() {
+        MemoryCache<string> string_cache(5, 2);    // holds five things; purge removes two
+        string_cache.put("one", "one");
+        CPPUNIT_ASSERT_MESSAGE("The cache should have one item", string_cache.size() == 1);
+        CPPUNIT_ASSERT_MESSAGE("The cache invariant should be true", string_cache.invariant());
+    }
+
+    void test_put_five_items() {
+        MemoryCache<string> string_cache(5, 2);    // holds five things; purge removes two
+        string_cache.put("one", "one_1");
+        string_cache.put("two", "two_2");
+        string_cache.put("three", "three_3");
+        string_cache.put("four", "four_4");
+        string_cache.put("five", "five_5");
+        CPPUNIT_ASSERT_MESSAGE("The cache should have five items", string_cache.size() == 5);
+        CPPUNIT_ASSERT_MESSAGE("The cache invariant should be true", string_cache.invariant());
+    }
+
+    void test_put_six_items_should_purge() {
+        MemoryCache<string> string_cache(5, 2);    // holds five things; purge removes two
+        string_cache.put("one", "one_1");
+        string_cache.put("two", "two_2");
+        string_cache.put("three", "three_3");
+        string_cache.put("four", "four_4");
+        string_cache.put("five", "five_5");
+        string_cache.put("six", "six_6");
+
+        // After adding the sixth item, the cache should have purged two items, leaving four.
+        CPPUNIT_ASSERT_MESSAGE("The cache should have five items", string_cache.size() == 4);
+        CPPUNIT_ASSERT_MESSAGE("The cache invariant should be true", string_cache.invariant());
+        CPPUNIT_ASSERT_MESSAGE("The oldest entry in the fifo of keys should be 'two'",
+                               *string_cache.d_fifo_keys.begin() == "three");
+    }
 
     CPPUNIT_TEST_SUITE( MemoryCacheTest );
 
-    //CPPUNIT_TEST(test_inject_data_url_default);
+    CPPUNIT_TEST(test_put_one_item);
+    CPPUNIT_TEST(test_put_five_items);
+    CPPUNIT_TEST(test_put_six_items_should_purge);
 
     CPPUNIT_TEST_SUITE_END();
 };
