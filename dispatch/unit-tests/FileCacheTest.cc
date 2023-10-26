e4b8ec71ff9fc3f38c5d1f2e82d535954ab64c7e
James Gallagher <jgallagher@opendap.org>
2023-10-25 19:54:35 -0600

    Added tests for multi-process and multi-threaded use of put()

--- dispatch/unit-tests/FileCacheTest.cc
+++ dispatch/unit-tests/FileCacheTest.cc
@@ -25,11 +25,15 @@
 
 #include <memory>
 #include <iostream>
-#include <future>
+
 #include <thread>
+#include <future>
+#include <chrono>
+
 #include <algorithm>
 
 #include <dirent.h>
+#include <sys/wait.h>
 
 #include "FileCache.h"
 #include "BESUtil.h"
@@ -41,7 +45,7 @@
 
 using namespace std;
 
-#define prolog std::string("FileCacheTest::").append(__func__).append("() - ")
+#define prolog string("FileCacheTest::").append(__func__).append("() - ")
 
 class FileCacheTest : public CppUnit::TestFixture {
     std::string cache_dir = string(TEST_BUILD_DIR) + "/test_cache";
@@ -55,7 +59,8 @@ public:
     ~FileCacheTest() override = default;
 
     // Called before each test
-    void setUp() override {
+    void setUp() override
+    {
         // Create the cache directory
         if (mkdir(cache_dir.c_str(), 0755) != 0) {
             if (errno != EEXIST) {
@@ -65,13 +70,14 @@ public:
         }
     }
 
-    void tearDown() override {
+    void tearDown() override
+    {
         // Remove the cache directory
         DIR *dir = nullptr;
         struct dirent *ent{0};
-        if ((dir = opendir (cache_dir.c_str())) != nullptr) {
+        if ((dir = opendir(cache_dir.c_str())) != nullptr) {
             /* print all the files and directories within directory */
-            while ((ent = readdir (dir)) != nullptr) {
+            while ((ent = readdir(dir)) != nullptr) {
                 // Skip the . and .. files and the cache info file
                 if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
                     continue;
@@ -81,33 +87,38 @@ public:
                     CPPUNIT_FAIL(string("Error removing ") + ent->d_name + " from cache directory (" + cache_dir + ")");
                 }
             }
-            closedir (dir);
+            closedir(dir);
             if (remove(cache_dir.c_str()) != 0) {
                 CPPUNIT_FAIL("Error removing cache directory:" + cache_dir);
             }
-        } else {
+        }
+        else {
             CPPUNIT_FAIL("Failed to open cache directory in tearDown()");
         }
     }
 
     // a cache that has not been initialized should fail the invariant test
-    void test_unintialized_cache() {
+    void test_unintialized_cache()
+    {
         FileCache fc;
         CPPUNIT_ASSERT_MESSAGE("Cache invariant should be false - no cache file open", !fc.invariant());
     }
 
-    void test_intialized_cache() {
+    void test_intialized_cache()
+    {
         FileCache fc;
         CPPUNIT_ASSERT_MESSAGE("Cache should initialize", fc.initialize("/tmp/", 100, 20));
         CPPUNIT_ASSERT_MESSAGE("Cache invariant should be true - cache initialized", fc.invariant());
         CPPUNIT_ASSERT_MESSAGE("d_cache_info_fd should not be -1 - cache initialized", fc.d_cache_info_fd != -1);
         CPPUNIT_ASSERT_MESSAGE("d_purge_size should be 20 - cache initialized", fc.d_purge_size == 20);
-        CPPUNIT_ASSERT_MESSAGE("d_max_cache_size_in_bytes should be 100 - cache initialized", fc.d_max_cache_size_in_bytes == 100);
+        CPPUNIT_ASSERT_MESSAGE("d_max_cache_size_in_bytes should be 100 - cache initialized",
+                               fc.d_max_cache_size_in_bytes == 100);
         CPPUNIT_ASSERT_MESSAGE("d_cache_dir should be \"/tmp/\" - cache initialized", fc.d_cache_dir == "/tmp/");
     }
 
     // test the private open_cache_into() method
-    void test_open_cache_info() {
+    void test_open_cache_info()
+    {
         FileCache fc;
         CPPUNIT_ASSERT_MESSAGE("Cache invariant should be false - no cache file open", !fc.invariant());
 
@@ -124,14 +135,16 @@ public:
         CPPUNIT_ASSERT_MESSAGE("The cache file should be 8 bytes", cifsb.st_size == 8);
     }
 
-    void test_open_cache_info_cache_dir_not_set() {
+    void test_open_cache_info_cache_dir_not_set()
+    {
         FileCache fc;
         CPPUNIT_ASSERT_MESSAGE("Cache invariant should be false - no cache file open", !fc.invariant());
 
         CPPUNIT_ASSERT_MESSAGE("The cache file should not be opened", !fc.open_cache_info());
     }
 
-    void test_get_cache_info_size() {
+    void test_get_cache_info_size()
+    {
         FileCache fc;
         CPPUNIT_ASSERT_MESSAGE("Cache invariant should be false - no cache file open", !fc.invariant());
 
@@ -141,7 +154,8 @@ public:
         CPPUNIT_ASSERT_MESSAGE("Initially, the cache info size should be zero", fc.get_cache_info_size() == 0);
     }
 
-    void test_update_cache_info_size() {
+    void test_update_cache_info_size()
+    {
         FileCache fc;
         CPPUNIT_ASSERT_MESSAGE("Cache invariant should be false - no cache file open", !fc.invariant());
 
@@ -159,7 +173,8 @@ public:
 
     }
 
-    void test_put_single_file() {
+    void test_put_single_file()
+    {
         DBG(cerr << prolog << "cache dir: " << cache_dir << '\n');
 
         FileCache fc;
@@ -187,7 +202,8 @@ public:
                                fc.get_cache_info_size() == rcfp_b.st_size);
     }
 
-    void test_put_two_files() {
+    void test_put_two_files()
+    {
         DBG(cerr << prolog << "cache dir: " << cache_dir << '\n');
 
         FileCache fc;
@@ -208,7 +224,8 @@ public:
                                fc.get_cache_info_size() == sb.st_size * 2);
     }
 
-    void test_put_duplicate_key() {
+    void test_put_duplicate_key()
+    {
         DBG(cerr << prolog << "cache dir: " << cache_dir << '\n');
 
         FileCache fc;
@@ -225,6 +242,77 @@ public:
         CPPUNIT_ASSERT_MESSAGE("third put() with duplicate key name should return false", !status);
     }
 
+    void test_put_duplicate_key_two_processes()
+    {
+        DBG(cerr << prolog << "cache dir: " << cache_dir << '\n');
+
+        FileCache fc;
+        CPPUNIT_ASSERT_MESSAGE("Cache should initialize", fc.initialize(cache_dir, 100, 20));
+        string source_file = string(TEST_SRC_DIR) + "/cache/template.txt";
+
+        if (fork() == 0) {
+            // child process
+            // sleep here to give the parent a head start, not much use.
+            bool status = fc.put("key1", source_file);
+            DBG(cerr << prolog << "child process put() status: " << status << '\n');
+            exit(status ? 0 : 1);   // exit with 0 if status is true, 1 if false (it's a process)
+        }
+        else {
+            // parent process - generally faster. I used std::this_thread::sleep_for() but this is
+            // not multi-threaded code. It's just a way to sleep for a short time.
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            bool status = fc.put("key1", source_file);
+            DBG(cerr << prolog << "parent process put() status: " << status << '\n');
+            int child_status;
+            wait(&child_status);
+            DBG(cerr << prolog << "child process exit status: " << WEXITSTATUS(child_status) << '\n');
+            // exit status for a process: 0 is true, 1 is false
+            bool xor_status = (status && WEXITSTATUS(child_status) != 0) || (!status && WEXITSTATUS(child_status) == 0);
+            CPPUNIT_ASSERT_MESSAGE("one status should be true and one false", xor_status);
+        }
+    }
+
+    void test_put_duplicate_key_three_threads()
+    {
+        FileCache fc;
+        CPPUNIT_ASSERT_MESSAGE("Cache should initialize", fc.initialize(cache_dir, 100, 20));
+        string source_file = string(TEST_SRC_DIR) + "/cache/template.txt";
+
+        auto f1 = async(launch::async,
+                        [source_file, &fc](const string &key) -> bool {
+                            // give the other threads a head start
+                            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                            bool status = fc.put(key, source_file);
+                            return status;
+                        },
+                        "key1");
+        auto f2 = async(launch::async,
+                        [source_file, &fc](const string &key) -> bool {
+                            bool status = fc.put(key, source_file);
+                            return status;
+                        },
+                        "key1");
+        auto f3 = async(launch::async,
+                        [source_file, &fc](const string &key) -> bool {
+                            bool status = fc.put(key, source_file);
+                            return status;
+                        },
+                        "key1");
+
+        DBG(cerr << "Start querying\n" );
+
+        bool f1_status = f1.get();
+        bool f2_status = f2.get();
+        bool f3_status = f3.get();
+        DBG(cerr << "f1_status: " << f1_status << '\n');
+        DBG(cerr << "f2_status: " << f2_status << '\n');
+        DBG(cerr << "f3_status: " << f3_status << '\n');
+        bool xor_status = (f1_status && !f2_status && !f3_status) ||
+                          (!f1_status && f2_status && !f3_status) ||
+                          (!f1_status && !f2_status && f3_status);
+        CPPUNIT_ASSERT_MESSAGE("Only one status should be true", xor_status);
+    }
+
     CPPUNIT_TEST_SUITE(FileCacheTest);
 
     CPPUNIT_TEST(test_unintialized_cache);
@@ -238,6 +326,8 @@ public:
     CPPUNIT_TEST(test_put_single_file);
     CPPUNIT_TEST(test_put_two_files);
     CPPUNIT_TEST(test_put_duplicate_key);
+    CPPUNIT_TEST(test_put_duplicate_key_two_processes);
+    CPPUNIT_TEST(test_put_duplicate_key_three_threads);
 
     CPPUNIT_TEST_SUITE_END();
 };
@@ -246,6 +336,7 @@ CPPUNIT_TEST_SUITE_REGISTRATION(FileCacheTest);
 
 // } // namespace http
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     return bes_run_tests<FileCacheTest>(argc, argv, "cerr,file-cache") ? 0 : 1;
 }
