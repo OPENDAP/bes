954aaf205a29249ca0a1b0e9850c67013528b4d0
myang6 <myang6@hdfgroup.org>
2023-12-07 14:01:44 -0500

    HYRAX-1250, adding the direct IO check before the read() in DmrppRequestHandler.cc.

--- modules/dmrpp_module/DmrppRequestHandler.cc
+++ modules/dmrpp_module/DmrppRequestHandler.cc
@@ -327,8 +327,10 @@ void DmrppRequestHandler::get_dmrpp_from_container_or_cache(BESContainer *contai
             if (DmrppRequestHandler::is_netcdf4_enhanced_response == true) { 
                 BESDEBUG("dmrpp","calling set_up_all_direct_io_flags"<<endl);
                 bool global_dio_flag = dmz->set_up_all_direct_io_flags_phase_1(dmr);
-                if (global_dio_flag) 
+                if (global_dio_flag) {
+                    BESDEBUG("dmrpp","global_dio_flags is true."<<endl);
                     dmz->set_up_all_direct_io_flags_phase_2(dmr);
+                }
             }
 
             dmz->load_all_attributes(dmr);
@@ -352,7 +354,11 @@ void DmrppRequestHandler::get_dmrpp_from_container_or_cache(BESContainer *contai
             BESDEBUG("dmrpp","Before calling set_up_all_direct_io_flags"<<endl);
             if (DmrppRequestHandler::is_netcdf4_enhanced_response == true) { 
                 BESDEBUG("dmrpp","calling set_up_all_direct_io_flags"<<endl);
-                dmz->set_up_all_direct_io_flags_phase_1(dmr);
+                bool global_dio_flag = dmz->set_up_all_direct_io_flags_phase_1(dmr);
+                if (global_dio_flag) {
+                    BESDEBUG("dmrpp","global_dio_flags is true."<<endl);
+                    dmz->set_up_all_direct_io_flags_phase_2(dmr);
+                }
             }
  
             dmz->load_all_attributes(dmr);
