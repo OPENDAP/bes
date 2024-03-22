91b399369d513ffb4b36bb8ad728f50bb7116a0c
Muqun Yang <myang6@hdfgroup.org>
2024-03-22 08:11:43 -0500

    HYRAX-1336, add tests in dmrpp and fileout netcdf for the case when the dimension size is 0.

--- modules/dmrpp_module/build_dmrpp_h4/valgrind_build_dmrpp_h4.sh
+++ modules/dmrpp_module/build_dmrpp_h4/valgrind_build_dmrpp_h4.sh
@@ -6,3 +6,4 @@ valgrind ./build_dmrpp_h4 -f ../tests/hdf4_build_dmrpp/testvs2.hdf -r ../tests/h
 valgrind ./build_dmrpp_h4 -f ../tests/hdf4_build_dmrpp/vdata_linked_blocks.hdf -r ../tests/hdf4_build_dmrpp/vdata_linked_blocks.hdf.dmr
 valgrind ./build_dmrpp_h4 -f ../tests/hdf4_build_dmrpp/vdata_test.hdf -r ../tests/hdf4_build_dmrpp/vdata_test.hdf.dmr
 valgrind ./build_dmrpp_h4 -f ../tests/hdf4_build_dmrpp/vg_hl_test.hdf -r ../tests/hdf4_build_dmrpp/vg_hl_test.hdf.dmr
+valgrind ./build_dmrpp_h4 -f ../tests/hdf4_build_dmrpp/SDS_simple_unlimited_0.hdf -r ../tests/hdf4_build_dmrpp/SDS_simple_unlimited_0.hdf.dmr
