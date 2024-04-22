cd07b0607ca3ed5a64da709fb04d13f8d23e03be
Muqun Yang <myang6@hdfgroup.org>
2024-04-22 14:24:52 -0500

    HYRAX-1404, add HDF-EOS2 swath and point data dmr and dmrpp tests to the hdf4 handler,dmrpp and fileout netcdf.

--- modules/dmrpp_module/build_dmrpp_h4/valgrind_build_dmrpp_h4.sh
+++ modules/dmrpp_module/build_dmrpp_h4/valgrind_build_dmrpp_h4.sh
@@ -9,3 +9,5 @@ valgrind ./build_dmrpp_h4 -f ../tests/hdf4_build_dmrpp/vg_hl_test.hdf -r ../test
 valgrind ./build_dmrpp_h4 -f ../tests/hdf4_build_dmrpp/SDS_simple_unlimited_0.hdf -r ../tests/hdf4_build_dmrpp/SDS_simple_unlimited_0.hdf.dmr
 valgrind ./build_dmrpp_h4 -f ../tests/hdf4_build_dmrpp/cea.hdf -r ../tests/hdf4_build_dmrpp/cea.hdf.dmr
 valgrind ./build_dmrpp_h4 -f ../tests/hdf4_build_dmrpp/sinusoid.hdf -r ../tests/hdf4_build_dmrpp/sinusoid.hdf.dmr
+valgrind ./build_dmrpp_h4 -f ../tests/hdf4_build_dmrpp/h_swath.hdf -r ../tests/hdf4_build_dmrpp/h_swath.hdf.dmr
+valgrind ./build_dmrpp_h4 -f ../tests/hdf4_build_dmrpp/PointFile.hdf -r ../tests/hdf4_build_dmrpp/PointFile.hdf.dmr
