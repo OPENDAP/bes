b144ba6e98ddc92e2ff585ff53fe1cac505b38c0
James Gallagher <jgallagher@opendap.org>
2024-08-07 14:26:23 -0600

    Fix for finding the data files for testing

--- modules/ngap_module/unit-tests/NgapOwnedContainerTest.cc
+++ modules/ngap_module/unit-tests/NgapOwnedContainerTest.cc
@@ -95,9 +95,9 @@ public:
     void test_file_to_string() {
         TEST_NAME;
         string content;
-        string file_name = string(TEST_SRC_DIR) + "/data/chunked_enum.h5.dmrpp";
+        string file_name = string(TEST_DATA_DIR) + "/chunked_enum.h5.dmrpp";
         int fd = open(file_name.c_str(), O_RDONLY);
-        CPPUNIT_ASSERT_MESSAGE("The file should open", fd > 0);
+        CPPUNIT_ASSERT_MESSAGE("The file should be open", fd != -1);
         CPPUNIT_ASSERT_MESSAGE("The file should be read", NgapOwnedContainer::file_to_string(fd, content));
         CPPUNIT_ASSERT_MESSAGE("The file should be closed", close(fd) == 0);
         CPPUNIT_ASSERT_MESSAGE("The file should have content", !content.empty());
@@ -108,9 +108,9 @@ public:
     void test_file_to_string_bigger_than_buffer() {
         TEST_NAME;
         string content;
-        string file_name = "NGAPApiTest.cc";    // ~16k while the buffer is 4k
+        string file_name = string(TEST_SRC_DIR) + "/NGAPApiTest.cc";    // ~16k while the buffer is 4k
         int fd = open(file_name.c_str(), O_RDONLY);
-        CPPUNIT_ASSERT_MESSAGE("The file should open be", fd != -1);
+        CPPUNIT_ASSERT_MESSAGE("The file should be open", fd != -1);
         CPPUNIT_ASSERT_MESSAGE("The file should be read", NgapOwnedContainer::file_to_string(fd, content));
         CPPUNIT_ASSERT_MESSAGE("The file should be closed", close(fd) == 0);
         CPPUNIT_ASSERT_MESSAGE("The file should have content", !content.empty());
