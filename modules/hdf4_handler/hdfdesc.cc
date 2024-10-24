f47dc27b12334a8d5330bcc803c250d4ee9c335b
myang6 <myang6@hdfgroup.org>
2024-10-24 17:25:25 -0400

    HYRAX-1553, add CF grid variables and attributes for the DMR generation.

--- modules/hdf4_handler/hdfdesc.cc
+++ modules/hdf4_handler/hdfdesc.cc
@@ -149,6 +149,8 @@
 // HDF-EOS2 (including the hybrid) will be handled as HDF-EOS2 objects if the HDF-EOS2 library is configured in
 #ifdef USE_HDFEOS2_LIB
 #include "HDFEOS2.h"
+#include "HDFEOS2GeoCFProj.h"
+#include "HDFEOS2GeoCF1D.h"
 #include "HDFEOS2Array_RealField.h"
 #include "HDFEOS2ArrayGridGeoField.h"
 #include "HDFEOS2ArraySwathGeoField.h"
@@ -255,11 +257,14 @@ void map_vdata_to_dap4_structure_array(int32 vdata_id, int32 num_elms, int32 nfl
 void map_vdata_to_dap4_atomic_array(int32 vdata_id, int32 num_elms, int32 obj_ref, D4Group *d4g, const string &filename);
 void map_vdata_to_dap4_attrs(HDFDMRArray_VD *ar, int32 vdata_id, int32 obj_ref);
 
-// 5. HDF-EOS2 handlings
+// 5. HDF-EOS2 handlings(Not need the hdf-eos2 library)
 int is_group_eos2_grid(const string& vgroup_name, vector<eos2_grid_t>& eos2_grid_lls);
 void add_eos2_latlon_info(D4Group *d4_grp, D4Group *root_grp, const eos2_grid_t &eos2_grid, const eos2_grid_info_t &eos2_grid_info, const string&filename);
 void add_dummy_grid_cv(D4Group *d4_grp, const eos2_grid_info_t &eos2_grid_info);
-void add_CF_1D_cvs(D4Group *d4_grp, const eos2_grid_t &eos2_grid, const eos2_grid_info_t &eos2_grid_info, const string& xdim_path, const string &ydim_path);
+void add_ps_cf_grid_mapping_attrs(libdap::BaseType *var, const eos2_grid_info_t & eg_info);
+void add_lamaz_cf_grid_mapping_attrs(libdap::BaseType *var, const eos2_grid_info_t & eg_info);
+void add_CF_1D_cvs(D4Group *d4_grp, D4Group *root_grp, const eos2_grid_t &eos2_grid, const eos2_grid_info_t &eos2_grid_info, const string& xdim_path, const string &ydim_path);
+void add_CF_1D_cv_attrs(libdap::BaseType *var, bool is_ydim);
 
 // 6. Special HDF4 handlings
 
@@ -4734,7 +4739,7 @@ void read_dmr_vlone_groups(D4Group *root_grp, int32 file_id, int32 sdfd, const s
         throw InternalErr(__FILE__, __LINE__, "error in the check_eos2_grid(). ");
     }
 //HDF-EOS2 DEBUG
-//#if 0
+#if 0
 for(const auto& eg:eos2_grid_lls) {
     cout<<"grid name: "<<eg.grid_name <<endl;
 if (eg.oned_latlon==true) 
@@ -4751,7 +4756,7 @@ for(const auto& eg:eos2_grids_info) {
 
 
 }
-//#endif
+#endif
 #endif
     
     ref_array.resize(num_lonevg);
@@ -4824,12 +4829,8 @@ for(const auto& eg:eos2_grids_info) {
            add_eos2_latlon_info(tem_d4_cgroup, root_grp, eos2_grid_lls[eos2_grid_lls_index],eos2_grids_info[eos2_grid_lls_index],filename);
            is_eos2_grid = true;
            int32 proj_code = eos2_grids_info[eos2_grid_lls_index].projcode;
-cerr<<"proj_code: "<<proj_code <<endl;
-           if (proj_code == GCTP_LAMAZ || proj_code == GCTP_PS || proj_code == GCTP_SNSOID) {
-
-cerr<<"proj_code inside: "<<proj_code <<endl;
+           if (proj_code == GCTP_LAMAZ || proj_code == GCTP_PS || proj_code == GCTP_SNSOID) 
                 is_eos2_grid_cf_mapping = true;
-}
         }
 
         convert_vgroup_objects(vgroup_id,file_id,sdfd,tem_d4_cgroup,root_grp, vgroup_name_orig_str,filename,is_eos2_grid,is_eos2_grid_cf_mapping);
@@ -5781,7 +5782,7 @@ void add_eos2_latlon_info(D4Group *d4_grp, D4Group *root_grp, const eos2_grid_t 
         // Add grid_mapping dummy variable
         add_dummy_grid_cv(d4_grp, eos2_grid_info);
         // Add 1-D CF grid coordinates
-        add_CF_1D_cvs(d4_grp,eos2_grid,eos2_grid_info,xdim_path,ydim_path);
+        add_CF_1D_cvs(d4_grp,root_grp,eos2_grid,eos2_grid_info,xdim_path,ydim_path);
     }
     string lat_name ="Latitude";
     string lon_name ="Longitude";
@@ -5925,13 +5926,183 @@ bool obtain_eos2_gd_ll_info(const string & fname, const string & grid_name, int3
 
     return true;
 }
-void add_dummy_grid_cv(D4Group *d4_grp, const eos2_grid_info_t &eos2_grid_info) {
+void add_dummy_grid_cv(D4Group *d4_grp, const eos2_grid_info_t &eg_info) {
+
+    string dummy_proj_cf_name = "eos5_cf_projection";
+    auto dummy_proj_cf_unique = make_unique<HDFEOS2GeoCFProj>(dummy_proj_cf_name, dummy_proj_cf_name);
+    HDFEOS2GeoCFProj *dummy_proj_cf = dummy_proj_cf_unique.release();
+    dummy_proj_cf->set_is_dap4(true);
+
+    if (eg_info.projcode == GCTP_SNSOID) {
+
+        add_var_dap4_attr(dummy_proj_cf, "grid_mapping_name", attr_str_c, "sinusoidal");
+        add_var_dap4_attr(dummy_proj_cf, "longitude_of_central_meridian", attr_float64_c, "0.0");
+        add_var_dap4_attr(dummy_proj_cf, "earth_radius", attr_float64_c, "6371007.181");
+        add_var_dap4_attr(dummy_proj_cf, "_CoordinateAxisTypes", attr_str_c, "GeoX GeoY");
+    } else if (eg_info.projcode == GCTP_PS)
+        add_ps_cf_grid_mapping_attrs(dummy_proj_cf, eg_info);
+    else if (eg_info.projcode == GCTP_LAMAZ)
+        add_lamaz_cf_grid_mapping_attrs(dummy_proj_cf, eg_info);
+    d4_grp->add_var_nocopy(dummy_proj_cf);
 
 }
 
-void add_CF_1D_cvs(D4Group *d4_grp, const eos2_grid_t &eos2_grid, const eos2_grid_info_t &eos2_grid_info, const string& xdim_path, const string &ydim_path) {
+void add_CF_1D_cvs(D4Group *d4_grp, D4Group *root_grp, const eos2_grid_t &eos2_grid, const eos2_grid_info_t &eos2_grid_info, const string& xdim_path, const string &ydim_path) {
+
+    auto ar_bt_dim1_unique = make_unique<Float64>("XDim");
+    auto ar_bt_dim1 = ar_bt_dim1_unique.get();
+    auto ar_dim1_unique = make_unique<HDFEOS2GeoCF1D>(eos2_grid_info.projcode, eos2_grid_info.upleft[0], eos2_grid_info.lowright[0],
+                                                        eos2_grid.xdim, "XDim", ar_bt_dim1);
+    auto ar_dim1 = ar_dim1_unique.release();
+    ar_dim1->append_dim_ll(eos2_grid.xdim, xdim_path);
+    dims_transform_to_dap4(ar_dim1,root_grp,true);
+    ar_dim1->set_is_dap4(true);
+    add_CF_1D_cv_attrs(ar_dim1,false);
+    d4_grp->add_var_nocopy(ar_dim1);
+
+    auto ar_bt_dim0_unique = make_unique<Float64>("YDim");
+    auto ar_bt_dim0 = ar_bt_dim0_unique.get();
+    auto ar_dim0_unique = make_unique<HDFEOS2GeoCF1D>(eos2_grid_info.projcode, eos2_grid_info.upleft[1], eos2_grid_info.lowright[1],
+                                                        eos2_grid.ydim, "YDim", ar_bt_dim0);
+    auto ar_dim0 = ar_dim0_unique.release();
+    ar_dim0->append_dim_ll(eos2_grid.ydim, ydim_path);
+    dims_transform_to_dap4(ar_dim0,root_grp,true);
+
+    ar_dim0->set_is_dap4(true);
+    add_CF_1D_cv_attrs(ar_dim0,true);
+    d4_grp->add_var_nocopy(ar_dim0);
 
 }
+
+// Direct CF to DAP4, 
+// add CF grid_mapping $attributes for the special dimension variables.
+void add_CF_1D_cv_attrs(libdap::BaseType *var, bool is_ydim) {
+
+    string standard_name;
+    string long_name;
+    string COORAxisTypes;
+
+    if (true == is_ydim) {
+        standard_name = "projection_y_coordinate";
+        long_name = "y coordinate of projection ";
+        COORAxisTypes = "GeoY";
+    }
+    else {
+        standard_name = "projection_x_coordinate";
+        long_name = "x coordinate of projection ";
+        COORAxisTypes = "GeoX";
+    }
+
+    add_var_dap4_attr(var,"standard_name", attr_str_c, standard_name);
+    add_var_dap4_attr(var,"long_name", attr_str_c, long_name);
+    add_var_dap4_attr(var,"units", attr_str_c, "meter");
+    add_var_dap4_attr(var,"_CoordinateAxisType", attr_str_c, COORAxisTypes);
+
+}
+
+
+void add_lamaz_cf_grid_mapping_attrs(libdap::BaseType *var, const eos2_grid_info_t & eg_info) {
+
+    double lon_proj_origin = EHconvAng(eg_info.params[4],HDFE_DMS_DEG);
+    double lat_proj_origin = EHconvAng(eg_info.params[5],HDFE_DMS_DEG);
+    double fe = eg_info.params[6];
+    double fn = eg_info.params[7];
+
+    add_var_dap4_attr(var,"grid_mapping_name", attr_str_c, "lambert_azimuthal_equal_area");
+
+    // Here we don't use to_string since it adds trailing zeros at the end.
+    ostringstream s_lon_proj_origin;
+    s_lon_proj_origin << lon_proj_origin;
+    add_var_dap4_attr(var,"longitude_of_projection_origin", attr_float64_c, s_lon_proj_origin.str());
+
+    ostringstream s_lat_proj_origin;
+    s_lat_proj_origin << lat_proj_origin;
+
+    add_var_dap4_attr(var,"latitude_of_projection_origin", attr_float64_c, s_lat_proj_origin.str());
+
+    if(fe == 0.0)
+        add_var_dap4_attr(var,"false_easting",attr_float64_c,"0.0");
+    else {
+        ostringstream s_fe;
+        s_fe << fe;
+        add_var_dap4_attr(var,"false_easting",attr_float64_c,s_fe.str());
+    }
+
+    if(fn == 0.0)
+        add_var_dap4_attr(var,"false_northing",attr_float64_c,"0.0");
+    else {
+        ostringstream s_fn;
+        s_fn << fn;
+        add_var_dap4_attr(var,"false_northing",attr_float64_c,s_fn.str());
+    }
+
+    add_var_dap4_attr(var,"_CoordinateAxisTypes", attr_str_c, "GeoX GeoY");
+}
+
+
+void add_ps_cf_grid_mapping_attrs(libdap::BaseType *var, const eos2_grid_info_t & eg_info) {
+
+    // The following information is added according to the HDF-EOS2 user's guide and
+    // CF 1.11 grid_mapping requirement.
+
+    // Longitude down below pole of map
+    double vert_lon_pole =  EHconvAng(eg_info.params[4],HDFE_DMS_DEG);
+
+    // Latitude of true scale
+    double lat_true_scale = EHconvAng(eg_info.params[5],HDFE_DMS_DEG);
+
+    // False easting
+    double fe = eg_info.params[6];
+
+    // False northing
+    double fn = eg_info.params[7];
+
+    add_var_dap4_attr(var,"grid_mapping_name",attr_str_c,"polar_stereographic");
+
+    ostringstream s_vert_lon_pole;
+    s_vert_lon_pole << vert_lon_pole;
+
+    // The following mapping is based on my best understanding. KY
+    // CF: straight_vertical_longitude_from_pole
+    add_var_dap4_attr(var,"straight_vertical_longitude_from_pole", attr_float64_c, s_vert_lon_pole.str());
+    // Not using to_string here because it adds trailing zeros at the end. 
+#if 0
+    add_var_dap4_attr(var,"straight_vertical_longitude_from_pole", attr_float64_c, to_string(vert_lon_pole));
+#endif
+
+    ostringstream s_lat_true_scale;
+    s_lat_true_scale << lat_true_scale;
+    add_var_dap4_attr(var,"standard_parallel", attr_float64_c, s_lat_true_scale.str());
+
+    if(fe == 0.0)
+        add_var_dap4_attr(var,"false_easting",attr_float64_c,"0.0");
+    else {
+        ostringstream s_fe;
+        s_fe << fe;
+        add_var_dap4_attr(var,"false_easting",attr_float64_c,s_fe.str());
+    }
+
+    if(fn == 0.0)
+        add_var_dap4_attr(var,"false_northing",attr_float64_c,"0.0");
+    else {
+        ostringstream s_fn;
+        s_fn << fn;
+        add_var_dap4_attr(var,"false_northing",attr_float64_c,s_fn.str());
+    }
+
+    if(lat_true_scale >0)
+        add_var_dap4_attr(var,"latitude_of_projection_origin",attr_float64_c,"+90.0");
+    else
+        add_var_dap4_attr(var, "latitude_of_projection_origin",attr_float64_c,"-90.0");
+
+    add_var_dap4_attr(var, "_CoordinateAxisTypes", attr_str_c, "GeoX GeoY");
+
+    // From CF, PS has another paramseter,
+    // Either standard_parallel (EPSG 9829) or scale_factor_at_projection_origin (EPSG 9810)
+    // I cannot find the corresponding paramseter from the EOS library.
+
+}
+
 
 #endif
 
