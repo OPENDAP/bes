4296f4bd1f4ef4f83d37bde0d1dee5408e506a13
Hannah Robertson <1076701+hannahilea@users.noreply.github.com>
2025-10-15 14:30:46 -0400

    fix wrong string

--- modules/dmrpp_module/ngap_container/NgapApi.cc
+++ modules/dmrpp_module/ngap_container/NgapApi.cc
@@ -535,7 +535,7 @@ string NgapApi::convert_ngap_resty_path_to_data_access_url(const string &restifi
     if (data_s3_url.empty() || s3credentials_url.empty()) {
         // Eventually we'll be removing the non-s3 access; we need to know about any unsupported cases before that happens.
         // Add a log warning that can be searched.
-        BES_PROFILE_TIMING(string("DEPRECATION WARNING - Data s3 url not found - ") + cmr_query_url);
+        BES_PROFILE_TIMING(string("PRE-DEPRECATION WARNING - Data s3 url or s3credentials not found - ") + cmr_query_url);
     }
 
     // Check for existing .dmrpp and remove it if found at the end of the url. - kln 6/6/25
@@ -545,8 +545,8 @@ string NgapApi::convert_ngap_resty_path_to_data_access_url(const string &restifi
         data_access_url.erase(data_access_url.size() - suffix.size());
     }
     // ...ditto for the s3 url
-    if (!data_s3_url.empty() && data_s3_url.size() >= suffix.size() &&
-    data_s3_url.compare(data_access_url.size() - suffix.size(), suffix.size(), suffix) == 0) {
+    if (data_s3_url.size() >= suffix.size() &&
+    data_s3_url.compare(data_s3_url.size() - suffix.size(), suffix.size(), suffix) == 0) {
         data_s3_url.erase(data_access_url.size() - suffix.size());
     }
 
