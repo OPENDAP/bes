6b54d8ae4672429929d7ed72a4cc246549b53aee
Hannah Robertson <1076701+hannahilea@users.noreply.github.com>
2025-11-19 12:52:40 -0500

    re-enable signedurlcachetest

--- aws/unit-tests/SignedUrlCacheTest.cc
+++ aws/unit-tests/SignedUrlCacheTest.cc
@@ -365,19 +365,19 @@ public:
 CPPUNIT_TEST_SUITE(SignedUrlCacheTest);
 
     // Test behavior analogous to that of the EffectiveUrlCache:
-    // CPPUNIT_TEST(get_cached_signed_url_test);
-    // CPPUNIT_TEST(is_cache_disabled_test);
-    // CPPUNIT_TEST(set_skip_regex_test);
-    // CPPUNIT_TEST(dump_test);
+    CPPUNIT_TEST(get_cached_signed_url_test);
+    CPPUNIT_TEST(is_cache_disabled_test);
+    CPPUNIT_TEST(set_skip_regex_test);
+    CPPUNIT_TEST(dump_test);
 
     // Test behavior specific to SignedUrlCache:
-    // CPPUNIT_TEST(is_timestamp_after_now_test);
-    // CPPUNIT_TEST(retrieve_cached_s3credentials_test);
-    // CPPUNIT_TEST(retrieve_cached_s3credentials_expired_credentials_test);
-    // CPPUNIT_TEST(extract_s3_credentials_from_response_json_test);
-    // CPPUNIT_TEST(cache_signed_url_components_test);
-    // CPPUNIT_TEST(retrieve_cached_signed_url_components_test);
-    // CPPUNIT_TEST(get_s3credentials_from_endpoint_test);
+    CPPUNIT_TEST(is_timestamp_after_now_test);
+    CPPUNIT_TEST(retrieve_cached_s3credentials_test);
+    CPPUNIT_TEST(retrieve_cached_s3credentials_expired_credentials_test);
+    CPPUNIT_TEST(extract_s3_credentials_from_response_json_test);
+    CPPUNIT_TEST(cache_signed_url_components_test);
+    CPPUNIT_TEST(retrieve_cached_signed_url_components_test);
+    CPPUNIT_TEST(get_s3credentials_from_endpoint_test);
 
     // ...and, specifically, the signing itself: 
     // TODO-future: will add/update these tests once signing behavior is implemented
@@ -385,8 +385,8 @@ CPPUNIT_TEST_SUITE(SignedUrlCacheTest);
     // - get_signed_url
 
     // Last but not least, test those helper functions
-    // CPPUNIT_TEST(split_s3_url_test);
-    // CPPUNIT_TEST(num_seconds_until_expiration_test);
+    CPPUNIT_TEST(split_s3_url_test);
+    CPPUNIT_TEST(num_seconds_until_expiration_test);
 
     CPPUNIT_TEST_SUITE_END();
 };
