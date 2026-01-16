a600d1990c7ea8729eef24cf2020f68dde3cee98
Hannah Robertson <1076701+hannahilea@users.noreply.github.com>
2026-01-16 00:47:50 -0500

    fix regex in dap unit test

--- dap/unit-tests/GlobalMetadataStoreTest.cc
+++ dap/unit-tests/GlobalMetadataStoreTest.cc
@@ -2037,11 +2037,15 @@ public:
          DBG(cerr << "Reading baseline: " << baseline_name << endl);
          CPPUNIT_ASSERT(access(baseline_name.c_str(), R_OK) == 0);
 
-         string insert_xml_base_baseline = read_test_baseline(baseline_name);
+         // Strip versions
+         std::regex regex1("dmrVersion=\"[0-9]+\.[0-9]+\"");
+         auto stripped_input = std::regex_replace(oss.str(), regex1, "dmrVersion=\"removed\"");
 
-         std::regex version_to_strip("(dmrVersion=\"[[0-9]+]\.[[0-9]+]\")");
-         auto stripped_baseline = std::regex_replace(insert_xml_base_baseline, version_to_strip, "dmrVersion=\"removed\"");
-         CPPUNIT_ASSERT(insert_xml_base_baseline == oss.str());
+         std::regex regex2("dmrpp/[0-9]+\.[0-9]+\.[0-9]+");
+         auto stripped_input2 = std::regex_replace(stripped_input, regex2, "dmrpp/removed_version");
+
+         string insert_xml_base_baseline = read_test_baseline(baseline_name);
+         CPPUNIT_ASSERT_MESSAGE("The baseline " + insert_xml_base_baseline + " did not match the value " + stripped_input2, insert_xml_base_baseline == stripped_input2);
      }
 
     void insert_xml_base_test_error() {
