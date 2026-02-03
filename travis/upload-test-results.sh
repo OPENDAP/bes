808e4c3b599344991c3b04cf908ec7cce14a5d57
Hannah Robertson <1076701+hannahilea@users.noreply.github.com>
2026-02-03 10:48:36 -0500

    post logs on failure

--- travis/upload-test-results.sh
+++ travis/upload-test-results.sh
@@ -3,14 +3,14 @@
 # Upload the results of tests after running a build on Travis
 
 LOG_FILE_TGZ=bes-autotest-${TRAVIS_JOB_NUMBER}-logs.tar.gz
-if test x$BES_BUILD = xmain -o x$BES_BUILD = xdistcheck 
+if [[ $BES_BUILD = "main" || "$BES_BUILD" = "distcheck" || "$BES_BUILD" = "docker"* ]]
 then
 	tar -czf /tmp/${LOG_FILE_TGZ} `find . -name timing -prune -o -name '*.log' -print -o -name '*site_map.txt' -print`
-	
+
 	# using: 'test -z "$AWS_ACCESS_KEY_ID" || ...' keeps after_script from running
 	# the aws cli for forked PRs (where secure env vars are null). I could've used
 	# an 'if' to block out the whole script, but I didn't... jhrg 3/21/18
-	
+
 	test -z "$AWS_ACCESS_KEY_ID" || aws s3 cp /tmp/${LOG_FILE_TGZ} s3://opendap.travis.tests/
 fi
 
