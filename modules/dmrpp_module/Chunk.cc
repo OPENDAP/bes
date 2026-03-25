46a6875cc6184b1839dd726075b638b178debf1d
Hannah Robertson <1076701+hannahilea@users.noreply.github.com>
2026-03-25 14:37:28 -0400

    fix merge

--- modules/dmrpp_module/Chunk.cc
+++ modules/dmrpp_module/Chunk.cc
@@ -1368,7 +1368,7 @@ std::shared_ptr<http::url> Chunk::get_data_url() const {
     if (url == nullptr) {
         url = EffectiveUrlCache::TheCache()->get_effective_url(d_data_url);
     }
-    BESDEBUG(MODULE, prolog << "Using data_url: " << effective_url->get_url_no_query() << endl);
+    BESDEBUG(MODULE, prolog << "Using data_url: " << url->get_url_no_query() << endl);
 
 #if ENABLE_TRACKING_QUERY_PARAMETER
     //A conditional call to void Chunk::add_tracking_query_param()
