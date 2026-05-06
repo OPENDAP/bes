3f84bb992599f08d0f43295538afa3190779db22
Hannah Robertson <1076701+hannahilea@users.noreply.github.com>
2026-05-06 09:21:39 -0400

    make test more robust to http request

--- modules/dmrpp_module/ngap_container/unit-tests/NgapOwnedContainerTest.cc
+++ modules/dmrpp_module/ngap_container/unit-tests/NgapOwnedContainerTest.cc
@@ -418,26 +418,38 @@ public:
         }
 
     void test_get_dmrpp_from_cache_or_remote_source_legacy_cmr_query_format() {
-        // NB: This test may be a little flakey; if the CMR is down, we'll get an HTTP error. If we get as far as an HTTP error, the test has successfully passed.
         NgapOwnedContainer container;
         string dmrpp_string = "";
 
         container.set_real_name("providers/POCLOUD/collections/ECCO Ocean Temperature and Salinity - Monthly Mean llc90 Grid (Version 4 Release 4)/granules/OCEAN_TEMPERATURE_SALINITY_mon_mean_2017-12_ECCO_V4r4_native_llc0090");
         CPPUNIT_ASSERT_MESSAGE("DMR++ should be retrieved for legacy URL `" + container.get_real_name() + "`", 
                                container.get_dmrpp_from_cache_or_remote_source(dmrpp_string));
-        CPPUNIT_ASSERT_MESSAGE("Retrieved dmrpp_string for legacy URL should not be empty", 
-                               !dmrpp_string.empty());
+        try {
+            CPPUNIT_ASSERT_MESSAGE("DMR++ URI generation should succeed for legacy URL `" + container.get_real_name(),
+                                     container.get_dmrpp_from_cache_or_remote_source(dmrpp_string)
+                                    );
+        } catch (http::HttpError &e) {
+            // If we fail with an HttpError, that's fine---we're already past the "DRM++ URI generation" we cared about.
+            // The downstream HTTP request to FETCH the DMR++ may fail, depending on the credentials of the test runner and the
+            // current CMR status. This is fine!
+        }
+        
 
-        // This test should pass as does the previous test, but due to url encoding of spaces in cmr for some legacy urls, this request fails.
-        // Filed as HYRAX-
+        // This test should behave identically to the previous test, but due to url encoding of
+        // spaces in CMR for some legacy urls, this request currently fails sooner, before even
+        // attempting to fetch the DMR++. Filed as HYRAX-2148
         dmrpp_string = "";
         container.set_real_name("providers/POCLOUD/collections/ECCO%20Ocean%20Temperature%20and%20Salinity%20-%20Monthly%20Mean%20llc90%20Grid%20(Version%204%20Release%204)/granules/OCEAN_TEMPERATURE_SALINITY_mon_mean_2017-12_ECCO_V4r4_native_llc0090");
-        CPPUNIT_ASSERT_THROW_MESSAGE("DMR++ retrieval will throw due to HYRAX-2148, even though ultimately it should succeed for legacy URL `" + container.get_real_name() + "`",
-                                     container.get_dmrpp_from_cache_or_remote_source(dmrpp_string),
-                                     BESNotFoundError
-                                    );
-        CPPUNIT_ASSERT_MESSAGE("Retrieved dmrpp_string should be empty due to HYRAX-2148, even though ultimately this legacy URL should *not* be empty",
-                              dmrpp_string.empty());
+        try {
+            CPPUNIT_ASSERT_THROW_MESSAGE("DMR++ retrieval will throw due to HYRAX-2148, even though ultimately it should succeed for legacy URL `" + container.get_real_name() + "`",
+                                         container.get_dmrpp_from_cache_or_remote_source(dmrpp_string),
+                                         BESNotFoundError
+                                         );
+        } catch (http::HttpError &e) {
+            // If we fail with an HttpError, that's fine---we're already past the "DRM++ URI generation" we cared about.
+            // The downstream HTTP request to FETCH the DMR++ may fail, depending on the credentials of the test runner and the
+            // current CMR status. This is fine!
+        }
     }
 
     void test_access() {
