39eba08d4bc33fe7371c494fbef0203fadd00ef9
myang6 <myang6@hdfgroup.org>
2026-05-28 18:16:51 -0400

    HYRAX-1956, clean up code a bit.

--- modules/fileout_netcdf/FONcArray.cc
+++ modules/fileout_netcdf/FONcArray.cc
@@ -720,7 +720,6 @@ void FONcArray::define(int ncid) {
                         FONcRequestHandler::use_shuffle)                
                         shuffle = 1;
                     
-                    //FONcRequestHandler::float_write_opt == false ) 
                     bool enable_float_write_opt = check_float_write_opt();
                     if (NC_SHORT == d_array_type || NC_USHORT == d_array_type || NC_INT == d_array_type ||
                         NC_UINT == d_array_type || NC_INT64 == d_array_type || NC_UINT64 == d_array_type ||
