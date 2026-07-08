e199415a5631689b97afb489fbd000b322804849
kyang2014 <kyang2014@users.noreply.github.com>
2026-07-08 13:46:54 -0400

    HYRAX-2205, add variable-string array tests for different encoding methods in the dmrpp module to the dmrpp and fileout netCDF modules. (#1408)

---
+++ modules/hdf5_handler/data/src/t_vl_str_dup.py
@@ -0,0 +1,21 @@
+import h5py
+import numpy as np
+
+# Variable-length UTF-8 string dtype
+str_dtype = h5py.string_dtype(encoding='utf-8')
+
+with h5py.File("vl_str_dup_array.h5", "w") as f:
+    # Create a 2x4 variable-length string dataset
+    dset = f.create_dataset(
+        "str_dup",
+        shape=(2, 4),
+        dtype=str_dtype
+    )
+
+    # Fill the dataset
+    dset[...] = np.array([
+        ["apple", "banana", "cherry", "cherry"],
+        ["giraffe", "giraffe", "giraffe", "hippopotamus"]
+    ], dtype=object)
+
+
