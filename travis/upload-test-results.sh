f4b7862f614a6a8e9cc93980a09edb5e3e7602d7
Nathan Potter <ndp@opendap.org>
2026-07-14 12:34:23 -0700

    Push the 'make check' log output out of the docker builds to S3 (#1407)
    Co-authored-by: Kodi Neumiller <kneumiller@mtech.edu>

--- travis/upload-test-results.sh
+++ travis/upload-test-results.sh
@@ -1,31 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Upload the results of tests after running a build on Travis
+function loggy() {
+    echo  "$@" | awk '{ print "# upload-test-result() - "$0;}'  >&2
+}
+HR="^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+HR1="--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---"
 
-LOG_FILE_TGZ=bes-autotest-${TRAVIS_JOB_NUMBER}-logs.tar.gz
+LOG_FILE_TGZ="/tmp/bes-autotest-${TRAVIS_JOB_NUMBER}-logs.tgz"
 S3_BUCKET="s3://opendap.travis.tests"
 
-if test "$BES_BUILD" = main -o "$BES_BUILD" = distcheck -o "$BES_BUILD" = "docker-el8" -o "$BES_BUILD" = "docker-el9"
-then
-  echo "Packaging log files for '$BES_BUILD'"
+loggy "$HR"
+loggy "BEGIN"
 
-  # Create a tarball for non-docker builds from local files - kln 6/23/26
-  if ! test -f /tmp/${LOG_FILE_TGZ}; then
-    echo "No tarball found, creating a new tarball of test logs"
-    tar -czf /tmp/${LOG_FILE_TGZ} $(find . \( -name "*.log" -o -name "*site_map.txt" \) -print)
+loggy "$HR1"
+
+if test -f "$LOG_FILE_TGZ"; then
+  if [ -n "$AWS_ACCESS_KEY_ID" ]; then
+      loggy "Pushing $LOG_FILE_TGZ to S3"
+      aws s3 cp "${LOG_FILE_TGZ}" "${S3_BUCKET}"
+  else
+    loggy "Skipping upload to S3, no AWS credentials were found."
   fi
-
-  # using: 'test -z "$AWS_ACCESS_KEY_ID" || ...' keeps after_script from running
-  # the aws cli for forked PRs (where secure env vars are null). I could've used
-  # an 'if' to block out the whole script, but I didn't... jhrg 3/21/18
-  test -z "$AWS_ACCESS_KEY_ID" || aws s3 cp /tmp/${LOG_FILE_TGZ} ${S3_BUCKET}
+else
+  loggy "ERROR - The test logs tarball ($LOG_FILE_TGZ) was not found."
 fi
 
 # A quick hack to get the gcovr report to S3. jhrg 4/20/23
-if test x$BES_BUILD = xsonar-bes-framework
+if test "$BES_BUILD" = "xsonar-bes-framework"
 then
 	# using: 'test -z "$AWS_ACCESS_KEY_ID" || ...' keeps after_script from running
 	# the aws cli for forked PRs (where secure env vars are null). I could've used
 	# an 'if' to block out the whole script, but I didn't... jhrg 3/21/18
-	test -z "$AWS_ACCESS_KEY_ID" || aws s3 cp ./gcovr_report.txt ${S3_BUCKET}/bes-gcov-${TRAVIS_JOB_NUMBER}.txt
+	test -z "$AWS_ACCESS_KEY_ID" || aws s3 cp ./gcovr_report.txt "${S3_BUCKET}/bes-gcov-${TRAVIS_JOB_NUMBER}.txt"
 fi
+
+loggy "END"
+loggy "$HR"
